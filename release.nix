56365164ca48bce624958e3aeceee4ddf4b21b49
Gabriel Gonzalez <Gabriel439@gmail.com>
2016-12-19 20:27:15 -0800

    Add Nix derivations for building Dhall

---
+++ release.nix
@@ -0,0 +1,27 @@
+# You can build this repository using Nix by running:
+#
+#     $ nix-build -A dhall release.nix
+#
+# You can also open up this repository inside of a Nix shell by running:
+#
+#     $ nix-shell -A dhall.env release.nix
+#
+# ... and then Nix will supply the correct Haskell development environment for
+# you
+let
+  config = {
+    packageOverrides = pkgs: {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: {
+          dhall = haskellPackagesNew.callPackage ./default.nix { };
+        };
+      };
+    };
+  };
+
+  pkgs =
+    import <nixpkgs> { inherit config; };
+
+in
+  { dhall = pkgs.haskellPackages.dhall;
+  }
