47fec820ee0527f1204dd63fe7fe25f214c6dc77
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-01-20 22:38:37 -0800

    Include `test` Nix expression
    This tests that `dhall-to-nix` can be used to build Nix derivations

--- dhall-nix/release.nix
+++ dhall-nix/release.nix
@@ -24,4 +24,27 @@ let
 
 in
   { dhall-nix = pkgs.haskellPackages.dhall-nix;
+
+    test =
+      let
+        dhallToNix = code :
+          let
+            file = builtins.toFile "dhall-expr" code;
+
+            drv = pkgs.stdenv.mkDerivation {
+              name = "dhall-expr-as-nix";
+
+              buildCommand = ''
+                dhall-to-nix <<< "${file}" > $out
+              '';
+
+              buildInputs = [ pkgs.haskellPackages.dhall-nix ];
+            };
+
+          in
+            import "${drv}";
+
+      in if (dhallToNix "{ foo = 1, bar = False}").bar
+         then pkgs.haskellPackages.lens-family-core
+         else pkgs.haskellPackages.pipes;
   }
