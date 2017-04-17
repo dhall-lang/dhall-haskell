7f7c2620c01a7301a916cd106e23b19d977acb6a
Markus Hauck <markus1189@gmail.com>
2017-04-17 18:54:11 +0200

    Add new argument: --version (#39)
    Useful to see which version of `dhall` you are currently using.

--- exec/Main.hs
+++ exec/Main.hs
@@ -7,15 +7,19 @@
 module Main where
 
 import Control.Exception (SomeException)
+import Control.Monad (when)
 import Data.Monoid (mempty)
+import Data.Version (showVersion)
 import Dhall.Core (pretty, normalize)
 import Dhall.Import (Imported(..), load)
 import Dhall.Parser (Src, exprFromText)
 import Dhall.TypeCheck (DetailedTypeError(..), TypeError)
 import Options.Generic (Generic, ParseRecord, type (<?>)(..))
 import System.IO (stderr)
-import System.Exit (exitFailure)
+import System.Exit (exitFailure, exitSuccess)
 import Text.Trifecta.Delta (Delta(..))
+
+import qualified Paths_dhall as Meta
 
 import qualified Control.Exception
 import qualified Data.Text.Lazy.IO
@@ -27,6 +31,7 @@ data Mode = Default | Resolve | TypeCheck | Normalize
 
 data Options = Options
     { explain :: Bool <?> "Explain error messages in more detail"
+    , version :: Bool <?> "Display version and exit"
     } deriving (Generic)
 
 instance ParseRecord Options
@@ -34,7 +39,9 @@ instance ParseRecord Options
 main :: IO ()
 main = do
     options <- Options.Generic.getRecord "Compiler for the Dhall language"
-
+    when (unHelpful (version options)) $ do
+      putStrLn (showVersion Meta.version)
+      exitSuccess
     let handle =
                 Control.Exception.handle handler2
             .   Control.Exception.handle handler1
