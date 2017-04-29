4b8d8438ea2c2705401c053875b594abe77ba31b
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-04-29 14:46:07 -0700

    Update `default.nix`

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 { mkDerivation, ansi-wl-pprint, base, bytestring, containers
 , http-client, http-client-tls, lens, neat-interpolation
 , optparse-generic, parsers, stdenv, system-fileio, system-filepath
-, text, text-format, transformers, trifecta, unordered-containers
-, vector
+, tasty, tasty-hunit, text, text-format, transformers, trifecta
+, unordered-containers, vector
 }:
 mkDerivation {
   pname = "dhall";
-  version = "1.0.2";
+  version = "1.2.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
@@ -17,6 +17,7 @@ mkDerivation {
     unordered-containers vector
   ];
   executableHaskellDepends = [ base optparse-generic text trifecta ];
+  testHaskellDepends = [ base tasty tasty-hunit text ];
   description = "A configuration language guaranteed to terminate";
   license = stdenv.lib.licenses.bsd3;
 }
