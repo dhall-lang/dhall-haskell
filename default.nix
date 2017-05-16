e0319ecaa5b1b8e582345f6e4281986f9fbabb2c
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-05-16 09:17:19 -0700

    Forbid additional characters from paths (#58)
    This reduces the number of situations where users have to end a path
    with a space

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
-{ mkDerivation, ansi-wl-pprint, base, bytestring, containers
-, http-client, http-client-tls, lens, neat-interpolation
-, optparse-generic, parsers, stdenv, system-fileio, system-filepath
-, tasty, tasty-hunit, text, text-format, transformers, trifecta
-, unordered-containers, vector
+{ mkDerivation, ansi-wl-pprint, base, bytestring, charset
+, containers, http-client, http-client-tls, lens
+, neat-interpolation, optparse-generic, parsers, stdenv
+, system-fileio, system-filepath, tasty, tasty-hunit, text
+, text-format, transformers, trifecta, unordered-containers, vector
 }:
 mkDerivation {
   pname = "dhall";
@@ -11,13 +11,15 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    ansi-wl-pprint base bytestring containers http-client
+    ansi-wl-pprint base bytestring charset containers http-client
     http-client-tls lens neat-interpolation parsers system-fileio
     system-filepath text text-format transformers trifecta
     unordered-containers vector
   ];
   executableHaskellDepends = [ base optparse-generic text trifecta ];
-  testHaskellDepends = [ base tasty tasty-hunit text ];
+  testHaskellDepends = [
+    base neat-interpolation tasty tasty-hunit text vector
+  ];
   description = "A configuration language guaranteed to terminate";
   license = stdenv.lib.licenses.bsd3;
 }
