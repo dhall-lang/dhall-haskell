02c0515bb183235653e07ee46deba37456ef0cad
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-07-21 18:28:27 -0700

    Add tests to match tutorial examples

--- tests/Tutorial.hs
+++ tests/Tutorial.hs
@@ -3,12 +3,16 @@
 
 module Tutorial where
 
+import qualified Data.Text.Lazy
+import qualified Data.Vector
+import qualified Dhall
 import qualified NeatInterpolation
 import qualified Test.Tasty
 import qualified Test.Tasty.HUnit
 import qualified Util
 
 import Test.Tasty (TestTree)
+import Test.Tasty.HUnit ((@?=))
 
 tutorialTests :: TestTree
 tutorialTests =
@@ -16,6 +20,10 @@ tutorialTests =
         [ Test.Tasty.testGroup "Interpolation"
             [ _Interpolation_0
             , _Interpolation_1
+            ]
+        , Test.Tasty.testGroup "Functions"
+            [ _Functions_0
+            , _Functions_1
             ]
         ]
 
@@ -29,7 +37,7 @@ in  "My name is $${name} and my age is $${Integer/show age}"
     Util.assertNormalizesTo e "\"My name is John Doe and my age is 21\"" )
 
 _Interpolation_1 :: TestTree
-_Interpolation_1 = Test.Tasty.HUnit.testCase "Example #0" (do
+_Interpolation_1 = Test.Tasty.HUnit.testCase "Example #1" (do
     e <- Util.code [NeatInterpolation.text|
 ''
     for file in *; do
@@ -38,3 +46,20 @@ _Interpolation_1 = Test.Tasty.HUnit.testCase "Example #0" (do
 ''
 |]
     Util.assertNormalized e )
+
+_Functions_0 :: TestTree
+_Functions_0 = Test.Tasty.HUnit.testCase "Example #0" (do
+    let text = Data.Text.Lazy.fromStrict [NeatInterpolation.text|
+\(n : Bool) ->
+    [ n && True, n && False, n || True, n || False ]
+|]
+    makeBools <- Dhall.input Dhall.auto text
+    makeBools True @?= Data.Vector.fromList [True,False,True,True] )
+
+_Functions_1 :: TestTree
+_Functions_1 = Test.Tasty.HUnit.testCase "Example #1" (do
+    let text = Data.Text.Lazy.fromStrict [NeatInterpolation.text|
+λ(x : Bool) → λ(y : Bool) → x && y
+|]
+    makeBools <- Dhall.input Dhall.auto text
+    makeBools True False @?= False )
