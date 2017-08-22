b58100759dd65bba495ff7f8714d52418937b3db
bosu <boriss@gmail.com>
2017-08-22 23:30:28 +0300

    Interpret instance for unnamed data fields (#103)
    Maps unnamed data fields to the positional record keys. For example:
    
    ```haskell
    data Foo = Foo Bool Integer Natural deriving (Generic, Interpret)
    ```
    
    ... is mapped to this Dhall data type:
    
    ```haskell
    Record { _1 : Bool, _2 : Integer, _3 : Natural }
    ```

--- tests/Regression.hs
+++ tests/Regression.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Regression where
 
+import qualified Data.Map
+import qualified Dhall
+import qualified Dhall.Core
 import qualified Test.Tasty
 import qualified Test.Tasty.HUnit
 import qualified Util
@@ -12,7 +18,24 @@ regressionTests :: TestTree
 regressionTests =
     Test.Tasty.testGroup "regression tests"
         [ issue96
+          , unnamedFields
         ]
+
+data Foo = Foo Integer Bool | Bar Bool Bool Bool | Baz Integer Integer
+    deriving (Show, Dhall.Generic, Dhall.Interpret)
+
+unnamedFields :: TestTree
+unnamedFields = Test.Tasty.HUnit.testCase "Unnamed Fields" (do
+    let ty = Dhall.auto @Foo
+    Test.Tasty.HUnit.assertEqual "Good type" (Dhall.expected ty) (Dhall.Core.Union (
+            Data.Map.fromList [
+                ("Bar",Dhall.Core.Record (Data.Map.fromList [
+                    ("_1",Dhall.Core.Bool),("_2",Dhall.Core.Bool),("_3",Dhall.Core.Bool)]))
+                , ("Baz",Dhall.Core.Record (Data.Map.fromList [
+                    ("_1",Dhall.Core.Integer),("_2",Dhall.Core.Integer)]))
+                ,("Foo",Dhall.Core.Record (Data.Map.fromList [
+                    ("_1",Dhall.Core.Integer),("_2",Dhall.Core.Bool)]))]))
+    return () )
 
 issue96 :: TestTree
 issue96 = Test.Tasty.HUnit.testCase "Issue #96" (do
