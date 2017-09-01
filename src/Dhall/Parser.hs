dhall-lang/dhall-haskell

commit 52d097eba3ffb820ce7e86d0fe2655437ef0915c

Minor simplification to `token` (#109)
The implementation of `token` for `Parser` is identical to the one for
`Text.Trifecta.Parser`, so we can simplify the implementation as just
unwrapping and wrapping the newtypes

src/Dhall/Parser.hs
@@ -102,10 +102,7 @@ instance TokenParsing Parser where
 
     highlight h (Parser m) = Parser (highlight h m)
 
-    token parser = do
-        r <- parser
-        Text.Parser.Token.whiteSpace
-        return r
+    token parser = Parser (token (unParser parser))
 
 identifierStyle :: IdentifierStyle Parser
 identifierStyle = IdentifierStyle

commit 1d525faca2d0e2cb6da6578f1dabf34086cafee6

Fix comment parsing
I broke comment parsing in 52d097eba3ffb820ce7e86d0fe2655437ef0915c due to
accidentally changing how `token` behaves.  What was supposed to be a
behavior-preserving simplification actually broke parsing of comments within
the internal whitespace of an expression

The root of the problem was that `token` used to correctly use the `whitespace`
parser for `Dhall.Parser.Parser`, which correctly parses comments, but after
the breaking change `token` would defer to the behavior of `whitespace` for
`Text.Trifecta.Parser`, which does not support parsing comments

The correct simplification is to omit the definition of `token` altogether,
because the default implementation matches the correct implementation.  This
change updates `token` to default to the right implementation

src/Dhall/Parser.hs
@@ -101,8 +101,6 @@ instance TokenParsing Parser where
 
     highlight h (Parser m) = Parser (highlight h m)
 
-    token parser = Parser (token (unParser parser))
-
 identifierStyle :: IdentifierStyle Parser
 identifierStyle = IdentifierStyle
     { _styleName     = "dhall"