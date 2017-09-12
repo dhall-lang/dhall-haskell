b6824330da49a974c6ed3417d1d2f6b6be49eacb
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-09-12 13:49:41 -0700

    Change `shell.nix` to stay in sync with `release.nix` (#133)
    This simplifies the `shell.nix` to reuse the `dhall` derivation from
    `release.nix` and also ensures that they stay in sync with one another

--- shell.nix
+++ shell.nix
@@ -1,15 +1,1 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
-
-let
-
-  inherit (nixpkgs) pkgs;
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  drv = haskellPackages.callPackage ./. {};
-
-in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+(import ./release.nix).dhall.env
