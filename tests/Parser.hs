005f8faf500051b9e40cca60b1a4d56f9845e7a0
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-10-06 18:10:06 -0700

    Add more parsing tests

--- tests/Parser.hs
+++ tests/Parser.hs
@@ -14,8 +14,39 @@ parserTests :: TestTree
 parserTests =
     Test.Tasty.testGroup "parser tests"
         [ Test.Tasty.testGroup "whitespace"
-            [ shouldPass "prefix/suffix" "./tests/parser/whitespace.dhall"
-            , shouldPass "block comment" "./tests/parser/blockComment.dhall"
+            [ shouldPass
+                "prefix/suffix"
+                "./tests/parser/whitespace.dhall"
+            , shouldPass
+                "block comment"
+                "./tests/parser/blockComment.dhall"
+            , shouldPass
+                "nested block comment"
+                "./tests/parser/nestedBlockComment.dhall"
+            , shouldPass
+                "line comment"
+                "./tests/parser/lineComment.dhall"
+            , shouldPass
+                "Unicode comment"
+                "./tests/parser/unicodeComment.dhall"
+            , shouldPass
+                "whitespace buffet"
+                "./tests/parser/whitespaceBuffet.dhall"
+            , shouldPass
+                "label"
+                "./tests/parser/label.dhall"
+            , shouldPass
+                "quoted label"
+                "./tests/parser/quotedLabel.dhall"
+            , shouldPass
+                "double quoted string"
+                "./tests/parser/doubleQuotedString.dhall"
+            , shouldPass
+                "Unicode double quoted string"
+                "./tests/parser/unicodeDoubleQuotedString.dhall"
+            , shouldPass
+                "escaped double quoted string"
+                "./tests/parser/escapedDoubleQuotedString.dhall"
             ]
         ]
 
