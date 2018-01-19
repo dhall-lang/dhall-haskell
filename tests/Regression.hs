859345491cb7a376077eeda9cf21efeab96ba977
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-01-19 09:57:43 -0800

    Fix infinite loop when pretty-printing `constructors` keyword
    Fixes #209
    
    The `prettyExprD` function would only transition to the `docs` loop if the
    outermost constructor at precedence level "D" was an `App` constructor.  This
    meant that the following code would pretty-print correctly:
    
    ```haskell
    (constructors x) y
    ```
    
    ... but this code would not pretty-print correctly and would instead trigger
    an infinite loop:
    
    ```haskell
    constructors x
    ```
    
    The reason for the infinite loop is that the pretty-printing logic assumes that
    the pretty-printer will make progress for at least one precedence level in order
    to terminate.  However, to the above issue the `Constructors` keyword would not
    make progress at precedence level "D", which triggered the infinite loop.
    
    The fix is to update `prettyExprD` to transition to the `docs` loops for both
    the `App` and `Constructor` constructors.

--- tests/Regression.hs
+++ tests/Regression.hs
@@ -10,10 +10,12 @@ import qualified Data.Map
 import qualified Dhall
 import qualified Dhall.Core
 import qualified Dhall.Parser
+import qualified System.Timeout
 import qualified Test.Tasty
 import qualified Test.Tasty.HUnit
 import qualified Util
 
+import Control.DeepSeq (($!!))
 import Dhall.Import (Imported)
 import Dhall.Parser (Src)
 import Dhall.TypeCheck (TypeError, X)
@@ -120,6 +122,15 @@ issue201 :: TestTree
 issue201 = Test.Tasty.HUnit.testCase "Issue #201" (do
     -- Verify that type synonyms work
     _ <- Util.code "./tests/regression/issue201.dhall"
+    return () )
+
+issue209 :: TestTree
+issue209 = Test.Tasty.HUnit.testCase "Issue #209" (do
+    -- Verify that pretty-printing `constructors` doesn't trigger an infinite
+    -- loop
+    e <- Util.code "./tests/regression/issue209.dhall"
+    let text = Dhall.Core.pretty e
+    Just _ <- System.Timeout.timeout 1000000 (Control.Exception.evaluate $!! text)
     return () )
 
 parsing0 :: TestTree
