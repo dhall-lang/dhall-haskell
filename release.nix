34dc69c7dd420cc1a9fa0c0da197731225726427
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-02-18 11:37:47 -0800

    Generate statically linked executables
    Eventually we will want to encourage users to download pre-built executables
    from Hydra, but this requires statically linking them so that they have
    a smaller footprint.  Otherwise users will have to download an entire
    GHC installation.

--- release.nix
+++ release.nix
@@ -1,19 +1,12 @@
-# You can build this repository using Nix by running:
-#
-#     $ nix-build release.nix
-#
-# You can also open up this repository inside of a Nix shell by running:
-#
-#     $ nix-shell
-#
-# ... and then Nix will supply the correct Haskell development environment for
-# you
 let
   config = {
     packageOverrides = pkgs: {
       haskellPackages = pkgs.haskellPackages.override {
         overrides = haskellPackagesNew: haskellPackagesOld: {
-          dhall = haskellPackagesNew.callPackage ./default.nix { };
+          dhall =
+            pkgs.haskell.lib.justStaticExecutables
+              (haskellPackagesNew.callPackage ./default.nix { })
+            ;
         };
       };
     };
@@ -23,5 +16,5 @@ let
     import <nixpkgs> { inherit config; };
 
 in
-  { dhall = pkgs.haskellPackages.dhall;
+  { inherit (pkgs.haskellPackages) dhall;
   }
