68d6b98cdbbc782b7e4ac92b47b5b65271ea9b16
Oliver Charles <ollie@ocharles.org.uk>
2018-02-18 17:30:17 +0000

    Regenerate default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
-{ mkDerivation, ansi-wl-pprint, base, base16-bytestring, bytestring
+{ mkDerivation, ansi-terminal, base, base16-bytestring, bytestring
 , case-insensitive, charset, containers, contravariant, cryptohash
-, deepseq, directory, exceptions, filepath, http-client
+, deepseq, directory, exceptions, filepath, haskeline, http-client
 , http-client-tls, insert-ordered-containers, lens-family-core
-, megaparsec, optparse-generic, parsers, prettyprinter, scientific
-, stdenv, system-filepath, tasty, tasty-hunit, text, text-format
-, transformers, unordered-containers, vector
+, megaparsec, mtl, optparse-generic, parsers, prettyprinter
+, prettyprinter-ansi-terminal, repline, scientific, stdenv, tasty
+, tasty-hunit, text, text-format, transformers, trifecta
+, unordered-containers, vector
 }:
 mkDerivation {
   pname = "dhall";
@@ -13,14 +14,17 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    ansi-wl-pprint base base16-bytestring bytestring case-insensitive
+    ansi-terminal base base16-bytestring bytestring case-insensitive
     charset containers contravariant cryptohash directory exceptions
     filepath http-client http-client-tls insert-ordered-containers
-    lens-family-core megaparsec parsers prettyprinter scientific text
-    text-format transformers unordered-containers vector
+    lens-family-core megaparsec parsers prettyprinter
+    prettyprinter-ansi-terminal scientific text text-format
+    transformers unordered-containers vector
   ];
   executableHaskellDepends = [
-    base megaparsec optparse-generic prettyprinter system-filepath text
+    ansi-terminal base containers haskeline megaparsec mtl
+    optparse-generic prettyprinter prettyprinter-ansi-terminal repline
+    text trifecta
   ];
   testHaskellDepends = [
     base containers deepseq insert-ordered-containers prettyprinter
