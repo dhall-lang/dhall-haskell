b96c99068f63ab33efc1283274173c38c7971941
Paul Cadman <git@paulcadman.io>
2018-03-04 17:38:55 +0000

    Fix a typo in the docs for Union constructors

--- src/Dhall/Tutorial.hs
+++ src/Dhall/Tutorial.hs
@@ -1115,7 +1115,7 @@ import Dhall
 -- common pattern:
 --
 -- >     let MyType = constructors < Empty : {} | Person : { name : Text, age : Natural } >
--- > in  [   MyType.Empty {}
+-- > in  [   MyType.Empty {=}
 -- >     ,   MyType.Person { name = "John", age = +23 }
 -- >     ,   MyType.Person { name = "Amy" , age = +25 }
 -- >     ]
