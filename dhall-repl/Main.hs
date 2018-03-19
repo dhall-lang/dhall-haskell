56d578c5764ec5874641dd50f5ab629ace5749b3
Ville Tirronen <ville.tirronen@jyu.fi>
2018-03-19 15:36:58 +0200

    A command to interact with local ipfs installation
    Since dhall has some affinity with ipfs I added a dhall-repl
    command to directly push expressions to ipfs.
    
    Here is an example of use:
    
        ⊢ :ipfs h = (\(x:Text) -> x++x)
    
        Added with hash: Qmee1h5H7o8Lp2FQtRJAPNGgLKRdrUSGn5jPkW6NBBxxh8
        ⊢ h
        "Qmee1h5H7o8Lp2FQtRJAPNGgLKRdrUSGn5jPkW6NBBxxh8"
    
    Then on a completely different machine:
    
        ⊢ https://ipfs.io/ipfs/Qmee1h5H7o8Lp2FQtRJAPNGgLKRdrUSGn5jPkW6NBBxxh8 "hello "
        "hello hello "
        ⊢
    
    I find this command delightful, but I won't be offended if this doesn't get
    merged.
    
    This incurs dependencies on typed-process, directory and deepseq. Also,
    it needs -threaded to work properly.

--- dhall-repl/Main.hs
+++ dhall-repl/Main.hs
@@ -8,13 +8,17 @@ import Control.Exception ( SomeException(SomeException), displayException, throw
 import Control.Monad.IO.Class ( MonadIO, liftIO )
 import Control.Monad.State.Class ( MonadState, get, modify )
 import Control.Monad.State.Strict ( evalStateT )
+import Control.DeepSeq (deepseq)
 import Data.List ( foldl' )
+import Data.Maybe ( fromMaybe )
 
+import qualified Data.Text.Lazy.Builder as Builder (fromLazyText)
 import qualified Data.Text.Lazy as LazyText
+import qualified Data.Text.Lazy.IO as LazyText
 import qualified Data.Text.Prettyprint.Doc as Pretty
 import qualified Data.Text.Prettyprint.Doc.Render.Terminal as Pretty ( renderIO )
 import qualified Dhall.Context
-import qualified Dhall.Core as Dhall ( Var(V), Expr, normalize )
+import qualified Dhall.Core as Dhall ( Var(V), Expr(Text, TextLit), normalize, Chunks(..) )
 import qualified Dhall.Pretty
 import qualified Dhall.Core as Expr ( Expr(..) )
 import qualified Dhall.Import as Dhall
@@ -24,6 +28,8 @@ import qualified System.Console.ANSI
 import qualified System.Console.Haskeline.MonadException as Haskeline
 import qualified System.Console.Repline as Repline
 import qualified System.IO
+import qualified System.Directory
+import qualified System.Process.Typed as Process
 import qualified Text.Trifecta.Delta as Trifecta
 
 
@@ -201,6 +207,53 @@ saveBinding (file : "=" : tokens) = do
   liftIO (System.IO.withFile file System.IO.WriteMode handler)
 saveBinding _ = fail ":save should be of the form `:save x = y`"
 
+exportIPFS :: ( MonadIO m, MonadState Env m ) => [String] -> m ()
+exportIPFS (hash : "=" : tokens) = do
+  ipfsExecutable <- fromMaybe ( fail "Could not find program `ipfs`. Make sure ipfs is on your path" )
+                              <$> 
+                              ( liftIO ( System.Directory.findExecutable "ipfs" ) )
+
+  let
+    varName = LazyText.pack hash
+
+  loadedExpression <- parseAndLoad (unwords tokens)
+
+  _ <- typeCheck loadedExpression
+
+  normalizedExpression <- normalize loadedExpression
+
+  let ipfsConfig =   Process.setStdin  Process.createPipe 
+                   $ Process.setStdout Process.createPipe
+                   $ Process.proc ipfsExecutable ["add","-Q"]
+
+  hash <- liftIO $ Process.withProcess_  ipfsConfig $ \ipfsProcess -> do 
+        let stdin = Process.getStdin ipfsProcess
+
+        output stdin normalizedExpression
+
+        System.IO.hClose stdin
+
+        r <- LazyText.hGetContents (Process.getStdout ipfsProcess)
+
+        let stripped = LazyText.strip r
+
+        stripped `deepseq` return stripped
+
+  liftIO $ LazyText.putStrLn (LazyText.append "Added with hash: "hash)
+  modify
+    ( \e ->
+        e
+          { envBindings =
+              Dhall.Context.insert
+                varName
+                Binding { bindingType = Dhall.Text
+                        , bindingExpr = Dhall.TextLit ( Dhall.Chunks [] (Builder.fromLazyText hash) ) }
+                ( envBindings e )
+          }
+    )
+
+exportIPFS _ = fail ":ipfs command should be of the form `:ipfs hash = x`"
+
 
 options
   :: ( Haskeline.MonadException m, MonadIO m, MonadState Env m )
@@ -209,6 +262,7 @@ options =
   [ ( "type", dontCrash . typeOf )
   , ( "let", dontCrash . addBinding )
   , ( "save", dontCrash . saveBinding )
+  , ( "ipfs", dontCrash . exportIPFS )
   ]
 
 
