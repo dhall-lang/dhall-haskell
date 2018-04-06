8c0f09fb4d201856564382943ee6a5566b048ffa
Javier Neira <atreyu.bbb@gmail.com>
2018-04-06 23:39:21 +0200

    Support for ghc-7.10.3 / lts-6.27 (#340)

--- src/Dhall.hs
+++ src/Dhall.hs
@@ -9,7 +9,6 @@
 {-# LANGUAGE RecordWildCards            #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE TypeOperators              #-}
 
 {-| Please read the "Dhall.Tutorial" module, which contains a tutorial explaining
@@ -884,7 +883,7 @@ instance Inject Scientific where
 
 instance Inject Double where
     injectWith =
-        fmap (contramap (Data.Scientific.fromFloatDigits @Double)) injectWith
+        fmap (contramap (Data.Scientific.fromFloatDigits :: Double -> Scientific)) injectWith
 
 instance Inject () where
     injectWith _ = InputType {..}
