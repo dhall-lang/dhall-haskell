c92dfacbd5d90686516c72f540b95e4cc93ddf29
Gregory Pfeil <greg@technomadic.org>
2018-06-01 14:01:39 -0600

    TemplateHaskell improvements.

--- src/Dhall/TH.hs
+++ src/Dhall/TH.hs
@@ -1,28 +1,49 @@
 {-# LANGUAGE TemplateHaskell #-}
 
+{-| This module provides `staticDhallExpression` which can be used to resolve
+    all of an expression’s imports at compile time, allowing one to reference
+    Dhall expression from Haskell without having a runtime dependency on the
+    location of Dhall files.
+
+    For example, given a file “Some/Type.dhall” containing
+
+        < This : Natural, Other : ../Other/Type.dhall >
+
+    rather than duplicating the AST manually in a Haskell `Type`, you can do
+
+        Dhall.Type
+        (\case
+            UnionLit "This" _ _  -> ...
+            UnionLit "Other" _ _ -> ...)
+        $(staticDhallExpression "../../Some/Type.dhall")
+
+    This would create the Dhall Expr AST from the `Type.dhall` file at compile
+    time with all imports resolved, making it easy to keep your Dhall configs
+    and Haskell interpreters in sync.
+-}
 module Dhall.TH where
 
-import           Control.Monad
+import Control.Monad
+import Data.Typeable
+import Language.Haskell.TH.Syntax
+
 import qualified Data.Text.Lazy as Text
-import           Data.Typeable
-import qualified Dhall.Core as D
-import qualified Dhall.Import as D
-import qualified Dhall.Parser as D
-import qualified Dhall.TypeCheck as D
-import           Language.Haskell.TH.Syntax
-
--- A workaround for a problem in TemplateHaskell (see
--- https://stackoverflow.com/questions/38143464/cant-find-inerface-file-declaration-for-variable)
-liftText :: Text.Text -> Q Exp
-liftText = fmap (AppE (VarE 'Text.pack)) . lift . Text.unpack
-
-importDhallExpression :: Text.Text -> IO (D.Expr D.Src D.X)
-importDhallExpression =
-  either (const (fail "invalid Dhall expression")) D.load
-  . D.exprFromText "(static)"
+import qualified Dhall.Import
+import qualified Dhall.Parser
 
 -- | This resolves all imports in the expression, so the resulting AST is self-
 --   contained.
 staticDhallExpression :: Text.Text -> Q Exp
 staticDhallExpression =
-  dataToExpQ (\a -> liftText <$> cast a) <=< runIO . importDhallExpression
+    dataToExpQ (\a -> liftText <$> cast a) <=< runIO . importDhallExpression
+  where
+    importDhallExpression =
+      either (fail . (++ "\nin Dhall.TH.staticDhallExpression.") . show)
+             Dhall.Import.load
+      . Dhall.Parser.exprFromText "(static)"
+
+    -- A workaround for a problem in TemplateHaskell (see
+    -- https://stackoverflow.com/questions/38143464/cant-find-inerface-file-declaration-for-variable)
+    liftText = fmap (AppE (VarE 'Text.pack)) . lift . Text.unpack
+
+
