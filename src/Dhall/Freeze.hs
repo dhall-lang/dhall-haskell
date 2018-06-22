8653a7372a0bb7275178fafefbf011cd9e8a85df
Tobias Pflug <tobias.pflug@holidaycheck.com>
2018-06-22 13:12:57 +0200

    Handle ansi support properly

--- src/Dhall/Freeze.hs
+++ src/Dhall/Freeze.hs
@@ -4,11 +4,14 @@ module Dhall.Freeze (freeze) where
 
 import Dhall.Core
 import Dhall.Import (load, hashExpression)
-import Dhall.Parser (exprAndHeaderFromText)
-import Data.Text.Prettyprint.Doc (Pretty)
-import Dhall.Pretty (prettyExpr)
+import Dhall.Parser (exprAndHeaderFromText, Src)
+import Dhall.Pretty (annToAnsiStyle)
 
+import System.Console.ANSI (hSupportsANSI)
+import Data.Monoid ((<>))
+import Data.Maybe (fromMaybe)
 import Data.Text
+
 import qualified Data.Text.Prettyprint.Doc                 as Pretty
 import qualified Data.Text.Prettyprint.Doc.Render.Terminal as Pretty
 import qualified Control.Exception
@@ -20,25 +23,10 @@ opts =
     Pretty.defaultLayoutOptions
         { Pretty.layoutPageWidth = Pretty.AvailablePerLine 80 1.0 }
 
-render :: Pretty a => Expr s a -> Text
-render e = do
-    let doc = prettyExpr e
-    let layoutOptions = opts
-    let stream = Pretty.layoutSmart layoutOptions doc
-    let ansiStream = Pretty.unAnnotateS stream
-    Pretty.renderStrict ansiStream
-
-
 readInput :: Maybe FilePath -> IO Text
 readInput = maybe fromStdin Data.Text.IO.readFile
     where 
-        fromStdin  = System.IO.hSetEncoding System.IO.stdin System.IO.utf8 >> Data.Text.IO.getContents
-
-writeFrozen :: Maybe FilePath -> Text -> IO ()
-writeFrozen inplace txt = maybe (toStdout txt) (writeToFile txt) inplace
-    where
-        toStdout = Data.Text.IO.putStrLn
-        writeToFile text path = Data.Text.IO.writeFile path text
+        fromStdin = System.IO.hSetEncoding System.IO.stdin System.IO.utf8 >> Data.Text.IO.getContents
 
 hashImport :: Import -> IO Import
 hashImport import_ = do
@@ -47,17 +35,39 @@ hashImport import_ = do
     let newImportHashed = (importHashed import_) { hash = expressionHash }
     return $ import_ { importHashed = newImportHashed }
 
-freezeExpression :: Text -> IO Text
-freezeExpression txt = do
-    (_, expr) <- case exprAndHeaderFromText "" txt of
+parseExpr :: String -> Text -> IO (Text, Expr Src Import)
+parseExpr src txt =
+    case exprAndHeaderFromText src txt of
         Left err -> Control.Exception.throwIO err
         Right x  -> return x
-    frozenExpr <- traverse hashImport expr
-    return $ render frozenExpr
 
+freezeExpr :: (Text, Expr s Import) -> IO (Text, Expr s Import)
+freezeExpr (t, e) = do
+    e' <- traverse hashImport e
+    return (t, e')
+
+writeExpr :: Maybe FilePath -> (Text, Expr s Import) -> IO ()
+writeExpr inplace (header, expr) = do
+    let doc = Pretty.pretty header <> Pretty.pretty expr
+    let layoutOptions = opts
+    let stream = Pretty.layoutSmart layoutOptions doc
+
+    case inplace of
+        Just f ->
+            System.IO.withFile f System.IO.WriteMode (\h ->
+                Pretty.renderIO h (annToAnsiStyle <$> stream))
+
+        Nothing -> do
+            supportsANSI <- System.Console.ANSI.hSupportsANSI System.IO.stdout
+            if supportsANSI 
+               then 
+                 Pretty.renderIO System.IO.stdout (annToAnsiStyle <$> Pretty.layoutSmart opts doc)
+               else
+                 Pretty.renderIO System.IO.stdout (Pretty.layoutSmart opts (Pretty.unAnnotate doc)) 
 
 freeze :: Maybe FilePath -> IO ()
-freeze inplace = readExpression >>= freezeExpression >>= writeExpression
-    where
-        readExpression = readInput inplace
-        writeExpression = writeFrozen inplace
+freeze inplace = do
+    expr <- readInput inplace
+    parseExpr srcInfo expr >>= freezeExpr >>= writeExpr inplace
+        where
+            srcInfo = fromMaybe "(stdin)" inplace
