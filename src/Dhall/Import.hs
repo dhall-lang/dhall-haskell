072f1cb232b578adaa17be20ea1a1184562a088c
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2018-06-24 12:36:30 +0300

    Cleanup

--- src/Dhall/Import.hs
+++ src/Dhall/Import.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE CPP                 #-}
 {-# LANGUAGE DeriveDataTypeable  #-}
-{-# LANGUAGE GADTs               #-}
 {-# LANGUAGE NamedFieldPuns      #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
