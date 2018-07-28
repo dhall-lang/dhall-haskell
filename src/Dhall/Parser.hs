eb79bf2e0ad3fe6e93d7fe7c08c8f64d3dc5bebb
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-07-27 18:00:01 -0700

    Improve parsing performance on happy path
    This uses `noted = id` on the first attempt to parse an expression and then
    uses the real `noted` if parsing fails to provide better error messages

--- src/Dhall/Parser.hs
+++ src/Dhall/Parser.hs
@@ -37,7 +37,7 @@ expr = exprA import_
 -- | Parser for a top-level Dhall expression. The expression is parameterized
 -- over any parseable type, allowing the language to be extended as needed.
 exprA :: Parser a -> Parser (Expr Src a)
-exprA = completeExpression
+exprA = parseExpression noted
 
 -- | A parsing error
 data ParseError = ParseError
@@ -76,14 +76,33 @@ exprAndHeaderFromText
               --   used in parsing error messages
     -> Text   -- ^ Input expression to parse
     -> Either ParseError (Text, Expr Src Import)
-exprAndHeaderFromText delta text = case result of
-    Left errInfo   -> Left (ParseError { unwrap = errInfo, input = text })
-    Right (txt, r) -> Right (Data.Text.dropWhileEnd (/= '\n') txt, r)
+exprAndHeaderFromText delta text = do
+    (txt, r) <- case Text.Megaparsec.parse (unParser (parser id)) delta text of
+        Left _ ->
+            case Text.Megaparsec.parse (unParser (parser noted)) delta text of
+                Left errInfo ->
+                    Left (ParseError { unwrap = errInfo, input = text })
+
+                Right x ->
+                    return x
+
+        Right x ->
+            return x
+
+    return (Data.Text.dropWhileEnd (/= '\n') txt, r)
   where
-    parser = do
+    parser n = do
         (bytes, _) <- Text.Megaparsec.match whitespace
-        r <- expr
+        r <- parseExpression n import_
         Text.Megaparsec.eof
         return (bytes, r)
 
-    result = Text.Megaparsec.parse (unParser parser) delta text
+noted :: Parser (Expr Src a) -> Parser (Expr Src a)
+noted parser = do
+    before      <- Text.Megaparsec.getPosition
+    (tokens, e) <- Text.Megaparsec.match parser
+    after       <- Text.Megaparsec.getPosition
+    let src₀ = Src before after tokens
+    case e of
+        Note src₁ _ | src₀ == src₁ -> return e
+        _                          -> return (Note src₀ e)
