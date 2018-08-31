382613da5d8abc3e80a9db944912cb46d7ac92ac
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-08-31 08:14:04 -0700

    Build against `dhall-1.17.0` (#14)

--- dhall-nix/nix/dhall.nix
+++ dhall-nix/nix/dhall.nix
@@ -1,33 +1,38 @@
 { mkDerivation, ansi-terminal, base, bytestring, case-insensitive
-, containers, contravariant, criterion, cryptonite, deepseq, Diff
-, directory, doctest, exceptions, filepath, haskeline, http-client
-, http-client-tls, insert-ordered-containers, lens-family-core
-, megaparsec, memory, mockery, mtl, optparse-applicative, parsers
-, prettyprinter, prettyprinter-ansi-terminal, repline, scientific
-, stdenv, tasty, tasty-hunit, template-haskell, text, transformers
+, cborg, containers, contravariant, criterion, cryptonite, deepseq
+, Diff, directory, doctest, exceptions, filepath, hashable
+, haskeline, http-client, http-client-tls
+, insert-ordered-containers, lens-family-core, megaparsec, memory
+, mockery, mtl, optparse-applicative, parsers, prettyprinter
+, prettyprinter-ansi-terminal, QuickCheck, quickcheck-instances
+, repline, scientific, serialise, stdenv, tasty, tasty-hunit
+, tasty-quickcheck, template-haskell, text, transformers
 , unordered-containers, vector
 }:
 mkDerivation {
   pname = "dhall";
-  version = "1.16.1";
-  sha256 = "02a69a5d6c61b646a3a3822f6e077c2d298624baa142148af108bfe004e9c0d5";
+  version = "1.17.0";
+  sha256 = "a029fea856224a79f0b7cc56fbb5c566d0dfd1d915d214f682006cabf1274791";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    ansi-terminal base bytestring case-insensitive containers
+    ansi-terminal base bytestring case-insensitive cborg containers
     contravariant cryptonite Diff directory exceptions filepath
-    haskeline http-client http-client-tls insert-ordered-containers
-    lens-family-core megaparsec memory mtl optparse-applicative parsers
-    prettyprinter prettyprinter-ansi-terminal repline scientific
+    hashable haskeline http-client http-client-tls
+    insert-ordered-containers lens-family-core megaparsec memory mtl
+    optparse-applicative parsers prettyprinter
+    prettyprinter-ansi-terminal repline scientific serialise
     template-haskell text transformers unordered-containers vector
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
-    base deepseq directory doctest filepath insert-ordered-containers
-    mockery prettyprinter tasty tasty-hunit text vector
+    base containers deepseq directory doctest filepath hashable
+    insert-ordered-containers mockery prettyprinter QuickCheck
+    quickcheck-instances serialise tasty tasty-hunit tasty-quickcheck
+    text transformers vector
   ];
   benchmarkHaskellDepends = [
-    base containers criterion directory text
+    base bytestring containers criterion directory serialise text
   ];
   description = "A configuration language guaranteed to terminate";
   license = stdenv.lib.licenses.bsd3;
