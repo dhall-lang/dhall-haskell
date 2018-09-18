dhall-lang/dhall-haskell

commit 07f873244038fcf1fd48db80584b5d577a05fe20

Add `--ascii` flag (#570)
Fixes #562

This flag changes causes the pretty-printer to render ASCII instead of
Unicode

src/Dhall/Format.hs
@@ -8,7 +8,7 @@ module Dhall.Format
     ) where
 
 import Dhall.Parser (exprAndHeaderFromText)
-import Dhall.Pretty (annToAnsiStyle, prettyExpr, layoutOpts)
+import Dhall.Pretty (CharacterSet(..), annToAnsiStyle, layoutOpts)
 
 import Data.Monoid ((<>))
 
@@ -16,16 +16,18 @@ import qualified Data.Text.Prettyprint.Doc                 as Pretty
 import qualified Data.Text.Prettyprint.Doc.Render.Terminal as Pretty
 import qualified Control.Exception
 import qualified Data.Text.IO
+import qualified Dhall.Pretty
 import qualified System.Console.ANSI
 import qualified System.IO
 
 -- | Implementation of the @dhall format@ subcommand
 format
-    :: Maybe FilePath
+    :: CharacterSet
+    -> Maybe FilePath
     -- ^ Modify file in-place if present, otherwise read from @stdin@ and write
     --   to @stdout@
     -> IO ()
-format inplace = do
+format characterSet inplace = do
         case inplace of
             Just file -> do
                 text <- Data.Text.IO.readFile file
@@ -33,7 +35,8 @@ format inplace = do
                     Left  err -> Control.Exception.throwIO err
                     Right x   -> return x
 
-                let doc = Pretty.pretty header <> Pretty.pretty expr
+                let doc =   Pretty.pretty header
+                        <>  fmap annToAnsiStyle (Dhall.Pretty.prettyCharacterSet characterSet expr)
                 System.IO.withFile file System.IO.WriteMode (\handle -> do
                     Pretty.renderIO handle (Pretty.layoutSmart layoutOpts doc)
                     Data.Text.IO.hPutStrLn handle "" )
@@ -44,7 +47,8 @@ format inplace = do
                     Left  err -> Control.Exception.throwIO err
                     Right x   -> return x
 
-                let doc = Pretty.pretty header <> prettyExpr expr
+                let doc =   Pretty.pretty header
+                        <>  Dhall.Pretty.prettyCharacterSet characterSet expr
 
                 supportsANSI <- System.Console.ANSI.hSupportsANSI System.IO.stdout
 

tests/Format.hs
@@ -4,6 +4,7 @@ module Format where
 
 import Data.Monoid (mempty, (<>))
 import Data.Text (Text)
+import Dhall.Pretty (CharacterSet(..))
 import Test.Tasty (TestTree)
 
 import qualified Control.Exception
@@ -20,45 +21,61 @@ formatTests :: TestTree
 formatTests =
     Test.Tasty.testGroup "format tests"
         [ should
+            Unicode
             "prefer multi-line strings when newlines present"
             "multiline"
         , should
+            Unicode
             "escape ${ for single-quoted strings"
             "escapeSingleQuotedOpenInterpolation"
         , should
+            Unicode
             "preserve the original order of fields"
             "fieldOrder"
         , should
+            Unicode
             "escape numeric labels correctly"
             "escapeNumericLabel"
         , should
+            Unicode
             "correctly handle scientific notation with a large exponent"
             "largeExponent"
         , should
+            Unicode
             "correctly format the empty record literal"
             "emptyRecord"
         , should
+            Unicode
             "indent then/else to the same column"
             "ifThenElse"
         , should
+            Unicode
             "handle indenting long imports correctly without trailing space per line"
             "importLines"
         , should
+            Unicode
             "handle indenting small imports correctly without trailing space inline"
             "importLines2"
         , should
+            Unicode
             "not remove parentheses when accessing a field of a record"
             "importAccess"
         , should
+            Unicode
             "handle formatting sha256 imports correctly"
             "sha256Printing"
         , should
+            Unicode
             "handle formatting of Import suffix correctly"
             "importSuffix"
+        , should
+            ASCII
+            "be able to format with ASCII characters"
+            "ascii"
         ]
 
-should :: Text -> Text -> TestTree
-should name basename =
+should :: CharacterSet -> Text -> Text -> TestTree
+should characterSet name basename =
     Test.Tasty.HUnit.testCase (Data.Text.unpack name) $ do
         let inputFile =
                 Data.Text.unpack ("./tests/format/" <> basename <> "A.dhall")
@@ -70,7 +87,7 @@ should name basename =
             Left  err  -> Control.Exception.throwIO err
             Right expr -> return expr
 
-        let doc        = Data.Text.Prettyprint.Doc.pretty expr
+        let doc        = Dhall.Pretty.prettyCharacterSet characterSet  expr
         let docStream  = Data.Text.Prettyprint.Doc.layoutSmart Dhall.Pretty.layoutOpts doc
         let actualText = Data.Text.Prettyprint.Doc.Render.Text.renderStrict docStream
 

src/Dhall/Diff.hs
@@ -115,10 +115,10 @@ equals :: Diff
 equals = token Internal.equals
 
 forall :: Diff
-forall = token Internal.forall
+forall = token (Internal.forall Internal.Unicode)
 
 lambda :: Diff
-lambda = token Internal.lambda
+lambda = token (Internal.lambda Internal.Unicode)
 
 langle :: Diff
 langle = token Internal.langle
@@ -139,7 +139,7 @@ rangle :: Diff
 rangle = token Internal.rangle
 
 rarrow :: Diff
-rarrow = token Internal.rarrow
+rarrow = token (Internal.rarrow Internal.Unicode)
 
 rbrace :: Diff
 rbrace = token Internal.rbrace

src/Dhall/Main.hs
@@ -28,14 +28,12 @@ import Dhall.Binary (ProtocolVersion)
 import Dhall.Core (Expr(..), Import)
 import Dhall.Import (Imported(..))
 import Dhall.Parser (Src)
-import Dhall.Pretty (annToAnsiStyle, prettyExpr, layoutOpts)
+import Dhall.Pretty (CharacterSet(..), annToAnsiStyle, layoutOpts)
 import Dhall.TypeCheck (DetailedTypeError(..), TypeError, X)
 import Lens.Family (set)
 import Options.Applicative (Parser, ParserInfo)
 import System.Exit (exitFailure)
 import System.IO (Handle)
-
-import qualified Paths_dhall as Meta
 
 import qualified Control.Exception
 import qualified Control.Monad.Trans.State.Strict          as State
@@ -53,10 +51,12 @@ import qualified Dhall.Hash
 import qualified Dhall.Import
 import qualified Dhall.Lint
 import qualified Dhall.Parser
+import qualified Dhall.Pretty
 import qualified Dhall.Repl
 import qualified Dhall.TypeCheck
 import qualified GHC.IO.Encoding
 import qualified Options.Applicative
+import qualified Paths_dhall as Meta
 import qualified System.Console.ANSI
 import qualified System.IO
 
@@ -65,6 +65,7 @@ data Options = Options
     { mode            :: Mode
     , explain         :: Bool
     , plain           :: Bool
+    , ascii           :: Bool
     , protocolVersion :: ProtocolVersion
     }
 
@@ -95,22 +96,16 @@ parseOptions :: Parser Options
 parseOptions =
         Options
     <$> parseMode
-    <*> parseExplain
-    <*> parsePlain
+    <*> switch "explain" "Explain error messages in more detail"
+    <*> switch "plain" "Disable syntax highlighting"
+    <*> switch "ascii" "Format code using only ASCII characters"
     <*> Dhall.Binary.parseProtocolVersion
   where
-    parseExplain =
+    switch name description =
         Options.Applicative.switch
-            (   Options.Applicative.long "explain"
-            <>  Options.Applicative.help "Explain error messages in more detail"
+            (   Options.Applicative.long name
+            <>  Options.Applicative.help description
             )
-
-    parsePlain =
-        Options.Applicative.switch
-            (   Options.Applicative.long "plain"
-            <>  Options.Applicative.help "Disable syntax highlighting"
-            )
-
 
 parseMode :: Parser Mode
 parseMode =
@@ -207,6 +202,10 @@ parserInfoOptions =
 -- | Run the command specified by the `Options` type
 command :: Options -> IO ()
 command (Options {..}) = do
+    let characterSet = case ascii of
+            True  -> ASCII
+            False -> Unicode
+
     GHC.IO.Encoding.setLocaleEncoding System.IO.utf8
 
     let status =
@@ -243,7 +242,7 @@ command (Options {..}) = do
 
     let render :: Pretty a => Handle -> Expr s a -> IO ()
         render h e = do
-            let doc = prettyExpr e
+            let doc = Dhall.Pretty.prettyCharacterSet characterSet e
 
             let stream = Pretty.layoutSmart layoutOpts doc
 
@@ -302,7 +301,7 @@ command (Options {..}) = do
             render System.IO.stdout (Dhall.Core.normalize inferredType)
 
         Repl -> do
-            Dhall.Repl.repl explain protocolVersion
+            Dhall.Repl.repl characterSet explain protocolVersion
 
         Diff expr1 expr2 -> do
             expression1 <- Dhall.inputExpr expr1
@@ -315,7 +314,7 @@ command (Options {..}) = do
             Pretty.hPutDoc System.IO.stdout prettyDiff
 
         Format inplace -> do
-            Dhall.Format.format inplace
+            Dhall.Format.format characterSet inplace
 
         Freeze inplace -> do
             Dhall.Freeze.freeze inplace protocolVersion
@@ -332,7 +331,8 @@ command (Options {..}) = do
 
                     let lintedExpression = Dhall.Lint.lint expression
 
-                    let doc = Pretty.pretty header <> Pretty.pretty lintedExpression
+                    let doc =   Pretty.pretty header
+                            <>  fmap annToAnsiStyle (Dhall.Pretty.prettyCharacterSet characterSet lintedExpression)
 
                     System.IO.withFile file System.IO.WriteMode (\h -> do
                         Pretty.renderIO h (Pretty.layoutSmart layoutOpts doc)
@@ -344,7 +344,8 @@ command (Options {..}) = do
 
                     let lintedExpression = Dhall.Lint.lint expression
 
-                    let doc = Pretty.pretty header <> prettyExpr lintedExpression
+                    let doc =   Pretty.pretty header
+                            <>  Dhall.Pretty.prettyCharacterSet characterSet lintedExpression
 
                     supportsANSI <- System.Console.ANSI.hSupportsANSI System.IO.stdout
 

src/Dhall/Pretty.hs
@@ -6,6 +6,10 @@ module Dhall.Pretty
       Ann(..)
     , annToAnsiStyle
     , prettyExpr
+
+    , CharacterSet(..)
+    , prettyCharacterSet
+
     , layoutOpts
     ) where
 
@@ -17,4 +21,3 @@ layoutOpts :: Pretty.LayoutOptions
 layoutOpts =
     Pretty.defaultLayoutOptions
         { Pretty.layoutPageWidth = Pretty.AvailablePerLine 80 1.0 }
-

src/Dhall/Repl.hs
@@ -16,6 +16,7 @@ import Control.Monad.State.Strict ( evalStateT )
 import Data.List ( foldl' )
 import Dhall.Binary (ProtocolVersion(..))
 import Dhall.Import (protocolVersion)
+import Dhall.Pretty (CharacterSet(..))
 import Lens.Family (set)
 
 import qualified Control.Monad.Trans.State.Strict as State
@@ -37,8 +38,9 @@ import qualified System.Console.Repline as Repline
 import qualified System.IO
 
 -- | Implementation of the @dhall repl@ subcommand
-repl :: Bool -> ProtocolVersion -> IO ()
-repl explain _protocolVersion = if explain then Dhall.detailed io else io
+repl :: CharacterSet -> Bool -> ProtocolVersion -> IO ()
+repl characterSet explain _protocolVersion =
+    if explain then Dhall.detailed io else io
   where
     io =
       evalStateT
@@ -49,13 +51,14 @@ repl explain _protocolVersion = if explain then Dhall.detailed io else io
         ( Repline.Word completer )
             greeter
         )
-        (emptyEnv { explain, _protocolVersion })
+        (emptyEnv { characterSet, explain, _protocolVersion })
 
 
 data Env = Env
   { envBindings      :: Dhall.Context.Context Binding
   , envIt            :: Maybe Binding
   , explain          :: Bool
+  , characterSet     :: CharacterSet
   , _protocolVersion :: ProtocolVersion
   }
 
@@ -67,6 +70,7 @@ emptyEnv =
     , envIt = Nothing
     , explain = False
     , _protocolVersion = Dhall.Binary.defaultProtocolVersion
+    , characterSet = Unicode
     }
 
 
@@ -220,7 +224,10 @@ saveBinding (file : "=" : tokens) = do
 
   normalizedExpression <- normalize loadedExpression
 
-  let handler handle = output handle normalizedExpression
+  env <- get
+
+  let handler handle =
+          State.evalStateT (output handle normalizedExpression) env
 
   liftIO (System.IO.withFile file System.IO.WriteMode handler)
 saveBinding _ = fail ":save should be of the form `:save x = y`"
@@ -254,12 +261,17 @@ dontCrash m =
 
 
 output
-    :: (Pretty.Pretty a, MonadIO m)
+    :: (Pretty.Pretty a, MonadState Env m, MonadIO m)
     => System.IO.Handle -> Dhall.Expr s a -> m ()
 output handle expr = do
+  Env { characterSet } <- get
+
   liftIO (System.IO.hPutStrLn handle "")  -- Visual spacing
 
-  let stream = Pretty.layoutSmart Dhall.Pretty.layoutOpts (Dhall.Pretty.prettyExpr expr)
+  let stream =
+          Pretty.layoutSmart Dhall.Pretty.layoutOpts
+              (Dhall.Pretty.prettyCharacterSet characterSet expr)
+
   supportsANSI <- liftIO (System.Console.ANSI.hSupportsANSI handle)
   let ansiStream =
           if supportsANSI

tests/format/asciiA.dhall
@@ -1,0 +1,1 @@
+λ(a : Type) → ∀(b : a) → a

commit 068dd46025d550c56d0d6e2f9aa1586b1be1a443

Fix `dhall format --inplace`
07f873244038fcf1fd48db80584b5d577a05fe20 caused `dhall format` to start saving
escape codes to formatted files, which this change fixes

src/Dhall/Format.hs
@@ -36,7 +36,7 @@ format characterSet inplace = do
                     Right x   -> return x
 
                 let doc =   Pretty.pretty header
-                        <>  fmap annToAnsiStyle (Dhall.Pretty.prettyCharacterSet characterSet expr)
+                        <>  Pretty.unAnnotate (Dhall.Pretty.prettyCharacterSet characterSet expr)
                 System.IO.withFile file System.IO.WriteMode (\handle -> do
                     Pretty.renderIO handle (Pretty.layoutSmart layoutOpts doc)
                     Data.Text.IO.hPutStrLn handle "" )