60accef002b265531b97b2a633d706be32e6540a
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-12-13 08:53:34 -0800

    Restrict `try-dhall` build to Linux

--- default.nix
+++ default.nix
@@ -3,8 +3,13 @@ let
 
   shared_ghcjs = import ./nix/shared.nix { compiler = "ghcjs"; };
 
+  shared_ghcjs_linux =
+    import ./nix/shared.nix { compiler = "ghcjs"; system = "x86_64-linux"; };
+
 in
   { inherit (shared) dhall dhall-bash dhall-json dhall-text;
 
-    inherit (shared_ghcjs) dhall-try try-dhall;
+    inherit (shared_ghcjs) dhall-try;
+
+    inherit (shared_ghcjs_linux) try-dhall;
   }
