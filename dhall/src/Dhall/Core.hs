cadd1022742cf2caf84b42ea41ca21bb2b1cdb85
András Kovács <puttamalac@gmail.com>
2019-04-20 20:34:42 +0200

    Remove subExpressions from Core

--- dhall/src/Dhall/Core.hs
+++ dhall/src/Dhall/Core.hs
@@ -52,7 +52,6 @@ module Dhall.Core (
     , internalError
     , reservedIdentifiers
     , escapeText
-    , subExpressions
     , pathCharacter
     ) where
 
@@ -74,7 +73,6 @@ import Data.Sequence (Seq)
 import Data.String (IsString(..))
 import Data.Text (Text)
 import Data.Text.Prettyprint.Doc (Doc, Pretty)
-import Data.Traversable
 import {-# SOURCE #-} Dhall.Pretty.Internal
 import {-# SOURCE #-} Dhall.Binary (ToTerm(..), FromTerm(..))
 import Dhall.Map (Map)
@@ -687,78 +685,6 @@ reservedIdentifiers =
         , "Infinity"
         ]
 
-
--- | A traversal over the immediate sub-expressions of an expression.
-subExpressions :: Applicative f => (Expr s a -> f (Expr s a)) -> Expr s a -> f (Expr s a)
-subExpressions _ (Const c) = pure (Const c)
-subExpressions _ (Var v) = pure (Var v)
-subExpressions f (Lam a b c) = Lam a <$> f b <*> f c
-subExpressions f (Pi a b c) = Pi a <$> f b <*> f c
-subExpressions f (App a b) = App <$> f a <*> f b
-subExpressions f (Let as b) = Let <$> traverse g as <*> f b
-  where
-    g (Binding c d e) = Binding c <$> traverse f d <*> f e
-subExpressions f (Annot a b) = Annot <$> f a <*> f b
-subExpressions _ Bool = pure Bool
-subExpressions _ (BoolLit b) = pure (BoolLit b)
-subExpressions f (BoolAnd a b) = BoolAnd <$> f a <*> f b
-subExpressions f (BoolOr a b) = BoolOr <$> f a <*> f b
-subExpressions f (BoolEQ a b) = BoolEQ <$> f a <*> f b
-subExpressions f (BoolNE a b) = BoolNE <$> f a <*> f b
-subExpressions f (BoolIf a b c) = BoolIf <$> f a <*> f b <*> f c
-subExpressions _ Natural = pure Natural
-subExpressions _ (NaturalLit n) = pure (NaturalLit n)
-subExpressions _ NaturalFold = pure NaturalFold
-subExpressions _ NaturalBuild = pure NaturalBuild
-subExpressions _ NaturalIsZero = pure NaturalIsZero
-subExpressions _ NaturalEven = pure NaturalEven
-subExpressions _ NaturalOdd = pure NaturalOdd
-subExpressions _ NaturalToInteger = pure NaturalToInteger
-subExpressions _ NaturalShow = pure NaturalShow
-subExpressions f (NaturalPlus a b) = NaturalPlus <$> f a <*> f b
-subExpressions f (NaturalTimes a b) = NaturalTimes <$> f a <*> f b
-subExpressions _ Integer = pure Integer
-subExpressions _ (IntegerLit n) = pure (IntegerLit n)
-subExpressions _ IntegerShow = pure IntegerShow
-subExpressions _ IntegerToDouble = pure IntegerToDouble
-subExpressions _ Double = pure Double
-subExpressions _ (DoubleLit n) = pure (DoubleLit n)
-subExpressions _ DoubleShow = pure DoubleShow
-subExpressions _ Text = pure Text
-subExpressions f (TextLit (Chunks a b)) =
-    TextLit <$> (Chunks <$> traverse (traverse f) a <*> pure b)
-subExpressions f (TextAppend a b) = TextAppend <$> f a <*> f b
-subExpressions _ TextShow = pure TextShow
-subExpressions _ List = pure List
-subExpressions f (ListLit a b) = ListLit <$> traverse f a <*> traverse f b
-subExpressions f (ListAppend a b) = ListAppend <$> f a <*> f b
-subExpressions _ ListBuild = pure ListBuild
-subExpressions _ ListFold = pure ListFold
-subExpressions _ ListLength = pure ListLength
-subExpressions _ ListHead = pure ListHead
-subExpressions _ ListLast = pure ListLast
-subExpressions _ ListIndexed = pure ListIndexed
-subExpressions _ ListReverse = pure ListReverse
-subExpressions _ Optional = pure Optional
-subExpressions f (OptionalLit a b) = OptionalLit <$> f a <*> traverse f b
-subExpressions f (Some a) = Some <$> f a
-subExpressions _ None = pure None
-subExpressions _ OptionalFold = pure OptionalFold
-subExpressions _ OptionalBuild = pure OptionalBuild
-subExpressions f (Record a) = Record <$> traverse f a
-subExpressions f ( RecordLit a ) = RecordLit <$> traverse f a
-subExpressions f (Union a) = Union <$> traverse (traverse f) a
-subExpressions f (UnionLit a b c) =
-    UnionLit a <$> f b <*> traverse (traverse f) c
-subExpressions f (Combine a b) = Combine <$> f a <*> f b
-subExpressions f (CombineTypes a b) = CombineTypes <$> f a <*> f b
-subExpressions f (Prefer a b) = Prefer <$> f a <*> f b
-subExpressions f (Merge a b t) = Merge <$> f a <*> f b <*> traverse f t
-subExpressions f (Field a b) = Field <$> f a <*> pure b
-subExpressions f (Project a b) = Project <$> f a <*> pure b
-subExpressions f (Note a b) = Note a <$> f b
-subExpressions f (ImportAlt l r) = ImportAlt <$> f l <*> f r
-subExpressions _ (Embed a) = pure (Embed a)
 
 {-| Returns `True` if the given `Char` is valid within an unquoted path
     component
--- dhall/src/Dhall/Lint.hs
+++ dhall/src/Dhall/Lint.hs
@@ -12,7 +12,7 @@ module Dhall.Lint
 import Control.Monad (mplus)
 import Data.List.NonEmpty (NonEmpty(..))
 import Data.Semigroup ((<>))
-import Dhall.Core (Binding(..), Expr(..), Import, Var(..), subExpressions)
+import Dhall.Core (Binding(..), Expr(..), Import, Var(..), Chunks(..))
 import Lens.Family (ASetter, over)
 import Unsafe.Coerce (unsafeCoerce)
 
@@ -73,3 +73,75 @@ rewriteOf l f = go where go = transformOf l (\x -> maybe x go (f x))
 
 transformOf :: ASetter a b a b -> (b -> b) -> a -> b
 transformOf l f = go where go = f . over l go
+
+-- | A traversal over the immediate sub-expressions of an expression.
+subExpressions :: Applicative f => (Expr s a -> f (Expr s a)) -> Expr s a -> f (Expr s a)
+subExpressions _ (Const c) = pure (Const c)
+subExpressions _ (Var v) = pure (Var v)
+subExpressions f (Lam a b c) = Lam a <$> f b <*> f c
+subExpressions f (Pi a b c) = Pi a <$> f b <*> f c
+subExpressions f (App a b) = App <$> f a <*> f b
+subExpressions f (Let as b) = Let <$> traverse g as <*> f b
+  where
+    g (Binding c d e) = Binding c <$> traverse f d <*> f e
+subExpressions f (Annot a b) = Annot <$> f a <*> f b
+subExpressions _ Bool = pure Bool
+subExpressions _ (BoolLit b) = pure (BoolLit b)
+subExpressions f (BoolAnd a b) = BoolAnd <$> f a <*> f b
+subExpressions f (BoolOr a b) = BoolOr <$> f a <*> f b
+subExpressions f (BoolEQ a b) = BoolEQ <$> f a <*> f b
+subExpressions f (BoolNE a b) = BoolNE <$> f a <*> f b
+subExpressions f (BoolIf a b c) = BoolIf <$> f a <*> f b <*> f c
+subExpressions _ Natural = pure Natural
+subExpressions _ (NaturalLit n) = pure (NaturalLit n)
+subExpressions _ NaturalFold = pure NaturalFold
+subExpressions _ NaturalBuild = pure NaturalBuild
+subExpressions _ NaturalIsZero = pure NaturalIsZero
+subExpressions _ NaturalEven = pure NaturalEven
+subExpressions _ NaturalOdd = pure NaturalOdd
+subExpressions _ NaturalToInteger = pure NaturalToInteger
+subExpressions _ NaturalShow = pure NaturalShow
+subExpressions f (NaturalPlus a b) = NaturalPlus <$> f a <*> f b
+subExpressions f (NaturalTimes a b) = NaturalTimes <$> f a <*> f b
+subExpressions _ Integer = pure Integer
+subExpressions _ (IntegerLit n) = pure (IntegerLit n)
+subExpressions _ IntegerShow = pure IntegerShow
+subExpressions _ IntegerToDouble = pure IntegerToDouble
+subExpressions _ Double = pure Double
+subExpressions _ (DoubleLit n) = pure (DoubleLit n)
+subExpressions _ DoubleShow = pure DoubleShow
+subExpressions _ Text = pure Text
+subExpressions f (TextLit (Chunks a b)) =
+    TextLit <$> (Chunks <$> traverse (traverse f) a <*> pure b)
+subExpressions f (TextAppend a b) = TextAppend <$> f a <*> f b
+subExpressions _ TextShow = pure TextShow
+subExpressions _ List = pure List
+subExpressions f (ListLit a b) = ListLit <$> traverse f a <*> traverse f b
+subExpressions f (ListAppend a b) = ListAppend <$> f a <*> f b
+subExpressions _ ListBuild = pure ListBuild
+subExpressions _ ListFold = pure ListFold
+subExpressions _ ListLength = pure ListLength
+subExpressions _ ListHead = pure ListHead
+subExpressions _ ListLast = pure ListLast
+subExpressions _ ListIndexed = pure ListIndexed
+subExpressions _ ListReverse = pure ListReverse
+subExpressions _ Optional = pure Optional
+subExpressions f (OptionalLit a b) = OptionalLit <$> f a <*> traverse f b
+subExpressions f (Some a) = Some <$> f a
+subExpressions _ None = pure None
+subExpressions _ OptionalFold = pure OptionalFold
+subExpressions _ OptionalBuild = pure OptionalBuild
+subExpressions f (Record a) = Record <$> traverse f a
+subExpressions f ( RecordLit a ) = RecordLit <$> traverse f a
+subExpressions f (Union a) = Union <$> traverse (traverse f) a
+subExpressions f (UnionLit a b c) =
+    UnionLit a <$> f b <*> traverse (traverse f) c
+subExpressions f (Combine a b) = Combine <$> f a <*> f b
+subExpressions f (CombineTypes a b) = CombineTypes <$> f a <*> f b
+subExpressions f (Prefer a b) = Prefer <$> f a <*> f b
+subExpressions f (Merge a b t) = Merge <$> f a <*> f b <*> traverse f t
+subExpressions f (Field a b) = Field <$> f a <*> pure b
+subExpressions f (Project a b) = Project <$> f a <*> pure b
+subExpressions f (Note a b) = Note a <$> f b
+subExpressions f (ImportAlt l r) = ImportAlt <$> f l <*> f r
+subExpressions _ (Embed a) = pure (Embed a)
