0c3cf6829676b5e4b4bd4edf87205d1f96c26a06
Gabriel Gonzalez <Gabriel439@gmail.com>
2019-05-03 22:17:20 -0700

    Fix marshalling of unions (including enums) in and out of Dhall
    Fixes https://github.com/dhall-lang/dhall-haskell/issues/930

--- dhall/tests/Dhall/Test/Dhall.hs
+++ dhall/tests/Dhall/Test/Dhall.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+
 module Dhall.Test.Dhall where
 
 import Control.Exception (SomeException, try)
+import Data.Text (Text)
+import Dhall (Inject, Interpret)
 import GHC.Generics (Generic)
 import Numeric.Natural (Natural)
 import Test.Tasty
@@ -12,7 +16,9 @@ import Test.Tasty.HUnit
 
 import qualified Dhall
 import qualified Dhall.Core
+import qualified Dhall.Import
 import qualified Dhall.Map
+import qualified Dhall.Parser
 
 tests :: TestTree
 tests =
@@ -20,6 +26,7 @@ tests =
      [ shouldShowDetailedTypeError
      , shouldHandleBothUnionLiterals
      , shouldHaveWorkingGenericAuto
+     , shouldHandleUnionsCorrectly
      ]
 
 data MyType = MyType { foo :: String , bar :: Natural }
@@ -61,14 +68,15 @@ shouldShowDetailedTypeError = testCase "detailed TypeError" $ do
     Left ex -> assertEqual assertMsg expectedMsg (show ex)
     Right _ -> fail "The extraction using a wrong type succeded"
 
-license :: Dhall.Type ()
-license = Dhall.union (Dhall.constructor "AllRightsReserved" Dhall.unit)
-
 -- https://github.com/dhall-lang/dhall-haskell/issues/915
 shouldHandleBothUnionLiterals :: TestTree
 shouldHandleBothUnionLiterals = testCase "Marshal union literals" $ do
-    _ <- Dhall.input license "< AllRightsReserved : {} >.AllRightsReserved {=}"
-    _ <- Dhall.input license "< AllRightsReserved = {=} >"
+    let example :: Dhall.Type Bool
+        example = Dhall.union (Dhall.constructor "Test" Dhall.bool)
+
+    _ <- Dhall.input example "< Test : Bool >.Test True"
+    _ <- Dhall.input example "< Test = True >"
+
     return ()
 
 data CompilerFlavor3 =
@@ -83,10 +91,89 @@ data CompilerFlavor2 =
 shouldHaveWorkingGenericAuto :: TestTree
 shouldHaveWorkingGenericAuto = testGroup "genericAuto"
   [ testCase "works for a three-constructor enum" $ do
-      compiler <- Dhall.input Dhall.genericAuto "< GHC3 : {} | GHCJS3 : {} | Helium3 : {} >.GHC3 {=}"
+      compiler <- Dhall.input Dhall.genericAuto "< GHC3 | GHCJS3 | Helium3 >.GHC3"
       assertEqual "genericAuto didn't give us what we wanted" GHC3 compiler
 
     , testCase "works for a two-constructor enum" $ do
-      compiler <- Dhall.input Dhall.genericAuto "< GHC2 : {} | GHCJS2 : {} >.GHC2 {=}"
+      compiler <- Dhall.input Dhall.genericAuto "< GHC2 | GHCJS2 >.GHC2"
       assertEqual "genericAuto didn't give us what we wanted" GHC2 compiler
   ]
+
+data NonEmptyUnion = N0 Bool | N1 Natural | N2 Text
+    deriving (Eq, Generic, Inject, Interpret, Show)
+
+data Enum = E0 | E1 | E2
+    deriving (Eq, Generic, Inject, Interpret, Show)
+
+data Mixed = M0 Bool | M1 | M2
+    deriving (Eq, Generic, Inject, Interpret, Show)
+
+shouldHandleUnionsCorrectly :: TestTree
+shouldHandleUnionsCorrectly =
+  testGroup "Handle union literals"
+    [ "λ(x : < N0 : { _1 : Bool } | N1 : { _1 : Natural } | N2 : { _1 : Text } >) → x"
+        `shouldPassThrough` [ N0 True, N1 5, N2 "ABC" ]
+    , "λ(x : < E0 | E1 | E2 >) → x"
+        `shouldPassThrough` [ E0, E1, E2 ]
+    , "λ(x : < M0 : { _1 : Bool } | M1 | M2 >) → x"
+        `shouldPassThrough` [ M0 True, M1, M2 ]
+
+    , "(< N0 : { _1 : Bool } | N1 : { _1 : Natural } | N2 : { _1 : Text } >).N0 { _1 = True }"
+        `shouldMarshalInto` N0 True
+    , "(< N0 : { _1 : Bool } | N1 : { _1 : Natural } | N2 : { _1 : Text } >).N1 { _1 = 5 }"
+        `shouldMarshalInto` N1 5
+    , "(< N0 : { _1 : Bool } | N1 : { _1 : Natural } | N2 : { _1 : Text } >).N2 { _1 = \"ABC\" }"
+        `shouldMarshalInto` N2 "ABC"
+
+    , "< N0 = { _1 = True } | N1 : { _1 : Natural } | N2 : { _1 : Text } >"
+        `shouldMarshalInto` N0 True
+    , "< N0 : { _1 : Bool } | N1 = { _1 = 5 } | N2 : { _1 : Text } >"
+        `shouldMarshalInto` N1 5
+    , "< N0 : { _1 : Bool } | N1 : { _1 : Natural } | N2 = { _1 = \"ABC\" } >"
+        `shouldMarshalInto` N2 "ABC"
+
+    , "(< E0 | E1 | E2>).E0" `shouldMarshalInto` E0
+    , "(< E0 | E1 | E2>).E1" `shouldMarshalInto` E1
+    , "(< E0 | E1 | E2>).E2" `shouldMarshalInto` E2
+
+    , "(< M0 : { _1 : Bool } | M1 | M2 >).M0 { _1 = True }"
+        `shouldMarshalInto` M0 True
+    , "(< M0 : { _1 : Bool } | M1 | M2 >).M1"
+        `shouldMarshalInto` M1
+    , "(< M0 : { _1 : Bool } | M1 | M2 >).M2"
+        `shouldMarshalInto` M2
+
+    , "< M0 = { _1 = True } | M1 | M2 >"
+        `shouldMarshalInto` M0 True
+
+    , N0 True
+        `shouldInjectInto` "(< N0 : { _1 : Bool } | N1 : { _1 : Natural } | N2 : { _1 : Text } >).N0 { _1 = True }"
+    , N1 5
+        `shouldInjectInto` "(< N0 : { _1 : Bool } | N1 : { _1 : Natural } | N2 : { _1 : Text } >).N1 { _1 = 5 }"
+    , N2 "ABC"
+        `shouldInjectInto` "(< N0 : { _1 : Bool } | N1 : { _1 : Natural } | N2 : { _1 : Text } >).N2 { _1 = \"ABC\" }"
+
+    , E0 `shouldInjectInto` "< E0 | E1 | E2 >.E0"
+    , E1 `shouldInjectInto` "< E0 | E1 | E2 >.E1"
+    , E2 `shouldInjectInto` "< E0 | E1 | E2 >.E2"
+
+    , M0 True `shouldInjectInto` "(< M0 : { _1 : Bool } | M1 | M2 >).M0 { _1 = True }"
+    , M1 `shouldInjectInto` "(< M0 : { _1 : Bool } | M1 | M2 >).M1"
+    , M2 `shouldInjectInto` "(< M0 : { _1 : Bool } | M1 | M2 >).M2"
+    ]
+  where
+    code `shouldPassThrough` values = testCase "Pass through" $ do
+        f <- Dhall.input Dhall.auto code
+
+        values @=? map f values
+
+    code `shouldMarshalInto` expectedValue = testCase "Marshal" $ do
+        actualValue <- Dhall.input Dhall.auto code
+        expectedValue @=? actualValue
+
+    value `shouldInjectInto` expectedCode = testCase "Inject" $ do
+        parsedExpression <- Dhall.Core.throws (Dhall.Parser.exprFromText "(test)" expectedCode)
+
+        resolvedExpression <- Dhall.Import.assertNoImports parsedExpression
+
+        Dhall.Core.denote resolvedExpression @=? Dhall.embed Dhall.inject value
