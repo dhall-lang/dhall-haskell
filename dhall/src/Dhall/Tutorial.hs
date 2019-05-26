d98364ef93fffafb1af35fe6db76043ca8a18432
Valery V. Vorotyntsev <valery.vv@gmail.com>
2019-05-26 13:01:42 +0300

    Dhall.Tutorial: fix shell output formatting
    Fixes #971

--- dhall/src/Dhall/Tutorial.hs
+++ dhall/src/Dhall/Tutorial.hs
@@ -1532,20 +1532,16 @@ import Dhall
 -- or you changed some of the imports and want to update the hashes you can use the
 -- freeze command to either add or update hashes:
 --
--- > cat foo.dhall
--- ''
--- let replicate =
---       https://raw.githubusercontent.com/dhall-lang/Prelude/c79c2bc3c46f129cc5b6d594ce298a381bcae92c/List/replicate
---
--- in  replicate 5
--- ''
--- > dhall freeze --inplace ./foo.dhall
--- > cat ./foo.dhall
--- ''
--- let replicate =
---       https://raw.githubusercontent.com/dhall-lang/Prelude/c79c2bc3c46f129cc5b6d594ce298a381bcae92c/List/replicate sha256:b0e3ec1797b32c80c0bcb7e8254b08c7e9e35e75e6b410c7ac21477ab90167ad 
--- in  replicate 5
--- ''
+-- > $ cat foo.dhall
+-- > let replicate =
+-- >       https://raw.githubusercontent.com/dhall-lang/Prelude/c79c2bc3c46f129cc5b6d594ce298a381bcae92c/List/replicate
+-- > in  replicate 5
+-- > $
+-- > $ dhall freeze --inplace ./foo.dhall
+-- > $ cat ./foo.dhall
+-- > let replicate =
+-- >       https://raw.githubusercontent.com/dhall-lang/Prelude/c79c2bc3c46f129cc5b6d594ce298a381bcae92c/List/replicate sha256:b0e3ec1797b32c80c0bcb7e8254b08c7e9e35e75e6b410c7ac21477ab90167ad
+-- > in  replicate 5
 --
 -- $rawText
 --
