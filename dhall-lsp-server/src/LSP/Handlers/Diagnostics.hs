49d29d685f39857b621d3ad36efeb7667fbe0a7e
Frederik Ramcke <folkmarfr@web.de>
2019-06-04 16:48:06 +0000

    dhall-lsp-server: Rework diagnostics backend + detailed error messages on hover (#982)
    * Refactor Diagnostics backend
    
    This commits refactors Backend.Dhall.Diagnostics, cleaning things up and
    adding comments along the way. We also lose Backend.Dhall.DhallErrors.
    
    The user-facing result is a step backwards, as we revert to using Dhall's
    Show instances to print errors, which gives us error messages that don't
    fit the LSP use case very well at the moment. In the future
    we should change Dhall's error printing API (the Show instances) to make
    them more generally useful (in particular for the LSP use case), thus
    saving us from having to duplicate code (as was previously did, when
    dhall-lsp-server was not yet part of the upstream project).
    
    * Proof of concept: Provide detailed error messages
    
    This proof of concept provides detailed explanations on hovering over type
    errors. Currently this is done by injecting the explanation directly into
    the hover text, which 1. conflicts with the short error messages, which
    are still being displayed at the same time and 2. is not particularly
    readable, since the explanations are meant to be rendered at least 80
    characters wide.
    
    For future work I am planning to instead only add an "explain" link to the
    hover window, which when clicked opens the explanation in a separate
    window (inspired by how the haskell ide engine presents documentation
    links). This will require a small amount of client-side logic.
    
    * Explain error messages via VSCode Command URIs
    
    The first proof of concept simply spit out the explanations as hover text;
    in order to make the feature a bit more useful, we now encode the
    explanation text into a VSCode command URI (that still needs to be
    implemented client-side), so that upon hovering over an error the user is
    presented with a clickable "Explain error message" link.
    
    The feature is still in the prototype stage!
    
    * Use custom URI scheme instead of command URIs
    
    Use URIs of the form "dhall-explain:?text" to present detailed
    explanations on hovering. Previously we used command URIs, which are
    specific to VSCode.
    
    Needs a correspondingly patched vscode-lsp-server to handle the
    "dhall-explain" URI scheme.
    
    * Fix handling of relative imports in diagnostics
    
    In refactoring the diagnostics backend I forgot to initialise the settings
    passed to the Dhall type-checker, which are needed to properly resolve
    relative imports. An easy fix for a silly mistake ;)
    
    * Remove outdated TODO comment
    
    * Make GHC stricter when building dhall-lsp-server
    
    Add -Wall and -fwarn-incomplete-uni-patterns to the GHC options when
    building dhall-lsp-server. This brings it in line with the rest of
    dhall-haskell.
    
    * Fix warnings when building dhall-lsp-server
    
    * Fix review comments
    
    Fixes Gabriel's PR comments. Note that I leave a proper fix to
    DhallException for the future (I want to work on something else for a bit
    ;)).
    
    * Use underscores to pacify warnings

--- dhall-lsp-server/src/LSP/Handlers/Diagnostics.hs
+++ dhall-lsp-server/src/LSP/Handlers/Diagnostics.hs
@@ -3,25 +3,26 @@ module LSP.Handlers.Diagnostics( sendEmptyDiagnostics
                                , sendDiagnostics
                                ) where
 
-import           Language.Haskell.LSP.Messages
+-- import           Language.Haskell.LSP.Messages
 import           Language.Haskell.LSP.Diagnostics
-import qualified Language.Haskell.LSP.Control as LSP.Control
+-- import qualified Language.Haskell.LSP.Control as LSP.Control
 import qualified Language.Haskell.LSP.Core as LSP.Core
 import qualified Language.Haskell.LSP.Types as LSP.Types
 import qualified Language.Haskell.LSP.Utility  as LSP.Utility
 
 
-import qualified Data.Aeson                            as J
-import qualified Language.Haskell.LSP.Types            as J
-import qualified Language.Haskell.LSP.Types.Lens       as J
+import qualified Language.Haskell.LSP.Types            as LSP
+-- import qualified Language.Haskell.LSP.Types.Lens       as LSP
 
-import qualified System.IO.Unsafe
+-- import qualified System.IO.Unsafe
 import qualified Data.Text.IO
 import qualified Data.SortedList
 import qualified Data.Map.Strict as Map
 import Control.Monad.Reader (ReaderT)
 import Control.Monad.Reader.Class (ask)
 import Control.Monad.Trans (lift, liftIO)
+
+import Data.Text (Text)
 
 import Backend.Dhall.Diagnostics
 
@@ -30,27 +31,47 @@ import Backend.Dhall.Diagnostics
 -- TODO: Make max number of errors parameter configurable (not rly relevant since we got 1, but still) 
 -- ---------------------------------------------------------------------
 -- Y no method to flush particular source errors?
-sendEmptyDiagnostics ::  J.Uri -> Maybe Int -> ReaderT (LSP.Core.LspFuncs ()) IO ()
+sendEmptyDiagnostics ::  LSP.Uri -> Maybe Int -> ReaderT (LSP.Core.LspFuncs ()) IO ()
 sendEmptyDiagnostics fileUri version =
   publishDiagnostics 10 fileUri version defaultDiagnosticBySource
 
 defaultDiagnosticBySource :: DiagnosticsBySource
-defaultDiagnosticBySource = Map.singleton (Just defaultDiagnosticSource) (Data.SortedList.toSortedList [])
+defaultDiagnosticBySource = Map.singleton (Just "Dhall") (Data.SortedList.toSortedList [])
 
+diagnosisToLSP :: Diagnosis -> LSP.Diagnostic
+diagnosisToLSP Diagnosis{..} = LSP.Diagnostic {..}
+  where
+    _range = case range of
+      Just (Range (line1, col1) (line2, col2)) ->
+        LSP.Range (LSP.Position line1 col1) (LSP.Position line2 col2)
+      Nothing -> LSP.Range (LSP.Position 0 0) (LSP.Position 0 0)
+    _severity = Just LSP.DsError
+    -- TODO use @doctor@ instead. Seems incompatible with LSP.Core's API...
+    _source = Just "Dhall"
+    _code = Nothing
+    _message = diagnosis
+    _relatedInformation = Nothing
+
+
+compilerDiagnostics :: FilePath -> Text -> IO [LSP.Diagnostic]
+compilerDiagnostics path txt = do
+  errors <- runDhall path txt
+  let diagnoses = concatMap (diagnose txt) errors
+  return (map diagnosisToLSP diagnoses)
+  
 -- | Analyze the file and send any diagnostics to the client in a
 -- "textDocument/publishDiagnostics" notification
-
-sendDiagnostics :: J.Uri -> Maybe Int ->  ReaderT (LSP.Core.LspFuncs ()) IO ()
+sendDiagnostics :: LSP.Uri -> Maybe Int ->  ReaderT (LSP.Core.LspFuncs ()) IO ()
 sendDiagnostics fileUri version = do
   let
-   filePath = maybe (error "can't convert uri to file path") id $ J.uriToFilePath fileUri -- !FIXME: handle non-file uris
+   filePath = maybe (error "can't convert uri to file path") id $ LSP.uriToFilePath fileUri -- !FIXME: handle non-file uris
   txt <- lift $ Data.Text.IO.readFile filePath
   diags' <- lift $ compilerDiagnostics filePath txt
   lift $ LSP.Utility.logs $ "diagnostic: " <> show diags'
   publishDiagnostics 10 fileUri version (Map.union (partitionBySource diags') defaultDiagnosticBySource)
 
 
-publishDiagnostics :: Int -> J.Uri -> J.TextDocumentVersion -> DiagnosticsBySource -> ReaderT (LSP.Core.LspFuncs ()) IO ()
+publishDiagnostics :: Int -> LSP.Uri -> LSP.TextDocumentVersion -> DiagnosticsBySource -> ReaderT (LSP.Core.LspFuncs ()) IO ()
 publishDiagnostics maxToPublish uri v diags = do
   lf <- ask
   liftIO $ (LSP.Core.publishDiagnosticsFunc lf) maxToPublish uri v diags
