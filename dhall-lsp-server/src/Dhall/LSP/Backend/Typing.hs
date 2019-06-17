e1528c8f8fa984be9a75c427279c5b378f1c95f0
Frederik Ramcke <folkmar.ramcke@student.chalmers.se>
2019-06-17 11:45:43 +0200

    Simplify typeAt' by assuming well-typedness

--- dhall-lsp-server/src/Dhall/LSP/Backend/Typing.hs
+++ dhall-lsp-server/src/Dhall/LSP/Backend/Typing.hs
@@ -11,7 +11,8 @@ import qualified Data.Text as Text
 
 import Dhall.LSP.Backend.Diagnostics (Position, positionFromMegaparsec, offsetToPosition)
 
--- | Find the type of the subexpression at the given position.
+-- | Find the type of the subexpression at the given position. Assumes that the
+--   input expression is well-typed.
 typeAt :: Position -> Expr Src X -> Maybe (Expr Src X)
 typeAt pos expr = case typeAt' pos empty expr of
   Right typ -> Just typ
@@ -24,23 +25,19 @@ typeAt' pos ctx (Note src (Let (b :| (b' : bs)) e)) =
   typeAt' pos ctx (Note src (Let (b :| []) (Note src (Let (b' :| bs) e))))
 
 -- only handle singleton lets explicitly
--- we don't check the optional annotation for consistency
 typeAt' pos ctx (Let (Binding x _ a :| []) (Note src e)) | pos `inside` src = do
   _A <- typeWithA absurd ctx a
   let ctx' = fmap (shift 1 (V x 0)) (insert x _A ctx)
   typeAt' pos ctx' e
 
 typeAt' pos ctx (Lam x _A (Note src b)) | pos `inside` src = do
-  _A' <- do _ <- typeWithA absurd ctx _A
-            return (Dhall.Core.normalize _A)
-  let ctx' = fmap (shift 1 (V x 0)) (insert x _A' ctx)
+  let _A' = Dhall.Core.normalize _A
+      ctx' = fmap (shift 1 (V x 0)) (insert x _A' ctx)
   typeAt' pos ctx' b
 
--- we don't check that the function type is itself well-formed
 typeAt' pos ctx (Pi x _A  (Note src _B)) | pos `inside` src = do
-  _A' <- do _ <- typeWithA absurd ctx _A
-            return (Dhall.Core.normalize _A)
-  let ctx' = fmap (shift 1 (V x 0)) (insert x _A' ctx)
+  let _A' = Dhall.Core.normalize _A
+      ctx' = fmap (shift 1 (V x 0)) (insert x _A' ctx)
   typeAt' pos ctx' _B
 
 -- need to catch Notes since the catch-all would remove two layers at once
@@ -50,8 +47,8 @@ typeAt' pos ctx (Note _ expr) = typeAt' pos ctx expr
 typeAt' pos ctx expr = do
   let subExprs = toListOf subExpressions expr
   case [ e | (Note src e) <- subExprs, pos `inside` src ] of
-    [] -> typeWithA absurd ctx expr
-    (t:_) -> typeAt' pos ctx t
+    [] -> typeWithA absurd ctx expr  -- return type of whole subexpression
+    (t:_) -> typeAt' pos ctx t  -- continue with leaf-expression
 
 
 -- | Find the smallest Src annotation containing the given position.
