962cc3a7a9f3346a876fa18ba91a405ba9fc1585
Simon Jakobi <simon.jakobi@gmail.com>
2019-06-27 06:54:20 +0200

    Remove an unnecessary 'void'

--- dhall/src/Dhall/Map.hs
+++ dhall/src/Dhall/Map.hs
@@ -476,7 +476,7 @@ traverseWithKey_ f m = Data.Functor.void (traverseWithKey f m)
 -}
 unorderedTraverseWithKey_
     :: Ord k => Applicative f => (k -> a -> f ()) -> Map k a -> f ()
-unorderedTraverseWithKey_ f = Data.Functor.void . traverse_ (uncurry f) . toList
+unorderedTraverseWithKey_ f = traverse_ (uncurry f) . toList
 {-# INLINABLE unorderedTraverseWithKey_ #-}
 
 {-| Convert a `Map` to a list of key-value pairs in the original order of keys
