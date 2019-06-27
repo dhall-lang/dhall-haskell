c193fe57ca898b6807369989daca618e42d61809
Frederik Ramcke <folkmar.ramcke@student.chalmers.se>
2019-06-27 13:20:54 +0200

    Refactor Dhall.LSP.Backend.Linting

--- dhall-lsp-server/src/Dhall/LSP/Backend/Linting.hs
+++ dhall-lsp-server/src/Dhall/LSP/Backend/Linting.hs
@@ -1,15 +1,14 @@
 module Dhall.LSP.Backend.Linting
-  ( suggest
-  , Suggestion(..)
-  , lintAndFormatDocument
+  ( Suggestion(..)
+  , suggest
+  , lint
   )
 where
 
-import Dhall.Parser (Src, ParseError, exprFromText, exprAndHeaderFromText)
-import Dhall.Core (Expr(..), Binding(..), Var(..), subExpressions, freeIn)
-import Dhall.Lint (lint)
+import Dhall.Parser (Src)
+import Dhall.Core (Expr(..), Binding(..), Var(..), subExpressions, freeIn, Import)
+import qualified Dhall.Lint as Dhall
 
-import Dhall.LSP.Backend.Formatting
 import Dhall.LSP.Backend.Diagnostics
 
 import Data.Text (Text)
@@ -21,13 +20,13 @@ data Suggestion = Suggestion {
     suggestion :: Text
     }
 
--- Diagnose nested Let blocks.
+-- Diagnose nested let blocks.
 diagLetInLet :: Expr Src a -> [Suggestion]
 diagLetInLet (Note _ (Let _ (Note src (Let _ _)))) =
   [Suggestion (rangeFromDhall src) "Superfluous 'in' before nested let binding"]
 diagLetInLet _ = []
 
--- Given a (noted) Let block compute all unused variables in the block.
+-- Given a (noted) let block compute all unused variables in the block.
 unusedBindings :: Eq a => Expr s a -> [Text]
 unusedBindings (Note _ (Let bindings d)) = concatMap
   (\case
@@ -37,7 +36,7 @@ unusedBindings (Note _ (Let bindings d)) = concatMap
   (toList $ tails bindings)
 unusedBindings _ = []
 
--- Diagnose unused Let bindings.
+-- Diagnose unused let bindings.
 diagUnusedBinding :: Eq a => Expr Src a -> [Suggestion]
 diagUnusedBinding e@(Note src (Let _ _)) = map
   (\var ->
@@ -47,14 +46,11 @@ diagUnusedBinding _ = []
 
 -- | Given an dhall expression suggest all the possible improvements that would
 --   be made by the linter.
-suggest :: Text -> [Suggestion]
-suggest txt = case exprFromText "" txt of
-  Right expr -> concat [ diagLetInLet e ++ diagUnusedBinding e
-                       | e <- universeOf subExpressions expr ]
-  _ -> []
+suggest :: Expr Src Import -> [Suggestion]
+suggest expr = concat [ diagLetInLet e ++ diagUnusedBinding e
+                      | e <- universeOf subExpressions expr ]
 
-lintAndFormatDocument :: Text -> Either ParseError Text
-lintAndFormatDocument text = do
-    (header, expr) <- exprAndHeaderFromText "" text
-    let expr' = lint expr
-    pure (formatExpr header expr')
+-- | Lint the given Dhall expression. Removes unused let bindings and combines
+--   nested lets.
+lint :: Expr Src Import -> Expr Src Import
+lint = Dhall.lint
