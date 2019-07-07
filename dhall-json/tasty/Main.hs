1a0afef5608669b39eeab136fbe55395d0d7c9f6
Gabriel Gonzalez <Gabriel439@gmail.com>
2019-07-06 17:12:40 -0700

    Handle empty alternatives when converting from JSON
    Fixes https://github.com/dhall-lang/dhall-haskell/issues/1074
    
    `dhall-to-{json,yaml}` treat empty alternatives as strings of the
    same name, but `{json,yaml}-to-dhall` were missing the corresponding
    logic to decode strings to empty alternatives, which this change
    fixes.
    
    This also ensures that the conversion logic contains no more
    `undefined`s, by making the union conversion branch total.

--- dhall-json/tasty/Main.hs
+++ dhall-json/tasty/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE RecordWildCards   #-}
 
 module Main where
@@ -11,8 +12,10 @@ import qualified Data.Aeson
 import qualified Data.ByteString.Lazy
 import qualified Data.Text
 import qualified Data.Text.IO
+import qualified Dhall.Core           as Core
 import qualified Dhall.Import
 import qualified Dhall.JSON
+import qualified Dhall.JSONToDhall    as JSONToDhall
 import qualified Dhall.Parser
 import qualified Dhall.Yaml
 import qualified Test.Tasty
@@ -27,6 +30,7 @@ testTree =
         [ issue48
         , yamlQuotedStrings
         , yaml
+        , emptyAlternative
         ]
 
 issue48 :: TestTree
@@ -107,3 +111,17 @@ yaml = Test.Tasty.HUnit.testCase "Yaml: normal string style" assertion
                 "Conversion to normal yaml did not generate the expected output"
 
         Test.Tasty.HUnit.assertEqual message expectedValue actualValue
+
+emptyAlternative :: TestTree
+emptyAlternative = Test.Tasty.HUnit.testCase "Empty alternative" $ do
+    let schema = Core.Union [ ("Bar", Nothing), ("Foo", Nothing) ]
+
+    let json = Data.Aeson.String "Foo"
+
+    let expectedResult = Core.Field schema "Foo"
+
+    actualResult <- Core.throws (JSONToDhall.dhallFromJSON JSONToDhall.defaultConversion schema json)
+
+    let message = "Empty alternatives were not decoded from JSON correctly"
+
+    Test.Tasty.HUnit.assertEqual message expectedResult actualResult
