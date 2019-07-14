6b3cc641b74db93a7cbaef284564af08915f0902
Gabriel Gonzalez <Gabriel439@gmail.com>
2019-07-13 18:17:38 -0700

    s/expression/YAML expression/
    ... as caught by @sjakobi
    
    Co-Authored-By: Simon Jakobi <simon.jakobi@gmail.com>

--- dhall-json/yaml-to-dhall/Main.hs
+++ dhall-json/yaml-to-dhall/Main.hs
@@ -72,7 +72,7 @@ parseOptions = CommandOptions <$> parseVersion
     parseFile =
         Options.strOption
             (   Options.long "file"
-            <>  Options.help "Read expression from a file instead of standard input"
+            <>  Options.help "Read YAML expression from a file instead of standard input"
             <>  Options.metavar "FILE"
             )
 
