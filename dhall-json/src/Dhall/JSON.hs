7d8ba636eccdbe3ae6bf458c22a22af66a8eb135
Simon Jakobi <simon.jakobi@gmail.com>
2019-07-30 19:38:58 +0200

    Update haddocks in Dhall.JSON

--- dhall-json/src/Dhall/JSON.hs
+++ dhall-json/src/Dhall/JSON.hs
@@ -72,22 +72,17 @@
 
     Dhall unions translate to the wrapped value:
 
-> $ dhall-to-json <<< "< Left = +2 | Right : Natural>"
+> $ dhall-to-json <<< "< Left : Natural | Right : Natural>.Left 2"
 > 2
 > $ cat config
-> [ < Person = { age = 47, name = "John" } -- TODO
->   | Place  : { location : Text }
->   >
-> , < Place  = { location = "North Pole" }
->   | Person : { age : Natural, name : Text }
->   >
-> , < Place  = { location = "Sahara Desert" }
->   | Person : { age : Natural, name : Text }
->   >
-> , < Person = { age = 35, name = "Alice" }
->   | Place  : { location : Text }
->   >
-> ]
+> let MyType =
+>       < Person : { age : Natural, name : Text } | Place : { location : Text } >
+>
+> in  [ MyType.Person { age = 47, name = "John" }
+>     , MyType.Place { location = "North Pole" }
+>     , MyType.Place { location = "Sahara Desert" }
+>     , MyType.Person { age = 35, name = "Alice" }
+>     ]
 > $ dhall-to-json <<< "./config"
 > [{"age":47,"name":"John"},{"location":"North Pole"},{"location":"Sahara Desert"},{"age":35,"name":"Alice"}]
 
@@ -107,10 +102,10 @@
 
 > let Example = < Left : { foo : Natural } | Right : { bar : Bool } >
 > 
-> let Nesting = < Inline : {} | Nested : Text >
+> let Nesting = < Inline | Nested : Text >
 > 
 > in  { field    = "name"
->     , nesting  = Nesting.Inline {=}
+>     , nesting  = Nesting.Inline
 >     , contents = Example.Left { foo = 2 }
 >     }
 
