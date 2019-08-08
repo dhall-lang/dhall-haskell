c97b563927b6493a51472bd8d24c209d1f86a043
Gabriel Gonzalez <Gabriel439@gmail.com>
2019-08-08 07:05:48 -0700

    Hopefully fix test failure on Windows
    I suspect that the test failure is due to the locale for the test suite
    not being set to UTF8

--- dhall-json/tasty/Main.hs
+++ dhall-json/tasty/Main.hs
@@ -20,11 +20,15 @@ import qualified Dhall.JSONToDhall    as JSONToDhall
 import qualified Dhall.Parser
 import qualified Dhall.TypeCheck
 import qualified Dhall.Yaml
+import qualified GHC.IO.Encoding
 import qualified Test.Tasty
 import qualified Test.Tasty.HUnit
 
 main :: IO ()
-main = Test.Tasty.defaultMain testTree
+main = do
+    GHC.IO.Encoding.setLocaleEncoding GHC.IO.Encoding.utf8
+
+    Test.Tasty.defaultMain testTree
 
 testTree :: TestTree
 testTree =
