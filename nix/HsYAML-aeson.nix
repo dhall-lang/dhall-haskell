c6e442465611c41b56fa7d0792d234c63302a8e7
vijayphoenix <cs17btech11040@iith.ac.in>
2019-08-25 20:24:58 +0530

    Fix test cases and nix builds

--- nix/HsYAML-aeson.nix
+++ nix/HsYAML-aeson.nix
@@ -1,14 +1,19 @@
-{ mkDerivation, aeson, base, bytestring, HsYAML, mtl, stdenv, text
-, vector
+{ mkDerivation, aeson, base, bytestring, containers, fetchgit
+, HsYAML, mtl, ordered-containers, scientific, stdenv, text
+, unordered-containers, vector
 }:
 mkDerivation {
   pname = "HsYAML-aeson";
-  version = "0.1.0.0";
-  sha256 = "dc06c55a7497a21c6a6bb1070e7c68e6decb536fe8991a95220dbe84147fc1c1";
-  revision = "1";
-  editedCabalFile = "1kf35mnvc2syly35c2ffl8xxcw4h6lxv9kqirzj2in1ms19df41y";
+  version = "0.2.0.0";
+  src = fetchgit {
+    url = "https://github.com/vijayphoenix/HsYAML-aeson.git";
+    sha256 = "0a2p4nmfsnvdp1vsaas1pgryx4fv5njdxw4l6vfl0aza3i7ld7kd";
+    rev = "e269cc466b43767a6b972a9ce946419641a80e1d";
+    fetchSubmodules = true;
+  };
   libraryHaskellDepends = [
-    aeson base bytestring HsYAML mtl text vector
+    aeson base bytestring containers HsYAML mtl ordered-containers
+    scientific text unordered-containers vector
   ];
   description = "JSON to YAML Adapter";
   license = stdenv.lib.licenses.gpl2Plus;
