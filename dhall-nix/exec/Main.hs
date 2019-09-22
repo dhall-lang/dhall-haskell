b556a65644ed5d656d0a75a795db7bf18518cb67
Simon Jakobi <simon.jakobi@gmail.com>
2019-09-22 05:55:26 +0200

    Handle --version consistently (#1334)
    * dhall --version
    
    * dhall-to-bash --version
    
    * dhall-to-nix --version
    
    * dhall-lsp-server --version

--- dhall-nix/exec/Main.hs
+++ dhall-nix/exec/Main.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Main where
 
 import Control.Exception (SomeException)
+import Options.Generic (Generic, ParseRecord, type (<?>)(..))
 import System.Exit (ExitCode(..))
 
 import qualified Control.Exception
 import qualified Data.Text.IO
+import qualified Data.Version
 import qualified Dhall
 import qualified Dhall.Import
 import qualified Dhall.Nix
@@ -15,13 +22,25 @@ import qualified Dhall.TypeCheck
 import qualified GHC.IO.Encoding
 import qualified Nix.Pretty
 import qualified Options.Generic
+import qualified Paths_dhall_nix
 import qualified System.Exit
 import qualified System.IO
+
+data Options = Options
+    { version :: Bool
+        <?> "Display version"
+    } deriving (Generic, ParseRecord)
 
 main :: IO ()
 main = handle (Dhall.detailed (do
     GHC.IO.Encoding.setLocaleEncoding GHC.IO.Encoding.utf8
-    () <- Options.Generic.getRecord "Compile Dhall to Nix"
+    Options{..} <- Options.Generic.getRecord "Compile Dhall to Nix"
+
+    if unHelpful version
+        then do
+            putStrLn (Data.Version.showVersion Paths_dhall_nix.version)
+            System.Exit.exitSuccess
+        else return ()
 
     inText <- Data.Text.IO.getContents
 
