dhall-lang/dhall-haskell

commit 7634ee740b96979bc13706afa98db5c209580c2c

Fix indentation for multiline Some-, toMap- and merge-expressions (#1377)
The loss of indentation for toMap and merge was due to
75e6cc5ca7dccf4c8d5831f0aaa664fc3400f060. I have included Some for
consistency.

Fixes #1376.

Also improve format test failure messages

dhall/src/Dhall/Pretty/Internal.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP               #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
@@ -837,22 +838,20 @@ prettyCharacterSet characterSet expression =
         prettyApplicationExpression a0
 
     prettyApplicationExpression :: Pretty a => Expr Src a -> Doc Ann
-    prettyApplicationExpression a0 = case a0 of
-        App   {} -> result
-        Some  {} -> result
-        ToMap {} -> result
-        Merge {} -> result
-        Note _ b -> prettyApplicationExpression b
-        _        -> prettyImportExpression a0
+    prettyApplicationExpression = \case
+        App a b           -> f (prettyApplicationExpression a) [b]
+        Some a            -> f (builtin "Some") [a]
+        ToMap a Nothing   -> f (keyword "toMap") [a]
+        Merge a b Nothing -> f (keyword "merge") [a, b]
+        Note _ b          -> prettyApplicationExpression b
+        e                 -> prettyImportExpression e
       where
-        result = enclose' "" "" " " "" (reverse (docs a0))
-
-        docs (App  a b) = ( prettyImportExpression b, Pretty.indent 2 (prettyImportExpression b) ) : docs a
-        docs (Some   a) = map duplicate [ prettyImportExpression a , builtin "Some" ]
-        docs (ToMap a Nothing) = map duplicate [ prettyImportExpression a, keyword "toMap" ]
-        docs (Merge a b Nothing) = map duplicate [ prettyImportExpression b, prettyImportExpression a, keyword "merge" ]
-        docs (Note _ b) = docs b
-        docs         b  = map duplicate [ prettyImportExpression b ]
+        f a bs =
+            enclose'
+                "" "" " " ""
+                ( duplicate a
+                : map (fmap (Pretty.indent 2) . duplicate . prettyImportExpression) bs
+                )
 
     prettyImportExpression :: Pretty a => Expr Src a -> Doc Ann
     prettyImportExpression (Embed a) =

dhall/tests/format/applicationMultilineA.dhall
@@ -1,0 +1,5 @@
+{ app = ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff aaaaaaaaaaaaaaaaa
+, some = Some aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+, merge = merge aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+, toMap = toMap aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+}

dhall/tests/format/applicationMultilineB.dhall
@@ -1,0 +1,14 @@
+{ app =
+    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+      aaaaaaaaaaaaaaaaa
+, some =
+    Some
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+, merge =
+    merge
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+, toMap =
+    toMap
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+}

dhall/tests/Dhall/Test/Format.hs
@@ -61,6 +61,10 @@ formatTest characterSet prefix =
         expectedText <- Text.IO.readFile outputFile
 
         let message =
-                "The formatted expression did not match the expected output"
+                   "The formatted expression did not match the expected output\n"
+                <> "Expected:\n\n" <> Text.unpack expectedText <> "\n\n"
+                <> "Actual:\n\n" <> Text.unpack actualText <> "\n\n"
+                <> "Expected (show): " <> show expectedText <> "\n"
+                <> "Actual   (show): " <> show actualText <> "\n"
 
-        Tasty.HUnit.assertEqual message expectedText actualText
+        Tasty.HUnit.assertBool message (actualText == expectedText)

dhall/tests/format/largeRecordB.dhall
@@ -34,15 +34,15 @@ in    defaults.Config
       , dark_room = Some False
       , disclose =
           Some
-          (   defaults.Disclose
-            ⫽ { inventory = Some { prompt = True, default = True }
-              , attributes = Some { prompt = True, default = False }
-              , monsters_killed = Some { prompt = False, default = True }
-              , monsters_genocided = Some { prompt = False, default = False }
-              , conduct = Some { prompt = False, default = False }
-              , dungeon_overview = Some { prompt = False, default = False }
-              }
-          )
+            (   defaults.Disclose
+              ⫽ { inventory = Some { prompt = True, default = True }
+                , attributes = Some { prompt = True, default = False }
+                , monsters_killed = Some { prompt = False, default = True }
+                , monsters_genocided = Some { prompt = False, default = False }
+                , conduct = Some { prompt = False, default = False }
+                , dungeon_overview = Some { prompt = False, default = False }
+                }
+            )
       , dogname = Some "Cujo"
       , extmenu = Some False
       , fixinv = Some True

commit e560e27d803edce3565da1d3aa3757bcda2c5157

Fix formatting of multi-argument function applications
merge-expressions with additional arguments are also formatted with
consistent indentation for all arguments. E.g.

    merge
      { x = Natural/even }
      < x >.x
      1111111111111111111111111111111111111111111111111111111111

Fixes a small regression introduced in 7634ee7.

dhall/src/Dhall/Pretty/Internal.hs
@@ -838,19 +838,22 @@ prettyCharacterSet characterSet expression =
         prettyApplicationExpression a0
 
     prettyApplicationExpression :: Pretty a => Expr Src a -> Doc Ann
-    prettyApplicationExpression = \case
-        App a b           -> f (prettyApplicationExpression a) [b]
-        Some a            -> f (builtin "Some") [a]
-        ToMap a Nothing   -> f (keyword "toMap") [a]
-        Merge a b Nothing -> f (keyword "merge") [a, b]
-        Note _ b          -> prettyApplicationExpression b
-        e                 -> prettyImportExpression e
+    prettyApplicationExpression = go []
       where
-        f a bs =
+        go args = \case
+            App a b           -> go (b : args) a
+            Some a            -> app (builtin "Some") (a : args)
+            Merge a b Nothing -> app (keyword "merge") (a : b : args)
+            ToMap a Nothing   -> app (keyword "toMap") (a : args)
+            Note _ b          -> go args b
+            e | null args     -> prettyImportExpression e -- just a performance optimization
+              | otherwise     -> app (prettyImportExpression e) args
+
+        app f args =
             enclose'
                 "" "" " " ""
-                ( duplicate a
-                : map (fmap (Pretty.indent 2) . duplicate . prettyImportExpression) bs
+                ( duplicate f
+                : map (fmap (Pretty.indent 2) . duplicate . prettyImportExpression) args
                 )
 
     prettyImportExpression :: Pretty a => Expr Src a -> Doc Ann

dhall/tests/format/applicationMultilineA.dhall
@@ -1,5 +1,30 @@
-{ app = ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff aaaaaaaaaaaaaaaaa
-, some = Some aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-, merge = merge aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-, toMap = toMap aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+{ app =
+    ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+      aaaaaaaaaaaaaaaaa
+, app2 =
+    f
+      a
+      b
+      cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+, app3 =
+    f
+      a
+      b
+      c
+      dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+, some =
+    Some
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+, merge =
+    merge
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+, merge3 =
+    merge
+      a
+      b
+      ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+, toMap =
+    toMap
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 }

dhall/tests/format/applicationMultilineB.dhall
@@ -1,6 +1,17 @@
 { app =
     ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
       aaaaaaaaaaaaaaaaa
+, app2 =
+    f
+      a
+      b
+      cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+, app3 =
+    f
+      a
+      b
+      c
+      dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 , some =
     Some
       aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
@@ -8,6 +19,11 @@
     merge
       aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
       bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+, merge3 =
+    merge
+      a
+      b
+      ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 , toMap =
     toMap
       aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

dhall/tests/format/concatSepA.dhall
@@ -1,0 +1,30 @@
+{-
+Concatenate a `List` of `Text` values with a separator in between each value
+-}
+let Status = < Empty | NonEmpty : Text >
+
+let concatSep
+    : ∀(separator : Text) → ∀(elements : List Text) → Text
+    =   λ(separator : Text)
+      → λ(elements : List Text)
+      → let status =
+              List/fold Text elements Status
+                (   λ(element : Text)
+                  → λ(status : Status)
+                  → merge
+                      { Empty = Status.NonEmpty element
+                      , NonEmpty =
+                            λ(result : Text)
+                          → Status.NonEmpty (element ++ separator ++ result)
+                      }
+                      status
+                )
+                Status.Empty
+        
+        in  merge { Empty = "", NonEmpty = λ(result : Text) → result } status
+
+let example0 = assert : concatSep ", " [ "ABC", "DEF", "GHI" ] ≡ "ABC, DEF, GHI"
+
+let example1 = assert : concatSep ", " ([] : List Text) ≡ ""
+
+in  concatSep

dhall/tests/format/concatSepB.dhall
@@ -1,0 +1,33 @@
+{-
+Concatenate a `List` of `Text` values with a separator in between each value
+-}
+let Status = < Empty | NonEmpty : Text >
+
+let concatSep
+    : ∀(separator : Text) → ∀(elements : List Text) → Text
+    =   λ(separator : Text)
+      → λ(elements : List Text)
+      → let status =
+              List/fold
+                Text
+                elements
+                Status
+                (   λ(element : Text)
+                  → λ(status : Status)
+                  → merge
+                      { Empty = Status.NonEmpty element
+                      , NonEmpty =
+                            λ(result : Text)
+                          → Status.NonEmpty (element ++ separator ++ result)
+                      }
+                      status
+                )
+                Status.Empty
+        
+        in  merge { Empty = "", NonEmpty = λ(result : Text) → result } status
+
+let example0 = assert : concatSep ", " [ "ABC", "DEF", "GHI" ] ≡ "ABC, DEF, GHI"
+
+let example1 = assert : concatSep ", " ([] : List Text) ≡ ""
+
+in  concatSep