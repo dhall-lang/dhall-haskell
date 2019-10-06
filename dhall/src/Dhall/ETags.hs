d9c555997a8ed4195911722decf9beeb69440dbf
Dima Kurilo <dkurilo@gmail.com>
2019-10-06 19:37:46 -0400

    fix for old Data.Either

--- dhall/src/Dhall/ETags.hs
+++ dhall/src/Dhall/ETags.hs
@@ -10,7 +10,9 @@ module Dhall.ETags
     ) where
 
 import Control.Exception (handle, SomeException(..))
+#if MIN_VERSION_base(4,10,0)
 import Data.Either (fromRight)
+#endif
 import Data.List (isSuffixOf)
 import Data.Maybe (fromMaybe)
 import Data.Semigroup (Semigroup(..))
@@ -34,6 +36,13 @@ import Text.Megaparsec (sourceLine, sourceColumn, unPos)
 import qualified Data.Map      as M
 import qualified Data.Text     as T
 import qualified Data.Text.IO  as TIO
+
+
+#if !(MIN_VERSION_base(4,10,0))
+fromRight :: b -> Either a b -> b
+fromRight _ (Right b) = b
+fromRight b _         = b
+#endif
 
 {-| 
     Documentation for ETags format is not very informative and not very correct.
