23c30f59b601dfcaf975f6a3791ff566873b914d
Jérémie Gillet <jie.gillet@gmail.com>
2019-10-07 21:36:59 +0900

    Revert Crypto to master version

--- dhall/ghc-src/Dhall/Crypto.hs
+++ dhall/ghc-src/Dhall/Crypto.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE PackageImports #-}
 
 module Dhall.Crypto (
       SHA256Digest(..)
@@ -9,13 +8,13 @@ module Dhall.Crypto (
     ) where
 
 import Control.DeepSeq (NFData)
-import "cryptonite" Crypto.Hash (SHA256) -- To avoid conflict with "hashing"
+import Crypto.Hash (SHA256)
 import Data.ByteArray (ByteArrayAccess, convert)
 import Data.ByteArray.Encoding (Base(Base16), convertToBase)
 import Data.ByteString (ByteString)
 import GHC.Generics (Generic)
 
-import qualified "cryptonite" Crypto.Hash -- To avoid conflict with "hashing"
+import qualified Crypto.Hash
 import qualified Data.ByteString.Char8 as ByteString.Char8
 
 newtype SHA256Digest = SHA256Digest { unSHA256Digest :: ByteString }
