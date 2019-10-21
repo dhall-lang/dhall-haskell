658bedf56af71aa525fc0497d0b4614eaaf74c4f
Simon Jakobi <simon.jakobi@gmail.com>
2019-10-21 15:46:16 +0200

    Update nix/prettyprinter.nix

--- nix/prettyprinter.nix
+++ nix/prettyprinter.nix
@@ -1,22 +1,22 @@
-{ mkDerivation, ansi-wl-pprint, base, bytestring, containers
-, criterion, deepseq, doctest, mtl, pgp-wordlist, QuickCheck
-, random, stdenv, tasty, tasty-hunit, tasty-quickcheck, text
-, transformers
+{ mkDerivation, ansi-wl-pprint, base, base-compat, bytestring
+, containers, criterion, deepseq, doctest, mtl, pgp-wordlist
+, QuickCheck, random, stdenv, tasty, tasty-hunit, tasty-quickcheck
+, text, transformers
 }:
 mkDerivation {
   pname = "prettyprinter";
-  version = "1.2.1";
-  sha256 = "e7653e0ba87cc06553a50e4780dde81c5dd156196c0199511d03d972e5517fcf";
+  version = "1.4.0";
+  sha256 = "7f1d9224f9e577eb24dda695beb6bc2f074e93a84a3c9f11bb578aa6ed39cb45";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [ base text ];
   testHaskellDepends = [
-    base bytestring doctest pgp-wordlist tasty tasty-hunit
+    base bytestring doctest pgp-wordlist QuickCheck tasty tasty-hunit
     tasty-quickcheck text
   ];
   benchmarkHaskellDepends = [
-    ansi-wl-pprint base containers criterion deepseq mtl QuickCheck
-    random text transformers
+    ansi-wl-pprint base base-compat containers criterion deepseq mtl
+    QuickCheck random text transformers
   ];
   homepage = "http://github.com/quchen/prettyprinter";
   description = "A modern, easy to use, well-documented, extensible pretty-printer";
