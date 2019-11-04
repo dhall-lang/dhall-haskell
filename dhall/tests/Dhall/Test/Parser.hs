dhall-lang/dhall-haskell

commit 7eec31d1d72927c4535383534890f180a2b0a85f

Parse whitespace more precisely (#1483)
This is preparatory work for #1454.

This also fixes some cases where dhall would previously accept
malformatted inputs.

The changes to dhall-lsp-server are mostly untested. See #1510.

Co-authored-by: Gabriel Gonzalez <Gabriel439@gmail.com>

dhall/src/Dhall/Parser/Expression.hs
@@ -128,9 +128,7 @@ data Parsers a = Parsers
 parsers :: Parser a -> Parsers a
 parsers embedded = Parsers {..}
   where
-    completeExpression_ = do
-        whitespace
-        expression
+    completeExpression_ = whitespace *> expression <* whitespace
 
     expression =
         noted
@@ -146,53 +144,72 @@ parsers embedded = Parsers {..}
       where
         alternative0 = do
             _lambda
+            whitespace
             _openParens
+            whitespace
             a <- label
+            whitespace
             _colon
+            nonemptyWhitespace
             b <- expression
+            whitespace
             _closeParens
+            whitespace
             _arrow
+            whitespace
             c <- expression
             return (Lam a b c)
 
         alternative1 = do
             _if
+            nonemptyWhitespace
             a <- expression
+            whitespace
             _then
+            nonemptyWhitespace
             b <- expression
+            whitespace
             _else
+            nonemptyWhitespace
             c <- expression
             return (BoolIf a b c)
 
         alternative2 = do
             let binding = do
-                    _letOnly
+                    _let
 
                     src0 <- src nonemptyWhitespace
 
-                    c <- labelOnly
+                    c <- label
 
                     src1 <- src whitespace
 
                     d <- optional (do
-                        _colonOnly
+                        _colon
 
                         src2 <- src nonemptyWhitespace
 
                         e <- expression
+
+                        whitespace
+
                         return (Just src2, e) )
 
-                    _equalOnly
+                    _equal
 
                     src3 <- src whitespace
 
                     f <- expression
+
+                    whitespace
 
                     return (Binding (Just src0) c (Just src1) d (Just src3) f)
 
             as <- Data.List.NonEmpty.some1 binding
 
             _in
+
+            nonemptyWhitespace
 
             b <- expression
 
@@ -217,18 +234,27 @@ parsers embedded = Parsers {..}
 
         alternative3 = do
             _forall
+            whitespace
             _openParens
+            whitespace
             a <- label
+            whitespace
             _colon
+            nonemptyWhitespace
             b <- expression
+            whitespace
             _closeParens
+            whitespace
             _arrow
+            whitespace
             c <- expression
             return (Pi a b c)
 
         alternative4 = do
             _assert
+            whitespace
             _colon
+            nonemptyWhitespace
             a <- expression
             return (Assert a)
 
@@ -236,15 +262,16 @@ parsers embedded = Parsers {..}
             a <- operatorExpression
 
             let alternative4A = do
-                    _arrow
+                    try (whitespace *> _arrow)
+                    whitespace
                     b <- expression
+                    whitespace
                     return (Pi "_" a b)
 
             let alternative4B = do
-                    _colon
-
+                    try (whitespace *> _colon)
+                    nonemptyWhitespace
                     b <- expression
-
                     case shallowDenote a of
                         ListLit _ [] ->
                             return (ListLit (Just b) [])
@@ -263,34 +290,33 @@ parsers embedded = Parsers {..}
             noted (do
                 a <- subExpression
                 b <- Text.Megaparsec.many $ do
-                    op <- operatorParser
+                    op <- try (whitespace *> operatorParser)
                     r  <- subExpression
-
                     return (\l -> l `op` r)
                 return (foldl (\x f -> f x) a b) )
 
     operatorParsers :: [Parser (Expr s a -> Expr s a -> Expr s a)]
     operatorParsers =
-        [ ImportAlt    <$ _importAlt
-        , BoolOr       <$ _or
-        , NaturalPlus  <$ _plus
-        , TextAppend   <$ _textAppend
-        , ListAppend   <$ _listAppend
-        , BoolAnd      <$ _and
-        , Combine      <$ _combine
-        , Prefer       <$ _prefer
-        , CombineTypes <$ _combineTypes
-        , NaturalTimes <$ _times
-        , BoolEQ       <$ _doubleEqual
-        , BoolNE       <$ _notEqual
-        , Equivalent   <$ _equivalent
+        [ ImportAlt    <$ _importAlt    <* nonemptyWhitespace
+        , BoolOr       <$ _or           <* whitespace
+        , NaturalPlus  <$ _plus         <* nonemptyWhitespace
+        , TextAppend   <$ _textAppend   <* whitespace
+        , ListAppend   <$ _listAppend   <* whitespace
+        , BoolAnd      <$ _and          <* whitespace
+        , Combine      <$ _combine      <* whitespace
+        , Prefer       <$ _prefer       <* whitespace
+        , CombineTypes <$ _combineTypes <* whitespace
+        , NaturalTimes <$ _times        <* whitespace
+        , BoolEQ       <$ _doubleEqual  <* whitespace
+        , BoolNE       <$ _notEqual     <* whitespace
+        , Equivalent   <$ _equivalent   <* whitespace
         ]
 
     applicationExpression = do
-            f <-    (do _Some; return Some)
+            f <-    (Some <$ _Some <* nonemptyWhitespace)
                 <|> return id
             a <- noted importExpression_
-            b <- Text.Megaparsec.many (noted importExpression_)
+            b <- Text.Megaparsec.many (try (nonemptyWhitespace *> noted importExpression_))
             return (foldl app (f a) b)
           where
             app nL@(Note (Src before _ bytesL) _) nR@(Note (Src _ after bytesR) _) =
@@ -321,7 +347,7 @@ parsers embedded = Parsers {..}
     selectorExpression = noted (do
             a <- primitiveExpression
 
-            let recordType = _openParens *> expression <* _closeParens
+            let recordType = _openParens *> whitespace *> expression <* whitespace <* _closeParens
 
             let field               x  e = Field   e  x
             let projectBySet        xs e = Project e (Left  xs)
@@ -332,7 +358,7 @@ parsers embedded = Parsers {..}
                     <|> fmap projectBySet        labels
                     <|> fmap projectByExpression recordType
 
-            b <- Text.Megaparsec.many (try (do _dot; alternatives))
+            b <- Text.Megaparsec.many (try (whitespace *> _dot *> whitespace *> alternatives))
             return (foldl (\e k -> k e) a b) )
 
     primitiveExpression =
@@ -375,9 +401,13 @@ parsers embedded = Parsers {..}
             alternative04 = (do
                 _openBrace
 
-                _ <- optional _comma
+                whitespace
+
+                _ <- optional (_comma *> whitespace)
 
                 a <- recordTypeOrLiteral
+
+                whitespace
 
                 _closeBrace
 
@@ -389,12 +419,15 @@ parsers embedded = Parsers {..}
 
             alternative07 = do
                 _merge
+                nonemptyWhitespace
                 a <- importExpression_
+                nonemptyWhitespace
                 b <- importExpression_ <?> "second argument to ❰merge❱"
                 return (Merge a b Nothing)
 
             alternative08 = do
                 _toMap
+                nonemptyWhitespace
                 a <- importExpression_
                 return (ToMap a Nothing)
 
@@ -484,7 +517,9 @@ parsers embedded = Parsers {..}
 
             alternative38 = do
                 _openParens
+                whitespace
                 a <- expression
+                whitespace
                 _closeParens
                 return a
 
@@ -650,7 +685,6 @@ parsers embedded = Parsers {..}
 
     textLiteral = (do
             literal <- doubleQuotedLiteral <|> singleQuoteLiteral
-            whitespace
             return (TextLit literal) ) <?> "literal"
 
     recordTypeOrLiteral =
@@ -672,24 +706,32 @@ parsers embedded = Parsers {..}
             a <- anyLabel
 
             let nonEmptyRecordType = do
-                    _colon
+                    try (whitespace *> _colon)
+                    nonemptyWhitespace
                     b <- expression
                     e <- Text.Megaparsec.many (do
-                        _comma
+                        try (whitespace *> _comma)
+                        whitespace
                         c <- anyLabel
+                        whitespace
                         _colon
+                        nonemptyWhitespace
                         d <- expression
                         return (c, d) )
                     m <- toMap ((a, b) : e)
                     return (Record m)
 
             let nonEmptyRecordLiteral = do
-                    _equal
+                    try (whitespace *> _equal)
+                    whitespace
                     b <- expression
                     e <- Text.Megaparsec.many (do
-                        _comma
+                        try (whitespace *> _comma)
+                        whitespace
                         c <- anyLabel
+                        whitespace
                         _equal
+                        whitespace
                         d <- expression
                         return (c, d) )
                     m <- toMap ((a, b) : e)
@@ -700,14 +742,17 @@ parsers embedded = Parsers {..}
     unionType = (do
             _openAngle
 
-            _ <- optional _bar
+            whitespace
+
+            _ <- optional (_bar *> whitespace)
 
             let unionTypeEntry = do
                     a <- anyLabel
-                    b <- optional (do _colon; expression)
+                    whitespace
+                    b <- optional (_colon *> nonemptyWhitespace *> expression <* whitespace)
                     return (a, b)
 
-            kvs <- Text.Megaparsec.sepBy unionTypeEntry _bar
+            kvs <- Text.Megaparsec.sepBy unionTypeEntry (_bar *> whitespace)
 
             m <- toMap kvs
 
@@ -718,11 +763,14 @@ parsers embedded = Parsers {..}
     listLiteral = (do
             _openBracket
 
-            _ <- optional _comma
+            whitespace
 
-            a <- Text.Megaparsec.sepBy expression _comma
+            _ <- optional (_comma *> whitespace)
+
+            a <- Text.Megaparsec.sepBy (expression <* whitespace) (_comma *> whitespace)
 
             _closeBracket
+
             return (ListLit Nothing (Data.Sequence.fromList a)) ) <?> "literal"
 
 {-| Parse an environment variable import
@@ -733,7 +781,6 @@ env :: Parser ImportType
 env = do
     _ <- text "env:"
     a <- (alternative0 <|> alternative1)
-    whitespace
     return (Env a)
   where
     alternative0 = bashEnvironmentVariable
@@ -784,7 +831,6 @@ localOnly =
 local :: Parser ImportType
 local = do
     a <- localOnly
-    whitespace
     return a
 
 {-| Parse an HTTP(S) import
@@ -794,9 +840,10 @@ local = do
 http :: Parser ImportType
 http = do
     url <- httpRaw
-    whitespace
-    headers <- optional (do
+    headers <- optional (try $ do
+        whitespace
         _using
+        nonemptyWhitespace
         importExpression import_ )
     return (Remote (url { headers }))
 
@@ -830,7 +877,6 @@ importHash_ :: Parser Dhall.Crypto.SHA256Digest
 importHash_ = do
     _ <- text "sha256:"
     t <- count 64 (satisfy hexdig <?> "hex digit")
-    whitespace
     let strictBytes16 = Data.Text.Encoding.encodeUtf8 t
     strictBytes <- case Data.ByteArray.Encoding.convertFromBase Base16 strictBytes16 of
         Left  string      -> fail string
@@ -846,7 +892,7 @@ importHash_ = do
 importHashed_ :: Parser ImportHashed
 importHashed_ = do
     importType <- importType_
-    hash       <- optional importHash_
+    hash       <- optional (try (nonemptyWhitespace *> importHash_))
     return (ImportHashed {..})
 
 {-| Parse an `Import`
@@ -859,8 +905,10 @@ import_ = (do
     importMode   <- alternative <|> pure Code
     return (Import {..}) ) <?> "import"
   where
-    alternative = do
+    alternative = try $ do
+      whitespace
       _as
+      nonemptyWhitespace
       (_Text >> pure RawText) <|> (_Location >> pure Location)
 
 -- | Same as @Data.Text.splitOn@, except always returning a `NonEmpty` result

dhall/tests/Dhall/Test/Parser.hs
@@ -89,7 +89,7 @@ notesInLetInLet = do
                       (Just " ")
                       Nothing
                       (Just " ")
-                      (Note "0 " (NaturalLit 0)))
+                      (Note "0" (NaturalLit 0)))
                     -- This 'Let' isn't wrapped in a 'Note'!
                     (Let
                       (Binding
@@ -98,7 +98,7 @@ notesInLetInLet = do
                         (Just " ")
                         Nothing
                         (Just " ")
-                        (Note "1 " (NaturalLit 1))
+                        (Note "1" (NaturalLit 1))
                       )
                       (Note "let z = 2 in x"
                         (Let
@@ -108,7 +108,7 @@ notesInLetInLet = do
                             (Just " ")
                             Nothing
                             (Just " ")
-                            (Note "2 " (NaturalLit 2))
+                            (Note "2" (NaturalLit 2))
                           )
                           (Note "x"
                             (Var (V "x" 0))))))))
@@ -125,8 +125,8 @@ shouldParse path = do
             [ parseDirectory </> "success/unit/MergeParenAnnotation"
 
             -- https://github.com/dhall-lang/dhall-haskell/issues/1454
-            , parseDirectory </> "success/preferMissingNoSpaces"
-            , parseDirectory </> "success/missingInParentheses"
+            , parseDirectory </> "success/missingFoo"
+            , parseDirectory </> "success/missingSlash"
             ]
 
     let pathString = Text.unpack path
@@ -157,35 +157,11 @@ shouldParse path = do
 shouldNotParse :: Text -> TestTree
 shouldNotParse path = do
     let expectedFailures =
-            [ -- These two unexpected successes are due to not correctly
-              -- requiring non-empty whitespace after the `:` in a type
-              -- annotation
-              parseDirectory </> "failure/unit/ImportEnvWrongEscape.dhall"
-
-              -- Other spacing related unexpected successes:
-            , parseDirectory </> "failure/spacing/AnnotationNoSpace.dhall"
-            , parseDirectory </> "failure/spacing/ApplicationNoSpace1.dhall"
-            , parseDirectory </> "failure/spacing/ApplicationNoSpace2.dhall"
-            , parseDirectory </> "failure/spacing/AssertNoSpace.dhall"
-            , parseDirectory </> "failure/spacing/ForallNoSpace.dhall"
-            , parseDirectory </> "failure/spacing/ImportAltNoSpace.dhall"
-            , parseDirectory </> "failure/spacing/ImportHashedNoSpace.dhall"
-            , parseDirectory </> "failure/spacing/LambdaNoSpace.dhall"
-            , parseDirectory </> "failure/spacing/ListLitEmptyNoSpace.dhall"
-            , parseDirectory </> "failure/spacing/MergeAnnotationNoSpace3.dhall"
-            , parseDirectory </> "failure/spacing/MergeNoSpace2.dhall"
-            , parseDirectory </> "failure/spacing/NaturalPlusNoSpace.dhall"
-            , parseDirectory </> "failure/spacing/RecordTypeNoSpace.dhall"
-            , parseDirectory </> "failure/spacing/ToMapAnnotNoSpace.dhall"
-            , parseDirectory </> "failure/spacing/UnionTypeNoSpace.dhall"
-
-            , parseDirectory </> "failure/ImportHeadersExteriorHash.dhall"
-
-              -- For parsing performance reasons the implementation
+            [ -- For parsing performance reasons the implementation
               -- treats a missing type annotation on an empty list as
               -- as a type-checking failure instead of a parse failure,
               -- but this might be fixable.
-            , parseDirectory </> "failure/unit/ListLitEmptyMissingAnnotation.dhall"
+              parseDirectory </> "failure/unit/ListLitEmptyMissingAnnotation.dhall"
             , parseDirectory </> "failure/unit/ListLitEmptyAnnotation.dhall"
 
               -- The same performance improvements also broke the

dhall-lsp-server/src/Dhall/LSP/Backend/Parsing.hs
@@ -73,6 +73,7 @@ getLetIdentifier src@(Src left _ text) =
   where parseLetIdentifier = do
           setSourcePos left
           _let
+          whitespace
           begin <- getSourcePos
           (tokens, _) <- Megaparsec.match label
           end <- getSourcePos
@@ -86,7 +87,9 @@ getLamIdentifier (Src left _ text) =
   where parseLetIdentifier = do
           setSourcePos left
           _lambda
+          whitespace
           _openParens
+          whitespace
           begin <- getSourcePos
           (tokens, _) <- Megaparsec.match label
           end <- getSourcePos
@@ -100,7 +103,9 @@ getForallIdentifier (Src left _ text) =
   where parseLetIdentifier = do
           setSourcePos left
           _forall
+          whitespace
           _openParens
+          whitespace
           begin <- getSourcePos
           (tokens, _) <- Megaparsec.match label
           end <- getSourcePos
@@ -116,6 +121,7 @@ getImportHash (Src left _ text) =
   where parseImportHashPosition = do
           setSourcePos left
           _ <- importType_
+          whitespace
           begin <- getSourcePos
           (tokens, _) <- Megaparsec.match $ optional importHash_
           end <- getSourcePos
@@ -166,37 +172,57 @@ binderExprFromText txt =
 
     closedLet = do
       _let
+      nonemptyWhitespace
       _ <- label
+      whitespace
       _ <- optional (do
         _colon
+        nonemptyWhitespace
         expr)
       _equal
+      whitespace
       _ <- expr
+      whitespace
       (do
         _in
+        nonemptyWhitespace
         _ <- expr
         return ())
         <|> closedLet
 
     closedLambda = do
       _lambda
+      whitespace
       _openParens
+      whitespace
       _ <- label
+      whitespace
       _colon
+      nonemptyWhitespace
       _ <- expr
+      whitespace
       _closeParens
+      whitespace
       _arrow
+      whitespace
       _ <- expr
       return ()
 
     closedPi = do
       _forall
+      whitespace
       _openParens
+      whitespace
       _ <- label
+      whitespace
       _colon
+      nonemptyWhitespace
       _ <- expr
+      whitespace
       _closeParens
+      whitespace
       _arrow
+      whitespace
       _ <- expr
       return ()
 
@@ -219,33 +245,45 @@ binderExprFromText txt =
 
     letBinder = do
       _let
+      nonemptyWhitespace
       name <- label
-      mType <- optional (do _colon; _type <- expr; return (Nothing, _type))
+      whitespace
+      mType <- optional (do _colon; nonemptyWhitespace; _type <- expr; whitespace; return (Nothing, _type))
 
       -- if the bound value does not parse, skip and replace with 'hole'
-      value <- try (do _equal; expr)
+      value <- try (do _equal; whitespace; expr <* whitespace)
           <|> (do skipManyTill anySingle (lookAhead boundary <|> _in); return holeExpr)
       inner <- parseBinderExpr
       return (Let (Binding Nothing name Nothing mType Nothing value) inner)
 
     forallBinder = do
       _forall
+      whitespace
       _openParens
+      whitespace
       name <- label
+      whitespace
       _colon
+      nonemptyWhitespace
       -- if the bound type does not parse, skip and replace with 'hole'
-      typ <- try (do e <- expr; _closeParens; _arrow; return e)
+      typ <- try (do e <- expr; whitespace; _closeParens; whitespace; _arrow; return e)
           <|> (do skipManyTill anySingle _arrow; return holeExpr)
+      whitespace
       inner <- parseBinderExpr
       return (Pi name typ inner)
 
     lambdaBinder = do
       _lambda
+      whitespace
       _openParens
+      whitespace
       name <- label
+      whitespace
       _colon
+      nonemptyWhitespace
       -- if the bound type does not parse, skip and replace with 'hole'
-      typ <- try (do e <- expr; _closeParens; _arrow; return e)
+      typ <- try (do e <- expr; whitespace; _closeParens; whitespace; _arrow; return e)
           <|> (do skipManyTill anySingle _arrow; return holeExpr)
+      whitespace
       inner <- parseBinderExpr
       return (Lam name typ inner)

commit a3e96489b03c9b47a7f745d6dc42cabb8a8c09f9

Partially fix whitespace parsing performance regression
This undoes some of the performance regression introduced
in https://github.com/dhall-lang/dhall-haskell/pull/1483

Before #1483:

```
benchmarked Line comment
time                 11.86 ms   (11.69 ms .. 11.98 ms)
                     0.999 R²   (0.999 R² .. 1.000 R²)
mean                 11.84 ms   (11.79 ms .. 11.89 ms)
std dev              129.4 μs   (107.2 μs .. 164.1 μs)

benchmarked Block comment
time                 13.20 ms   (13.00 ms .. 13.41 ms)
                     0.999 R²   (0.998 R² .. 1.000 R²)
mean                 13.59 ms   (13.41 ms .. 13.94 ms)
std dev              600.0 μs   (142.2 μs .. 953.7 μs)
```

After #1483:

```
benchmarked Line comment
time                 288.7 ms   (282.8 ms .. 294.7 ms)
                     1.000 R²   (0.999 R² .. 1.000 R²)
mean                 292.3 ms   (290.8 ms .. 294.6 ms)
std dev              3.156 ms   (2.216 ms .. 4.546 ms)

benchmarked Block comment
time                 286.2 ms   (280.9 ms .. 292.6 ms)
                     0.999 R²   (0.998 R² .. 1.000 R²)
mean                 290.6 ms   (288.3 ms .. 292.9 ms)
std dev              3.875 ms   (2.866 ms .. 5.500 ms)
```

After this change:

```
benchmarked Line comment
time                 61.44 ms   (60.37 ms .. 63.03 ms)
                     0.999 R²   (0.997 R² .. 1.000 R²)
mean                 61.41 ms   (60.74 ms .. 62.25 ms)
std dev              1.341 ms   (945.0 μs .. 1.901 ms)

benchmarked Block comment
time                 61.83 ms   (60.97 ms .. 63.14 ms)
                     0.999 R²   (0.998 R² .. 1.000 R²)
mean                 61.16 ms   (60.33 ms .. 61.85 ms)
std dev              1.396 ms   (1.011 ms .. 1.907 ms)
```

dhall/src/Dhall/Parser/Expression.hs
@@ -262,14 +262,14 @@ parsers embedded = Parsers {..}
             a <- operatorExpression
 
             let alternative4A = do
-                    try (whitespace *> _arrow)
+                    _arrow
                     whitespace
                     b <- expression
                     whitespace
                     return (Pi "_" a b)
 
             let alternative4B = do
-                    try (whitespace *> _colon)
+                    _colon
                     nonemptyWhitespace
                     b <- expression
                     case shallowDenote a of
@@ -289,9 +289,16 @@ parsers embedded = Parsers {..}
     makeOperatorExpression operatorParser subExpression =
             noted (do
                 a <- subExpression
+
+                whitespace
+
                 b <- Text.Megaparsec.many $ do
-                    op <- try (whitespace *> operatorParser)
+                    op <- operatorParser
+
                     r  <- subExpression
+
+                    whitespace
+
                     return (\l -> l `op` r)
                 return (foldl (\x f -> f x) a b) )
 
@@ -705,36 +712,70 @@ parsers embedded = Parsers {..}
     nonEmptyRecordTypeOrLiteral = do
             a <- anyLabel
 
+            whitespace
+
             let nonEmptyRecordType = do
-                    try (whitespace *> _colon)
+                    _colon
+
                     nonemptyWhitespace
+
                     b <- expression
+
+                    whitespace
+
                     e <- Text.Megaparsec.many (do
-                        try (whitespace *> _comma)
+                        _comma
+
                         whitespace
+
                         c <- anyLabel
+
                         whitespace
+
                         _colon
+
                         nonemptyWhitespace
+
                         d <- expression
+
+                        whitespace
+
                         return (c, d) )
+
                     m <- toMap ((a, b) : e)
+
                     return (Record m)
 
             let nonEmptyRecordLiteral = do
-                    try (whitespace *> _equal)
+                    _equal
+
                     whitespace
+
                     b <- expression
+
+                    whitespace
+
                     e <- Text.Megaparsec.many (do
-                        try (whitespace *> _comma)
+                        _comma
+
                         whitespace
+
                         c <- anyLabel
+
                         whitespace
+
                         _equal
+
                         whitespace
+
                         d <- expression
+
+                        whitespace
+
                         return (c, d) )
+
                     m <- toMap ((a, b) : e)
+
                     return (RecordLit m)
 
             nonEmptyRecordType <|> nonEmptyRecordLiteral
@@ -840,9 +881,8 @@ local = do
 http :: Parser ImportType
 http = do
     url <- httpRaw
-    headers <- optional (try $ do
-        whitespace
-        _using
+    headers <- optional (do
+        try (whitespace *> _using)
         nonemptyWhitespace
         importExpression import_ )
     return (Remote (url { headers }))
@@ -905,10 +945,9 @@ import_ = (do
     importMode   <- alternative <|> pure Code
     return (Import {..}) ) <?> "import"
   where
-    alternative = try $ do
-      whitespace
-      _as
-      nonemptyWhitespace
+    alternative = do
+      try (whitespace *> _as *> nonemptyWhitespace)
+
       (_Text >> pure RawText) <|> (_Location >> pure Location)
 
 -- | Same as @Data.Text.splitOn@, except always returning a `NonEmpty` result

dhall/tests/Dhall/Test/Parser.hs
@@ -89,7 +89,8 @@ notesInLetInLet = do
                       (Just " ")
                       Nothing
                       (Just " ")
-                      (Note "0" (NaturalLit 0)))
+                      (Note "0 " (Note "0" (NaturalLit 0)))
+                    )
                     -- This 'Let' isn't wrapped in a 'Note'!
                     (Let
                       (Binding
@@ -98,7 +99,7 @@ notesInLetInLet = do
                         (Just " ")
                         Nothing
                         (Just " ")
-                        (Note "1" (NaturalLit 1))
+                        (Note "1 " (Note "1" (NaturalLit 1)))
                       )
                       (Note "let z = 2 in x"
                         (Let
@@ -108,10 +109,14 @@ notesInLetInLet = do
                             (Just " ")
                             Nothing
                             (Just " ")
-                            (Note "2" (NaturalLit 2))
+                            (Note "2 " (Note "2" (NaturalLit 2)))
                           )
-                          (Note "x"
-                            (Var (V "x" 0))))))))
+                          (Note "x" (Var (V "x" 0)))
+                        )
+                      )
+                    )
+                  )
+                )
 
         let msg = "Unexpected parse result"
 