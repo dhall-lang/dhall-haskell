dhall-lang/dhall-haskell

commit fd8683216d50a114c5421f9bdb64d98ea9b9592f

Fix tests without `with-http` flag (#1159)
* Allow customization of remote import resolution

Makes the `Status` type more general; previously support for
`Network.HTTP.Client` was hardcoded. In short:

```
data Status = Status
    { _stack :: NonEmpty Chained
    [...]
--  , _manager :: Maybe Dynamic
--  --   importing the same expression twice with different values
++  , _remote :: URL -> StateT Status IO Data.Text.Text
++  -- ^ The remote resolver, fetches the content at the given URL.

    [...]
    }

```

* Simplify and expose `toHeaders`

`toHeaders` will be needed for mock http testing

* Fix compilation without `with-http` flag

* Fix compilation with `with-http` flag

* Fix tests without `with-http` flag

Implements a mock http client that handles requests to:
- `https://raw.githubusercontent.com/dhall-lang/dhall-lang/master/`
- `https://test.dhall-lang.org/Bool/package.dhall`
- `https://httpbin.org/user-agent`

This allows tests involving remote imports to succeed even when compiled
without the `with-http` flag.

* Build `dhall` with HTTP support compiled out in CI

... to prevent regressions from occurring in the future

dhall/dhall.cabal
@@ -686,6 +686,7 @@ Test-Suite tasty
         directory                                      ,
         filepath                                       ,
         foldl                                    < 1.5 ,
+        lens-family-core          >= 1.0.0    && < 2.1 ,
         megaparsec                                     ,
         prettyprinter                                  ,
         QuickCheck                >= 2.10     && < 2.14,
@@ -700,6 +701,9 @@ Test-Suite tasty
         transformers                                   ,
         turtle                                   < 1.6 ,
         vector                    >= 0.11.0.0 && < 0.13
+    if flag(with-http)
+      CPP-Options:
+        -DWITH_HTTP
     Default-Language: Haskell2010
 
 Test-Suite doctest

dhall/src/Dhall/Import.hs
@@ -117,7 +117,8 @@ module Dhall.Import (
     , cache
     , Depends(..)
     , graph
-    , manager
+    , remote
+    , toHeaders
     , standardVersion
     , normalizer
     , startingContext
@@ -169,6 +170,7 @@ import Dhall.Core
     , URL(..)
     )
 #ifdef MIN_VERSION_http_client
+import Network.HTTP.Client (Manager)
 import Dhall.Import.HTTP hiding (HTTPHeader)
 #endif
 import Dhall.Import.Types
@@ -396,29 +398,6 @@ instance Canonicalize ImportHashed where
 instance Canonicalize Import where
     canonicalize (Import importHashed importMode) =
         Import (canonicalize importHashed) importMode
-
--- Given a well-typed (of type `List { header : Text, value Text }` or
--- `List { mapKey : Text, mapValue Text }`) headers expressions in normal form
--- construct the corresponding binary http headers.
-toHeaders :: Expr s a -> Maybe [HTTPHeader]
-toHeaders (ListLit _ hs) = do
-    hs' <- mapM toHeader hs
-    return (Data.Foldable.toList hs')
-toHeaders _ = do
-    empty
-
-toHeader :: Expr s a -> Maybe HTTPHeader
-toHeader (RecordLit m) = do
-    TextLit (Chunks [] keyText  ) <-
-        Dhall.Map.lookup "header" m <|> Dhall.Map.lookup "mapKey" m
-    TextLit (Chunks [] valueText) <-
-        Dhall.Map.lookup "value" m <|> Dhall.Map.lookup "mapValue" m
-    let keyBytes   = Data.Text.Encoding.encodeUtf8 keyText
-    let valueBytes = Data.Text.Encoding.encodeUtf8 valueText
-    return (Data.CaseInsensitive.mk keyBytes, valueBytes)
-toHeader _ = do
-    empty
-
 
 -- | Exception thrown when an integrity check fails
 data HashMismatch = HashMismatch
@@ -663,9 +642,9 @@ loadImportWithSemisemanticCache (Chained (Import (ImportHashed _ importType) Loc
             local@(Local _ _) ->
                 App (Field locationType "Local")
                   (TextLit (Chunks [] (Dhall.Pretty.Internal.pretty local)))
-            remote@(Remote _) ->
+            remote_@(Remote _) ->
                 App (Field locationType "Remote")
-                  (TextLit (Chunks [] (Dhall.Pretty.Internal.pretty remote)))
+                  (TextLit (Chunks [] (Dhall.Pretty.Internal.pretty remote_)))
             Env env ->
                 App (Field locationType "Environment")
                   (TextLit (Chunks [] (Dhall.Pretty.Internal.pretty env)))
@@ -704,16 +683,9 @@ fetchFresh (Local prefix file) = do
         then liftIO $ Data.Text.IO.readFile path
         else throwMissingImport (Imported _stack (MissingFile path))
 
-fetchFresh (Remote (url@URL { headers = maybeHeadersExpression })) = do
-#ifdef MIN_VERSION_http_client
-    let maybeHeaders = foldMap toHeaders maybeHeadersExpression
-    fetchFromHttpUrl url maybeHeaders
-#else
-    let maybeHeaders = foldMap toHeaders maybeHeadersExpression
-    let urlString = Text.unpack (Dhall.Core.pretty url)
-    Status { _stack } <- State.get
-    throwMissingImport (Imported _stack (CannotImportHTTPURL urlString maybeHeaders))
-#endif
+fetchFresh (Remote url) = do
+    Status { _remote } <- State.get
+    _remote url
 
 fetchFresh (Env env) = do
     Status { _stack } <- State.get
@@ -725,6 +697,48 @@ fetchFresh (Env env) = do
                 throwMissingImport (Imported _stack (MissingEnvironmentVariable env))
 
 fetchFresh Missing = throwM (MissingImports [])
+
+
+fetchRemote :: URL -> StateT Status IO Data.Text.Text
+#ifndef MIN_VERSION_http_client
+fetchRemote (url@URL { headers = maybeHeadersExpression }) = do
+    let maybeHeaders = fmap toHeaders maybeHeadersExpression
+    let urlString = Text.unpack (Dhall.Core.pretty url)
+    Status { _stack } <- State.get
+    throwMissingImport (Imported _stack (CannotImportHTTPURL urlString maybeHeaders))
+#else
+fetchRemote url = do
+    manager <- liftIO $ newManager
+    zoom remote (State.put (fetchFromHTTP manager))
+    fetchFromHTTP manager url
+  where
+    fetchFromHTTP :: Manager -> URL -> StateT Status IO Data.Text.Text
+    fetchFromHTTP manager (url'@URL { headers = maybeHeadersExpression }) = do
+        let maybeHeaders = fmap toHeaders maybeHeadersExpression
+        fetchFromHttpUrl manager url' maybeHeaders
+#endif
+
+-- | Given a well-typed (of type `List { header : Text, value Text }` or
+-- `List { mapKey : Text, mapValue Text }`) headers expressions in normal form
+-- construct the corresponding binary http headers; otherwise return the empty
+-- list.
+toHeaders :: Expr s a -> [HTTPHeader]
+toHeaders (ListLit _ hs) = Data.Foldable.toList (Data.Foldable.fold maybeHeaders)
+  where
+      maybeHeaders = mapM toHeader hs
+toHeaders _ = []
+
+toHeader :: Expr s a -> Maybe HTTPHeader
+toHeader (RecordLit m) = do
+    TextLit (Chunks [] keyText  ) <-
+        Dhall.Map.lookup "header" m <|> Dhall.Map.lookup "mapKey" m
+    TextLit (Chunks [] valueText) <-
+        Dhall.Map.lookup "value" m <|> Dhall.Map.lookup "mapValue" m
+    let keyBytes   = Data.Text.Encoding.encodeUtf8 keyText
+    let valueBytes = Data.Text.Encoding.encodeUtf8 valueText
+    return (Data.CaseInsensitive.mk keyBytes, valueBytes)
+toHeader _ = do
+    empty
 
 getCacheFile
     :: (Alternative m, MonadIO m) => FilePath -> Crypto.Hash.Digest SHA256 -> m FilePath
@@ -831,6 +845,10 @@ normalizeHeaders url@URL { headers = Just headersExpression } = do
     return url { headers = Just (fmap absurd headersExpression') }
 
 normalizeHeaders url = return url
+
+-- | Default starting `Status`, importing relative to the given directory.
+emptyStatus :: FilePath -> Status
+emptyStatus = emptyStatusWith fetchRemote
 
 {-| Generalized version of `load`
 

dhall/src/Dhall/Import/Types.hs
@@ -5,6 +5,7 @@
 module Dhall.Import.Types where
 
 import Control.Exception (Exception)
+import Control.Monad.Trans.State.Strict (StateT)
 import Data.Dynamic
 import Data.List.NonEmpty (NonEmpty)
 import Data.Map (Map)
@@ -22,6 +23,7 @@ import Dhall.Core
   , ImportMode (..)
   , ImportType (..)
   , ReifiedNormalizer(..)
+  , URL
   )
 import Dhall.Parser (Src)
 import Dhall.TypeCheck (X)
@@ -66,8 +68,8 @@ data Status = Status
     -- ^ Cache of imported expressions with their node id in order to avoid
     --   importing the same expression twice with different values
 
-    , _manager :: Maybe Dynamic
-    -- ^ Cache for the HTTP `Manager` so that we only acquire it once
+    , _remote :: URL -> StateT Status IO Data.Text.Text
+    -- ^ The remote resolver, fetches the content at the given URL.
 
     , _standardVersion :: StandardVersion
 
@@ -76,9 +78,10 @@ data Status = Status
     , _startingContext :: Context (Expr Src X)
     }
 
--- | Default starting `Status`, importing relative to the given directory.
-emptyStatus :: FilePath -> Status
-emptyStatus rootDirectory = Status {..}
+-- | Initial `Status`, parameterised over the remote resolver, importing
+--   relative to the given directory.
+emptyStatusWith :: (URL -> StateT Status IO Data.Text.Text) -> FilePath -> Status
+emptyStatusWith _remote rootDirectory = Status {..}
   where
     _stack = pure (Chained rootImport)
 
@@ -120,8 +123,8 @@ graph k s = fmap (\x -> s { _graph = x }) (k (_graph s))
 cache :: Functor f => LensLike' f Status (Map Chained ImportSemantics)
 cache k s = fmap (\x -> s { _cache = x }) (k (_cache s))
 
-manager :: Functor f => LensLike' f Status (Maybe Dynamic)
-manager k s = fmap (\x -> s { _manager = x }) (k (_manager s))
+remote :: Functor f => LensLike' f Status (URL -> StateT Status IO Data.Text.Text)
+remote k s = fmap (\x -> s { _remote = x }) (k (_remote s))
 
 standardVersion :: Functor f => LensLike' f Status StandardVersion
 standardVersion k s =

dhall/src/Dhall/Import/HTTP.hs
@@ -5,15 +5,13 @@
 module Dhall.Import.HTTP where
 
 import Control.Exception (Exception)
-import Control.Monad (join)
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.Trans.State.Strict (StateT)
 import Data.ByteString (ByteString)
 import Data.CaseInsensitive (CI)
-import Data.Dynamic (fromDynamic, toDyn)
+import Data.Dynamic (toDyn)
 import Data.Semigroup ((<>))
 import Data.Text (Text)
-import Lens.Family.State.Strict (zoom)
 
 import Dhall.Core
     ( Directory(..)
@@ -109,24 +107,17 @@ renderPrettyHttpException e = case e of
         <> show e'
 #endif
 
-needManager :: StateT Status IO Manager
-needManager = do
-    x <- zoom manager State.get
-    case join (fmap fromDynamic x) of
-        Just m  -> return m
-        Nothing -> do
-            let settings = HTTP.tlsManagerSettings
-
+newManager :: IO Manager
+newManager = do
+    let settings = HTTP.tlsManagerSettings
 #ifdef MIN_VERSION_http_client
 #if MIN_VERSION_http_client(0,5,0)
-                    { HTTP.managerResponseTimeout = HTTP.responseTimeoutMicro (30 * 1000 * 1000) }  -- 30 seconds
+          { HTTP.managerResponseTimeout = HTTP.responseTimeoutMicro (30 * 1000 * 1000) }  -- 30 seconds
 #else
-                    { HTTP.managerResponseTimeout = Just (30 * 1000 * 1000) }  -- 30 seconds
+          { HTTP.managerResponseTimeout = Just (30 * 1000 * 1000) }  -- 30 seconds
 #endif
 #endif
-            m <- liftIO (HTTP.newManager settings)
-            zoom manager (State.put (Just (toDyn m)))
-            return m
+    HTTP.newManager settings
 
 data NotCORSCompliant = NotCORSCompliant
     { expectedOrigins :: [ByteString]
@@ -235,11 +226,12 @@ renderURL url =
 type HTTPHeader = Network.HTTP.Types.Header
 
 fetchFromHttpUrl
-    :: URL
+    :: Manager
+    -> URL
     -> Maybe [HTTPHeader]
     -> StateT Status IO Text.Text
 #ifdef __GHCJS__
-fetchFromHttpUrl childURL Nothing = do
+fetchFromHttpUrl _ childURL Nothing = do
     let childURLText = renderURL childURL
 
     let childURLString = Text.unpack childURLText
@@ -253,13 +245,11 @@ fetchFromHttpUrl childURL Nothing = do
         _   -> fail (childURLString <> " returned a non-200 status code: " <> show statusCode)
 
     return body
-fetchFromHttpUrl _ _ = do
+fetchFromHttpUrl _ _ _ = do
     fail "Dhall does not yet support custom headers when built using GHCJS"
 #else
-fetchFromHttpUrl childURL mheaders = do
+fetchFromHttpUrl manager childURL mheaders = do
     let childURLString = Text.unpack (renderURL childURL)
-
-    m <- needManager
 
     request <- liftIO (HTTP.parseUrlThrow childURLString)
 
@@ -268,7 +258,7 @@ fetchFromHttpUrl childURL mheaders = do
               Nothing      -> request
               Just headers -> request { HTTP.requestHeaders = headers }
 
-    let io = HTTP.httpLbs requestWithHeaders m
+    let io = HTTP.httpLbs requestWithHeaders manager
 
     let handler e = do
             let _ = e :: HttpException

dhall/tests/Dhall/Test/Import.hs
@@ -58,7 +58,7 @@ successTest path = do
         let unsetCache = Turtle.unset "XDG_CACHE_HOME"
 
         let load =
-                State.evalStateT (Import.loadWith actualExpr) (Import.emptyStatus directoryString)
+                State.evalStateT (Test.Util.loadWith actualExpr) (Import.emptyStatus directoryString)
 
         if Turtle.filename (Turtle.fromText path) == "hashFromCacheA.dhall"
             then do
@@ -81,7 +81,7 @@ failureTest path = do
         actualExpr <- Core.throws (Parser.exprFromText mempty text)
 
         Exception.catch
-          (do _ <- Import.load actualExpr
+          (do _ <- Test.Util.load actualExpr
 
               fail "Import should have failed, but it succeeds")
           (\(SourcedException _ (MissingImports _)) -> pure ()) )

dhall/tests/Dhall/Test/TypeCheck.hs
@@ -13,7 +13,6 @@ import qualified Control.Exception as Exception
 import qualified Control.Monad     as Monad
 import qualified Data.Text         as Text
 import qualified Dhall.Core        as Core
-import qualified Dhall.Import      as Import
 import qualified Dhall.Parser      as Parser
 import qualified Dhall.Test.Util   as Test.Util
 import qualified Dhall.TypeCheck   as TypeCheck
@@ -58,7 +57,7 @@ successTest prefix =
 
         let annotatedExpr = Core.Annot actualExpr expectedExpr
 
-        resolvedExpr <- Import.load annotatedExpr
+        resolvedExpr <- Test.Util.load annotatedExpr
 
         _ <- Core.throws (TypeCheck.typeOf resolvedExpr)
 
@@ -73,7 +72,7 @@ failureTest path = do
 
         let io :: IO Bool
             io = do
-                _ <- Import.load expression
+                _ <- Test.Util.load expression
                 return True
 
         let handler :: SomeException -> IO Bool

dhall/tests/Dhall/Test/Util.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE CPP #-}
 
 module Dhall.Test.Util
     ( code
     , codeWith
     , equivalent
+    , load
+    , loadWith
     , normalize'
     , normalizeWith'
     , assertNormalizesTo
@@ -16,10 +19,12 @@ module Dhall.Test.Util
     , toDhallPath
     ) where
 
+import Control.Monad.Trans.State.Strict (StateT)
 import Data.Bifunctor (first)
 import Data.Text (Text)
 import Dhall.Context (Context)
-import Dhall.Core (Expr, Normalizer, ReifiedNormalizer(..))
+import Dhall.Core (Expr, Normalizer, ReifiedNormalizer(..), Import)
+import Dhall.Import (Status)
 import Dhall.Parser (Src)
 import Dhall.TypeCheck (X)
 import Prelude hiding (FilePath)
@@ -36,8 +41,19 @@ import qualified Dhall.Core
 import qualified Dhall.Import
 import qualified Dhall.Parser
 import qualified Dhall.TypeCheck
+import qualified Control.Monad.Trans.State.Strict as State
 import qualified Test.Tasty        as Tasty
 import qualified Turtle
+
+#ifndef WITH_HTTP
+import Control.Monad.IO.Class (MonadIO(..))
+import Dhall.Core (URL(..))
+import Lens.Family.State.Strict (zoom)
+
+import qualified Data.Foldable
+import qualified Data.Text.Encoding
+import qualified Data.Text.IO
+#endif
 
 normalize' :: Expr Src X -> Text
 normalize' = Dhall.Core.pretty . Dhall.Core.normalize
@@ -54,11 +70,78 @@ codeWith ctx expr = do
     expr0 <- case Dhall.Parser.exprFromText mempty expr of
         Left parseError -> Control.Exception.throwIO parseError
         Right expr0     -> return expr0
-    expr1 <- Dhall.Import.load expr0
+    expr1 <- load expr0
     case Dhall.TypeCheck.typeWith ctx expr1 of
         Left typeError -> Control.Exception.throwIO typeError
         Right _        -> return ()
     return expr1
+
+load :: Expr Src Import -> IO (Expr Src X)
+load expression =
+    State.evalStateT (loadWith expression) (Dhall.Import.emptyStatus ".")
+
+#ifdef WITH_HTTP
+loadWith :: Expr Src Import -> StateT Status IO (Expr Src X)
+loadWith = Dhall.Import.loadWith
+
+#else
+loadWith :: Expr Src Import -> StateT Status IO (Expr Src X)
+loadWith expr = do
+    let mockRemote' url = do
+            liftIO . putStrLn $ "\nTesting without real HTTP support --"
+                ++ " using mock HTTP client to resolve remote import."
+            mockRemote url
+    zoom Dhall.Import.remote (State.put mockRemote')
+    Dhall.Import.loadWith expr
+
+mockRemote :: Dhall.Core.URL -> StateT Status IO Data.Text.Text
+-- Matches anything pointing to
+-- `https://raw.githubusercontent.com/dhall-lang/dhall-lang/master/`
+mockRemote (URL { authority = "raw.githubusercontent.com"
+                , path = Dhall.Core.File (Dhall.Core.Directory components) file })
+  | take 3 (reverse components) == ["dhall-lang", "dhall-lang", "master"] = do
+    let dropEnd n ls = take (length ls - n) ls
+    let localDir = dropEnd 3 components ++ ["dhall-lang"]
+
+    localPath <- Dhall.Import.localToPath Dhall.Core.Here (Dhall.Core.File (Dhall.Core.Directory localDir) file)
+    liftIO $ Data.Text.IO.readFile localPath
+
+-- Matches anything pointing to
+-- `https://test.dhall-lang.org/Bool/package.dhall`; checks that a `test` header
+-- is present and redirects to the local copy of the prelude.
+mockRemote (URL { authority = "test.dhall-lang.org"
+                , path = Dhall.Core.File (Dhall.Core.Directory components) file
+                , headers = Just headersExpr }) =
+    case Data.Foldable.find ((== "test") . fst) hs of
+        Nothing -> fail $ "(mock http) Tried to load an import from "
+                          ++"\"test.dhall-lang.org\""
+                          ++ "without setting the \"test\" header field."
+        Just (_, _) -> do
+            let localDir = components ++ ["Prelude", "dhall-lang"]
+            localPath <- Dhall.Import.localToPath Dhall.Core.Here (Dhall.Core.File (Dhall.Core.Directory localDir) file)
+            liftIO $ Data.Text.IO.readFile localPath
+  where
+    hs = Dhall.Import.toHeaders headersExpr
+
+-- Emulates `https://httpbin.org/user-agent`
+mockRemote (URL { authority = "httpbin.org"
+                , path = Dhall.Core.File (Dhall.Core.Directory []) "user-agent"
+                , headers = Just headersExpr }) =
+    case Data.Foldable.find ((== "user-agent") . fst) hs of
+        Nothing -> fail $ "(mock http) Tried to read the user agent via "
+                          ++ "\"httpbin.com/user-agent\" without supplying one "
+                          ++ "in the header!"
+        Just (_, userAgent) -> do
+            let agentText = Data.Text.Encoding.decodeUtf8 userAgent
+            return ("{\n  \"user-agent\": \"" <> agentText <> "\"\n}\n")
+  where
+    hs = Dhall.Import.toHeaders headersExpr
+
+mockRemote url = do
+    let urlString = Text.unpack (Dhall.Core.pretty url)
+    fail ("(mock http) Url does not match any of the hard-coded rules: "
+        <> urlString)
+#endif
 
 equivalent :: Text -> Text -> IO ()
 equivalent text0 text1 = do
@@ -93,7 +176,7 @@ assertDoesntTypeCheck text = do
     expr0 <- case Dhall.Parser.exprFromText mempty text of
         Left parseError -> Control.Exception.throwIO parseError
         Right e         -> return e
-    expr1 <- Dhall.Import.load expr0
+    expr1 <- load expr0
     case Dhall.TypeCheck.typeOf expr1 of
         Left _      -> return ()
         Right type_ -> fail ("Bad type for " <> Text.unpack text <> "\n  " <> show type_)
@@ -104,9 +187,9 @@ assertDoesntTypeCheck text = do
 discover :: Pattern Text -> (Text -> TestTree) -> Shell FilePath -> IO TestTree
 discover pattern buildTest paths = do
     let shell = do
-            path <- paths
+            path_ <- paths
 
-            let pathText = Turtle.format fp path
+            let pathText = Turtle.format fp path_
 
             prefix : _ <- return (Turtle.match pattern pathText)
 

nix/shared.nix
@@ -169,6 +169,11 @@ let
                           { }
                         );
 
+                    dhall-no-http =
+                      pkgsNew.haskell.lib.appendConfigureFlag
+                        haskellPackagesNew.dhall
+                        [ "-f-with-http" ];
+
                     dhall-bash =
                       haskellPackagesNew.callCabal2nix
                         "dhall-bash"
@@ -612,7 +617,15 @@ in
 
     inherit (pkgs) tarball-website website;
 
-    inherit (pkgs.haskell.packages."${compiler}") dhall dhall-bash dhall-json dhall-lsp-server dhall-nix dhall-try;
+    inherit (pkgs.haskell.packages."${compiler}")
+      dhall
+      dhall-no-http
+      dhall-bash
+      dhall-json
+      dhall-lsp-server
+      dhall-nix
+      dhall-try
+    ;
 
     inherit (pkgs.releaseTools) aggregate;
 

commit 8ed0aa870fb5285f0b5085459ff46b5123629b5e

Fix import resolution performance regression
Related to https://github.com/dhall-lang/dhall-haskell/issues/1511

This fixes a performance regression introduced in #1159 where `newManager`
was being called on every remote import.  This fixes that by going back to
caching the `Manager` created by the first request.

This leads to *dramatic* performance improvements for import-rich packages
(like the Prelude or `dhall-kubernetes`) on the first import.  For example,
here are the performance numbers for importing the Prelude for a cold cache
before and after this change:

Before:

```
$ XDG_CACHE_HOME=.cache time dhall hash <<< 'https://prelude.dhall-lang.org/package.dhall'
sha256:99462c205117931c0919f155a6046aec140c70fb8876d208c7c77027ab19c2fa
       64.10 real        10.83 user         2.73 sys
```

After:

```
$ XDG_CACHE_HOME=.cache2 time dhall hash <<< 'https://prelude.dhall-lang.org/package.dhall'
sha256:99462c205117931c0919f155a6046aec140c70fb8876d208c7c77027ab19c2fa
        4.39 real         0.49 user         0.15 sys
```

That's ~16x faster!

The improvement for `dhall-kubernetes` is smaller, but still significant:

Before:

```
$ XDG_CACHE_HOME=.cache3 time dhall hash <<< ~/proj/dhall-kubernetes-charts/stable/jenkins/index.dhall
sha256:04ebd960f6af331c49c3ccaedb353ac8269032b54fe0a29bd167febcd7104d4f
      833.59 real       145.36 user        36.16 sys

After:

```
$ XDG_CACHE_HOME=.cache4 time dhall hash <<< ~/proj/dhall-kubernetes-charts/stable/jenkins/index.dhall
sha256:04ebd960f6af331c49c3ccaedb353ac8269032b54fe0a29bd167febcd7104d4f
      381.41 real         8.41 user         1.91 sys
```

... or ~2-3x improvement.

dhall/dhall.cabal
@@ -535,6 +535,7 @@ Library
     if flag(with-http)
       Other-Modules:
         Dhall.Import.HTTP
+        Dhall.Import.Manager
 
     GHC-Options: -Wall -fwarn-incomplete-uni-patterns
     Default-Language: Haskell2010

dhall/ghc-src/Dhall/Import/HTTP.hs
@@ -2,7 +2,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
-module Dhall.Import.HTTP where
+module Dhall.Import.HTTP
+    ( fetchFromHttpUrl
+    ) where
 
 import Control.Exception (Exception)
 import Control.Monad.IO.Class (MonadIO(..))
@@ -169,7 +171,7 @@ renderPrettyHttpException url e = case e of
         <>  show e' <> "\n"
 #endif
 
-newManager :: IO Manager
+newManager :: StateT Status IO Manager
 newManager = do
     let settings = HTTP.tlsManagerSettings
 #ifdef MIN_VERSION_http_client
@@ -179,7 +181,19 @@ newManager = do
           { HTTP.managerResponseTimeout = Just (30 * 1000 * 1000) }  -- 30 seconds
 #endif
 #endif
-    HTTP.newManager settings
+
+    Status { _manager = oldManager, ..} <- State.get
+
+    case oldManager of
+        Nothing -> do
+            manager <- liftIO (HTTP.newManager settings)
+
+            State.put (Status { _manager = Just manager , ..})
+
+            return manager
+
+        Just manager -> do
+            return manager
 
 data NotCORSCompliant = NotCORSCompliant
     { expectedOrigins :: [ByteString]
@@ -260,7 +274,7 @@ type HTTPHeader = Network.HTTP.Types.Header
 
 fetchFromHttpUrl :: URL -> Maybe [HTTPHeader] -> StateT Status IO Text.Text
 fetchFromHttpUrl childURL mheaders = do
-    manager <- liftIO $ newManager
+    manager <- newManager
 
     let childURLString = Text.unpack (renderURL childURL)
 

dhall/ghc-src/Dhall/Import/Manager.hs
@@ -1,0 +1,14 @@
+{-| Both the GHC and GHCJS implementations of "Dhall.Import.Manager" export a
+    `Manager` type suitable for use within the "Dhall.Import" module
+
+    For the GHC implementation the `Manager` type is a real `Manager` from the
+    @http-client@ package.  For the GHCJS implementation the `Manager` type is
+    a synonym for @`Data.Void.Void`@ since GHCJS does not use a `Manager` for
+    HTTP requests.
+-}
+module Dhall.Import.Manager
+    ( -- * Manager
+      Manager
+    ) where
+
+import Network.HTTP.Client (Manager)

dhall/ghcjs-src/Dhall/Import/HTTP.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
-module Dhall.Import.HTTP where
+module Dhall.Import.HTTP
+    ( fetchFromHttpUrl
+    , Manager
+    ) where
 
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.Trans.State.Strict (StateT)
@@ -12,9 +15,18 @@ import Data.Semigroup ((<>))
 import qualified Data.Text as Text
 import qualified JavaScript.XHR
 
+import Data.Void (Void)
 import Dhall.Core (URL(..))
 import Dhall.URL (renderURL)
 import Dhall.Import.Types
+
+{-| The GHCJS implementation does not require a `Manager`
+
+    The purpose of this synonym is so that "Dhall.Import.Types" can import a
+    `Manager` type from "Dhall.Import.HTTP" that does the correct thing for
+    both the GHC and GHCJS implementations
+-}
+type Manager = Void
 
 fetchFromHttpUrl
     :: URL

dhall/ghcjs-src/Dhall/Import/Manager.hs
@@ -1,0 +1,17 @@
+{-| Both the GHC and GHCJS implementations of "Dhall.Import.Manager" export a
+    `Manager` type suitable for use within the "Dhall.Import" module
+
+    For the GHC implementation the `Manager` type is a real `Manager` from the
+    @http-client@ package.  For the GHCJS implementation the `Manager` type is
+    a synonym for @`Data.Void.Void`@ since GHCJS does not use a `Manager` for
+    HTTP requests.
+-}
+module Dhall.Import.Manager
+    ( -- * Manager
+      Manager
+    ) where
+
+import Data.Void (Void)
+
+-- | GHCJS does not use a `Manager`
+type Manager = Void

dhall/src/Dhall/Import.hs
@@ -172,7 +172,7 @@ import Dhall.Syntax
     , chunkExprs
     )
 #ifdef WITH_HTTP
-import Dhall.Import.HTTP hiding (HTTPHeader)
+import Dhall.Import.HTTP
 #endif
 import Dhall.Import.Types
 

dhall/src/Dhall/Import/Types.hs
@@ -25,6 +25,7 @@ import Dhall.Core
   , ReifiedNormalizer(..)
   , URL
   )
+import Dhall.Import.Manager (Manager)
 import Dhall.Parser (Src)
 import Lens.Family (LensLike')
 import System.FilePath (isRelative, splitDirectories)
@@ -75,6 +76,9 @@ data Status = Status
     -- ^ Cache of imported expressions with their node id in order to avoid
     --   importing the same expression twice with different values
 
+    , _manager :: Maybe Manager
+    -- ^ Used to cache the `Manager` when making multiple requests
+
     , _remote :: URL -> StateT Status IO Data.Text.Text
     -- ^ The remote resolver, fetches the content at the given URL.
 
@@ -95,6 +99,8 @@ emptyStatusWith _remote rootDirectory = Status {..}
     _graph = []
 
     _cache = Map.empty
+
+    _manager = Nothing
 
     _normalizer = Nothing
 