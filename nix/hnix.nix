446710e09888593b6bda1c47947a370bf0ff3703
Simon Jakobi <simon.jakobi@gmail.com>
2019-11-13 21:28:41 +0100

    nix/hnix.nix: Use hnix master
    Command used:
    
        cabal2nix git://github.com/haskell-nix/hnix.git --dont-fetch-submodules
    
    (within nix-shell)

--- nix/hnix.nix
+++ nix/hnix.nix
@@ -2,22 +2,28 @@
 , bytestring, comonad, containers, contravariant, criterion
 , cryptohash-md5, cryptohash-sha1, cryptohash-sha256
 , cryptohash-sha512, data-fix, deepseq, dependent-sum
-, deriving-compat, Diff, directory, exceptions, filepath, free
-, generic-random, Glob, hashable, hashing, haskeline, hedgehog
-, hnix-store-core, http-client, http-client-tls, http-types
-, interpolate, lens-family, lens-family-core, lens-family-th
-, logict, megaparsec, monad-control, monadlist, mtl
+, deriving-compat, Diff, directory, exceptions, fetchgit, filepath
+, free, generic-random, Glob, hashable, hashing, haskeline
+, hedgehog, hnix-store-core, http-client, http-client-tls
+, http-types, interpolate, lens-family, lens-family-core
+, lens-family-th, logict, megaparsec, monad-control, monadlist, mtl
 , optparse-applicative, parser-combinators, pretty-show
 , prettyprinter, process, ref-tf, regex-tdfa, regex-tdfa-text
-, repline, scientific, semigroups, serialise, split, stdenv, syb
-, tasty, tasty-hedgehog, tasty-hunit, tasty-quickcheck, tasty-th
-, template-haskell, text, these, time, transformers
-, transformers-base, unix, unordered-containers, vector, xml
+, repline, scientific, semialign, semialign-indexed, semigroups
+, serialise, split, stdenv, syb, tasty, tasty-hedgehog, tasty-hunit
+, tasty-quickcheck, tasty-th, template-haskell, text, these, time
+, transformers, transformers-base, unix, unordered-containers
+, vector, xml
 }:
 mkDerivation {
   pname = "hnix";
   version = "0.6.1";
-  sha256 = "914b98e6d3e0c7f17308b3a459cae8eb717ade65c75983edac08fd5173e3e960";
+  src = fetchgit {
+    url = "git://github.com/haskell-nix/hnix.git";
+    sha256 = "1hckwhv1gnsdw0v2lm6hqshy5hh9pz1j37adysfqc4a4w4jgkc7z";
+    rev = "8ce466ee803abc5d1ccd56b1905b261c728ac4d3";
+    fetchSubmodules = false;
+  };
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
@@ -29,9 +35,9 @@ mkDerivation {
     interpolate lens-family lens-family-core lens-family-th logict
     megaparsec monad-control monadlist mtl optparse-applicative
     parser-combinators pretty-show prettyprinter process ref-tf
-    regex-tdfa regex-tdfa-text scientific semigroups serialise split
-    syb template-haskell text these time transformers transformers-base
-    unix unordered-containers vector xml
+    regex-tdfa regex-tdfa-text scientific semialign semialign-indexed
+    semigroups serialise split syb template-haskell text these time
+    transformers transformers-base unix unordered-containers vector xml
   ];
   executableHaskellDepends = [
     aeson base base16-bytestring bytestring comonad containers
