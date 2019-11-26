d9121bcd063467f5fbfb330a26911dcf624b600a
quasicomputational <quasicomputational@gmail.com>
2019-11-26 11:04:04 +0000

    Nix: Use v2-sdist.
    This doesn't need a GHC, which was blocking #1125.

--- nix/sdist.nix
+++ nix/sdist.nix
@@ -1,33 +1,14 @@
-{ cabal-install, coreutils, glibcLocales, lib, runCommand, stdenv }:
+{ cabal-install, coreutils, glibcLocales, lib, runCommand, stdenv, tar }:
 
 src:
 
-let
-  predicate = path: type:
-    let
-      base = baseNameOf path;
-
-    in
-       !( lib.hasSuffix ".nix" base
-       || base == "dist"
-       || base == "result"
-       || base == ".git"
-       || base == "index.html"
-       );
-
-  filteredSource = builtins.filterSource predicate src;
-
-in
-  runCommand "dhall-sdist"
-    (   { LANG = "en_US.UTF-8"; }
-    //  lib.optionalAttrs (stdenv.buildPlatform.libc == "glibc")
-          { LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive"; }
-    )
-    ''
-    ${coreutils}/bin/mkdir $out
-    ${coreutils}/bin/cp --recursive ${filteredSource} src
-    cd src
-    ${coreutils}/bin/chmod --recursive u+w .
-    ${coreutils}/bin/mkdir dist
-    HOME=$PWD ${cabal-install}/bin/cabal sdist --output-directory=$out
-    ''
+runCommand "dhall-sdist"
+  (   { LANG = "en_US.UTF-8"; }
+  //  lib.optionalAttrs (stdenv.buildPlatform.libc == "glibc")
+        { LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive"; }
+  )
+  ''
+  ${coreutils}/bin/mkdir $out
+  HOME=$PWD ${cabal-install}/bin/cabal v2-sdist --output-dir=- ${src} \
+  | ${tar}/bin/tar --extract -z --strip-components=1 -C $out -f -
+  ''
