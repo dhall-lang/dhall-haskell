4905db7ebe5ff19383aa69a20d32a54cb8e1a593
Mann mit Hut <mmh@riseup.net>
2020-02-02 05:52:03 +0100

    Added some Haddock documentation

--- dhall/src/Dhall/Substitution.hs
+++ dhall/src/Dhall/Substitution.hs
@@ -5,10 +5,17 @@ import Dhall.Syntax (Expr, Var)
 
 import qualified Data.Map
 
+{- | Substitutions map variables to arbitrary Dhall expressions.
+-}
 type Substitutions s a = Data.Map.Map Var (Expr s a)
 
+{- | An empty substitution map.
+-}
 empty :: Substitutions s a
 empty = Data.Map.empty
 
+{- | `substitute expr s` replaces all variables in `expr` (or its subexpression) with their substitute.
+     For example, if the substitution map maps the Variable `Foo` to the text \"Foo\" all occurrences of `Foo` with the text \"Foo\".
+-}
 substitute :: Expr s a -> Substitutions s a -> Expr s a
 substitute = Data.Map.foldrWithKey subst
