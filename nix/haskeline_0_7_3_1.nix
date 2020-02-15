dhall-lang/dhall-haskell

commit ce7b4be361ba935dbc439ceb86b70bf2312fd62a

Drop support for GHC 7.10.3 (#1649)
The immediate motivation for doing this is so that we don't need to
introduce yet another GHC 7.10.3 workaround for
https://github.com/dhall-lang/dhall-haskell/pull/1640

More generally, the main reason we were still supporting GHC 7.10.3
was for Eta compatibility, but Eta appears to no longer be actively
developed.  Given that Eta seems unlikely to support GHC 8 and we
don't plan on supporting GHC 7.10.3 in perpetuity it's probably
time to drop support.

We could probably drop support for additional GHC versions, but this
is just the smallest change necessary to unblock #1640.

nix/haskeline_0_7_3_1.nix
@@ -1,16 +1,0 @@
-{ mkDerivation, base, bytestring, containers, directory, filepath
-, process, stdenv, stm, transformers, unix
-}:
-mkDerivation {
-  pname = "haskeline";
-  version = "0.7.4.3";
-  sha256 = "046d0930bc2dbc57a7cd9ddb5d1e92c7fdb71c6b91b2bbf673f5406843d6b679";
-  configureFlags = [ "-f-terminfo" ];
-  libraryHaskellDepends = [
-    base bytestring containers directory filepath process stm
-    transformers unix
-  ];
-  homepage = "https://github.com/judah/haskeline";
-  description = "A command-line interface for user input, written in Haskell";
-  license = stdenv.lib.licenses.bsd3;
-}

release.nix
@@ -9,7 +9,7 @@ let
   callShared = args:
     import ./nix/shared.nix ({ inherit nixpkgs nixpkgsStaticLinux; } // args);
 
-  shared_7_10_3 = callShared { compiler = "ghc7103"; };
+  shared_8_0_2 = callShared { compiler = "ghc802"; };
 
   shared_8_6_1 = callShared { compiler = "ghc861"; };
 
@@ -28,15 +28,15 @@ in
         constituents = [
           # Verify that the packages build against the oldest supported version
           # of the compiler
-          shared_7_10_3.dhall
-          shared_7_10_3.dhall-bash
-          shared_7_10_3.dhall-json
-          # `HsYAML-aeson` requires Cabal >= 2.2
-          # shared_7_10_3.dhall-yaml
+          shared_8_0_2.dhall
+          shared_8_0_2.dhall-bash
+          shared_8_0_2.dhall-json
+          # `dhall-nix` → `hnix-store-core` → `4.10 <= base` → `8.2.1 <= ghc`
+          # shared_8_0_2.dhall-nix
+          # `HsYAML-aeson` requires Cabal >= 2.2, which implies GHC 8.4 or newer
+          # shared_8_0_2.dhall-yaml
           # `base-noprelude` depends on a specific version of `base`
-          # shared_7_10_3.dhall-lsp-server
-          # `hnix` depends on `lens-family-th`, which doesn't support GHC 7.10.3
-          # shared_7_10_3.dhall-nix
+          # shared_8_0_2.dhall-lsp-server
 
           # Verify that the packages build against the latest supported version
           # of the compiler

appveyor.yml
@@ -17,7 +17,6 @@ environment:
 
   matrix:
     - STACK_YAML: stack.yaml
-    - STACK_YAML: stack-lts-6.yaml
 
 install:
   # http://help.appveyor.com/discussions/problems/6312-curl-command-not-found
@@ -34,48 +33,29 @@ cache:
   - 'dhall/.stack-work -> %STACK_YAML%, appveyor.yml'
   - 'dhall-bash/.stack-work -> %STACK_YAML%, appveyor.yml'
 
-for:
--
-  matrix:
-    except:
-      - STACK_YAML: stack-lts-6.yaml
+build_script:
+  - stack build
+  - stack install --local-bin-path bin
+  - if /I "%APPVEYOR_REPO_TAG%" EQU "true" (set DEPLOY_TAG=%APPVEYOR_REPO_TAG_NAME%-x86_64-windows.zip)
+  - set DEPLOY_DIR=%APPVEYOR_BUILD_FOLDER%\bin
+  - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall.exe")
+  - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-json-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall-to-json.exe")
+  - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-json-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall-to-yaml.exe")
+  - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-json-%DEPLOY_TAG%" "%DEPLOY_DIR%\json-to-dhall.exe")
+  - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-bash-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall-to-bash.exe")
+  - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-lsp-server-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall-lsp-server.exe")
+  - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-yaml-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall-to-yaml-ng.exe")
+  - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-yaml-%DEPLOY_TAG%" "%DEPLOY_DIR%\yaml-to-dhall.exe")
 
-  build_script:
-    - stack build
-    - stack install --local-bin-path bin
-    - if /I "%APPVEYOR_REPO_TAG%" EQU "true" (set DEPLOY_TAG=%APPVEYOR_REPO_TAG_NAME%-x86_64-windows.zip)
-    - set DEPLOY_DIR=%APPVEYOR_BUILD_FOLDER%\bin
-    - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall.exe")
-    - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-json-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall-to-json.exe")
-    - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-json-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall-to-yaml.exe")
-    - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-json-%DEPLOY_TAG%" "%DEPLOY_DIR%\json-to-dhall.exe")
-    - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-bash-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall-to-bash.exe")
-    - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-lsp-server-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall-lsp-server.exe")
-    - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-yaml-%DEPLOY_TAG%" "%DEPLOY_DIR%\dhall-to-yaml-ng.exe")
-    - if /I "%DEPLOY_TAG%" NEQ "" (7z a "bin\dhall-yaml-%DEPLOY_TAG%" "%DEPLOY_DIR%\yaml-to-dhall.exe")
-
-  test_script:
-    # chcp 65001 (utf-8) needed to make doctest work
-    - chcp 65001 && stack test dhall
-    - stack test dhall-json
-    - stack test dhall-yaml
-    - stack test dhall-bash
-    - stack test dhall-lsp-server
-    - stack bench dhall --benchmark-arguments "--quick --min-duration=0 --include-first-iter"
+test_script:
+  # chcp 65001 (utf-8) needed to make doctest work
+  - chcp 65001 && stack test dhall
+  - stack test dhall-json
+  - stack test dhall-yaml
+  - stack test dhall-bash
+  - stack test dhall-lsp-server
+  - stack bench dhall --benchmark-arguments "--quick --min-duration=0 --include-first-iter"
     
--
-  matrix:
-    only:
-      - STACK_YAML: stack-lts-6.yaml
-
-  build_script:
-    - stack build
-    - stack install --local-bin-path bin
-
-  test_script:
-    # chcp 65001 (utf-8) needed to make doctest work
-    - chcp 65001 && stack test dhall
-
 artifacts:
   - path: bin\dhall-%DEPLOY_TAG%
     name: dhall

dhall-json/src/Dhall/JSON/Yaml.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP               #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 

dhall-lsp-server/dhall-lsp-server.cabal
@@ -57,7 +57,7 @@ library
     , rope-utf16-splay     >= 0.3.1.0  && < 0.4
     , hslogger             >= 1.2.10   && < 1.4
     , lens                 >= 4.16.1   && < 4.19
-    , lens-family-core     >= 1.2.3    && < 2.1
+    , lens-family-core     >= 1.2.2    && < 2.1
     , megaparsec           >= 7.0.2    && < 8.1
     , mtl                  >= 2.2.2    && < 2.3
     , network-uri          >= 2.6.1.0  && < 2.7

dhall-yaml/src/Dhall/YamlToDhall.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP             #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Dhall.YamlToDhall

dhall/dhall.cabal
@@ -2,7 +2,7 @@ Name: dhall
 Version: 1.29.0
 Cabal-Version: >=1.10
 Build-Type: Simple
-Tested-With: GHC == 7.10.3, GHC == 8.4.3, GHC == 8.6.1
+Tested-With: GHC == 8.0.2, GHC == 8.4.3, GHC == 8.6.1
 License: BSD3
 License-File: LICENSE
 Copyright: 2017 Gabriel Gonzalez

dhall/src/Dhall/Import.hs
@@ -150,10 +150,6 @@ import Data.List.NonEmpty (NonEmpty(..))
 import Data.Semigroup (Semigroup(..))
 import Data.Text (Text)
 import Data.Void (Void, absurd)
-#if MIN_VERSION_base(4,8,0)
-#else
-import Data.Traversable (traverse)
-#endif
 import Data.Typeable (Typeable)
 import System.FilePath ((</>))
 import Dhall.Binary (StandardVersion(..))

commit 3b43e94e6a3455144aa961951a8d137049df65e0

Fix build of `dhall-try`
Ever since #1642 the `dhall-try` package was not being built by CI, which led
to it being inadvertently broken in #1649.  This change fixes the build for
`dhall-try` and adds it back to CI.

dhall-try/dhall-try.cabal
@@ -16,7 +16,7 @@ executable dhall-try
   main-is:             Main.hs
   build-depends:       base           >= 4.11.0.0 && < 5
                      , aeson-pretty   >= 0.8.7    && < 0.9
-                     , dhall          >= 1.19.0   && < 1.30
+                     , dhall          >= 1.19.0   && < 1.31
                      , dhall-json     >= 1.2.5    && < 1.7
                      , prettyprinter  >= 1.5.1    && < 1.7
                      , text           >= 1.2.3.0  && < 1.3

nix/haskeline_0_7_3_1.nix
@@ -1,0 +1,16 @@
+{ mkDerivation, base, bytestring, containers, directory, filepath
+, process, stdenv, stm, transformers, unix
+}:
+mkDerivation {
+  pname = "haskeline";
+  version = "0.7.4.3";
+  sha256 = "046d0930bc2dbc57a7cd9ddb5d1e92c7fdb71c6b91b2bbf673f5406843d6b679";
+  configureFlags = [ "-f-terminfo" ];
+  libraryHaskellDepends = [
+    base bytestring containers directory filepath process stm
+    transformers unix
+  ];
+  homepage = "https://github.com/judah/haskeline";
+  description = "A command-line interface for user input, written in Haskell";
+  license = stdenv.lib.licenses.bsd3;
+}

release.nix
@@ -49,6 +49,8 @@ in
           # `hnix` depends on `unix-2.7.*` and doesn't work with GHC 8.6
           # shared_8_6_1.dhall-nix
 
+          shared_ghcjs.dhall-try
+
           # However, we still use GHC 8.4.3 to build the distributed tarballs
           # due to a bug in GHC 8.6.1.  See:
           #