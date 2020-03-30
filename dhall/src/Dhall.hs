b3a5a785bf1ac2e2125f5c98c8e280840be708ca
Gabriel Gonzalez <Gabriel439@gmail.com>
2020-03-30 14:57:09 -0700

    Expose `{default,}InputNormalizer` (#1727)
    * Expose `{default,}InputNormalizer`
    
    ... as caught by @patrickmn in https://github.com/dhall-lang/dhall-haskell/pull/1696#issuecomment-605709924
    
    * Fix missing haddocks
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- dhall/src/Dhall.hs
+++ dhall/src/Dhall.hs
@@ -61,6 +61,8 @@ module Dhall
     , genericAuto
     , genericAutoWith
     , InterpretOptions(..)
+    , InputNormalizer(..)
+    , defaultInputNormalizer
     , SingletonConstructors(..)
     , defaultInterpretOptions
     , bool
@@ -1346,6 +1348,7 @@ data InterpretOptions = InterpretOptions
 newtype InputNormalizer = InputNormalizer
   { getInputNormalizer :: Dhall.Core.ReifiedNormalizer Void }
 
+-- | Default normalization-related settings (no custom normalization)
 defaultInputNormalizer :: InputNormalizer
 defaultInputNormalizer = InputNormalizer
  { getInputNormalizer = Dhall.Core.ReifiedNormalizer (const (pure Nothing)) }
