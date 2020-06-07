979002a23dd952e66471e7bc75a3deec44e0b06c
Gabriel Gonzalez <Gabriel439@gmail.com>
2020-06-07 11:18:47 -0700

    Fix `dhall-nix` build failure

--- nix/packages/semialign.nix
+++ nix/packages/semialign.nix
@@ -6,6 +6,8 @@ mkDerivation {
   pname = "semialign";
   version = "1.1";
   sha256 = "f3e218bf7fb3ea8145dbf1051e3460e99a0d4064f0e76238595f996858e287d8";
+  revision = "2";
+  editedCabalFile = "1s0gja9kig4j550kjgrknf6kyw8qns9fksrdm0rvv7bmf30dwh8n";
   libraryHaskellDepends = [
     base base-compat containers hashable semigroupoids tagged these
     transformers unordered-containers vector
