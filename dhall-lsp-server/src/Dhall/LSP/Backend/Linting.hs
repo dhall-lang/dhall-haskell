7d9ead5d97c1906a4699570eddc362e72c7e9dcc
Gabriel Gonzalez <Gabriel439@gmail.com>
2020-06-13 14:41:47 -0700

    Narrow source span for unused let warnings
    Fixes https://github.com/dhall-lang/dhall-haskell/issues/1847

--- dhall-lsp-server/src/Dhall/LSP/Backend/Linting.hs
+++ dhall-lsp-server/src/Dhall/LSP/Backend/Linting.hs
@@ -1,22 +1,24 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Dhall.LSP.Backend.Linting
   ( Suggestion(..)
   , suggest
-  , Dhall.lint
+  , Lint.lint
   )
 where
 
-import Dhall.Parser (Src)
-import Dhall.Core ( Expr(..), Binding(..), MultiLet(..), Import
-                  , subExpressions, multiLet, wrapInLets)
-import qualified Dhall.Lint as Dhall
-
-import Dhall.LSP.Backend.Diagnostics
-
-import qualified Data.List.NonEmpty as NonEmpty
+import Control.Lens (universeOf)
 import Data.Maybe (maybeToList)
 import Data.Monoid ((<>))
 import Data.Text (Text)
-import Control.Lens (universeOf)
+import Dhall.LSP.Backend.Diagnostics
+import Dhall.Parser (Src)
+import Dhall.Core (Expr(..), Binding(..), MultiLet(..), Import)
+
+import qualified Data.Maybe         as Maybe
+import qualified Dhall.Core         as Core
+import qualified Dhall.Lint         as Lint
+import qualified Data.List.NonEmpty as NonEmpty
 
 data Suggestion = Suggestion {
     range :: Range,
@@ -29,29 +31,38 @@ data Suggestion = Suggestion {
 -- the search beginning at different @let@s in the same let-block – only
 -- the outermost 'Let' of a let-block is wrapped in a 'Note'.
 diagLetInLet :: Expr Src a -> Maybe Suggestion
-diagLetInLet (Note _ (Let b e)) = case multiLet b e of
+diagLetInLet (Note _ (Let b e)) = case Core.multiLet b e of
     MultiLet _ (Note src (Let {})) ->
       Just (Suggestion (rangeFromDhall src) "Superfluous 'in' before nested let binding")
     _ -> Nothing
 diagLetInLet _ = Nothing
 
 -- Given a let-block compute all unused variables in the block.
-unusedBindings :: Eq a => MultiLet s a -> [Text]
+unusedBindings :: Eq a => MultiLet s a -> [ (Text, Maybe s) ]
 unusedBindings (MultiLet bindings d) =
-  let go bs@(Binding { variable = var} : _) | Just _ <- Dhall.removeUnusedBindings (wrapInLets bs d) = [var]
+  let go bs@(Binding { variable = var, value } : _)
+          | Just _ <- Lint.removeUnusedBindings (Core.wrapInLets bs d) =
+              [ (var, maybeSrc) ]
+        where
+          maybeSrc = case value of
+              Note src _ -> Just src
+              _          -> Nothing
       go _ = []
   in foldMap go (NonEmpty.tails bindings)
 
 -- Diagnose unused let bindings.
 diagUnusedBindings :: Eq a => Expr Src a -> [Suggestion]
-diagUnusedBindings (Note src (Let b e)) = map
-  (\var ->
-    Suggestion (rangeFromDhall src) ("Unused let binding '" <> var <> "'"))
-  (unusedBindings (multiLet b e))
+diagUnusedBindings (Note src (Let b e)) =
+    map adapt (unusedBindings (Core.multiLet b e))
+  where
+    adapt (var, maybeSrc) =
+        Suggestion (rangeFromDhall finalSrc) ("Unused let binding '" <> var <> "'")
+      where
+        finalSrc = Maybe.fromMaybe src maybeSrc
 diagUnusedBindings _ = []
 
 -- | Given an dhall expression suggest all the possible improvements that would
 --   be made by the linter.
 suggest :: Expr Src Import -> [Suggestion]
 suggest expr = concat [ maybeToList (diagLetInLet e) ++ diagUnusedBindings e
-                      | e <- universeOf subExpressions expr ]
+                      | e <- universeOf Core.subExpressions expr ]
