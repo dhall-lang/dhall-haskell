e94f8691f1ab63bab4ceeb80ccbfd7dedaf81ddb
German Robayo <germanrobayo33@gmail.com>
2020-06-24 12:19:35 -0400

    add trailing slash to directory name

--- nix/dhall-docs-gen.nix
+++ nix/dhall-docs-gen.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation rec {
     mkdir -p $out/nix-support
     export XDG_DATA_HOME=$out/
     dhall-docs --input ./Prelude > $out/dhall-docs.log
-    echo "report html $out/dhall-docs/5b8855b55ed149bcb1e6153c9c45c9be5d2c8e085b5bb3edf51398f326157b93-Prelude" >> $out/nix-support/hydra-build-products
+    echo "report html $out/dhall-docs/5b8855b55ed149bcb1e6153c9c45c9be5d2c8e085b5bb3edf51398f326157b93-Prelude/" >> $out/nix-support/hydra-build-products
     echo "report log $out/dhall-docs.log" >> $out/nix-support/hydra-build-products
   '';
 }
