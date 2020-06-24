2cad6df260ee642bcace0f8bd8fea74a47023fbd
German Robayo <germanrobayo33@gmail.com>
2020-06-24 12:36:07 -0400

    log the generated tree structure

--- nix/dhall-docs-gen.nix
+++ nix/dhall-docs-gen.nix
@@ -1,4 +1,4 @@
-{ stdenv, dhall-docs, unzip, fetchurl }:
+{ stdenv, dhall-docs, unzip, fetchurl, tree }:
 
 stdenv.mkDerivation rec {
   name = "dhall-docs-artifacts";
@@ -8,12 +8,13 @@ stdenv.mkDerivation rec {
     sha256 = "8c46f81d6131bf5dac715a3f107d0a43f4e8decaadd7ed36fbc4375b5b32eeaf";
   };
 
-  buildInputs = [ dhall-docs unzip ];
+  buildInputs = [ dhall-docs unzip tree ];
 
   installPhase = ''
     mkdir -p $out/nix-support
     export XDG_DATA_HOME=$out/
     dhall-docs --input ./Prelude > $out/dhall-docs.log
+    tree $XDG_DATA_HOME/dhall-docs > $out/dhall-docs.log
     echo "report html $out/dhall-docs/5b8855b55ed149bcb1e6153c9c45c9be5d2c8e085b5bb3edf51398f326157b93-Prelude/" >> $out/nix-support/hydra-build-products
     echo "report log $out/dhall-docs.log" >> $out/nix-support/hydra-build-products
   '';
