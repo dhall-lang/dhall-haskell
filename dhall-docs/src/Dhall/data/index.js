8ab946d42b48adf002b40debdfba6a234010b159
German Robayo <germanrobayo33@gmail.com>
2020-06-28 03:13:45 -0400

    fix bug with slow dark mode

--- dhall-docs/src/Dhall/data/index.js
+++ dhall-docs/src/Dhall/data/index.js
@@ -2,12 +2,15 @@ const DARK_MODE_OPT = 'dark-mode'
 const DARK_MODE_ACTIVE = 'dark-mode-active'
 const DARK_MODE_INACTIVE = 'dark-mode-inactive'
 
+// this is moved here because there is a delay on slow internet connections
+// that makes the dark mode longer to appear
+if (localStorage.getItem(DARK_MODE_OPT) == DARK_MODE_ACTIVE) {
+  document.body.classList.add('dark-mode')
+} else {
+  document.body.classList.remove('dark-mode')
+}
+
 function onReady() {
-  if (localStorage.getItem(DARK_MODE_OPT) == DARK_MODE_ACTIVE) {
-    document.body.classList.add('dark-mode')
-  } else {
-    document.body.classList.remove('dark-mode')
-  }
   document.getElementById('switch-light-dark-mode')
     .addEventListener('click', () => {
       document.body.classList.toggle('dark-mode')
