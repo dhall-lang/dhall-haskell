f76baa3aa116edfeb96f95741a50aa837f9db539
Simon Jakobi <simon.jakobi@gmail.com>
2020-07-31 22:36:29 +0200

    Use path-0.8 (#1962)
    * Use path-0.8
    
    See http://hackage.haskell.org/package/path-0.8.0/changelog.
    
    * stack.yaml: Unpin path-io revision
    
    * Update nix/packages/path-io.nix
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- nix/packages/path-io.nix
+++ nix/packages/path-io.nix
@@ -6,8 +6,8 @@ mkDerivation {
   pname = "path-io";
   version = "1.6.0";
   sha256 = "73e420ad029626182ad56c200640500e67fdb40b939c6e6e2a15363879ef8d41";
-  revision = "1";
-  editedCabalFile = "1kwrkpmwmar8nwaar02m3kfy24vl3kzm0m3iq0d4ryd84a6a0dax";
+  revision = "3";
+  editedCabalFile = "0rd7svl3jxzqnf8l2h4f7xwlv8av67y85bwmr40954disq714l74";
   libraryHaskellDepends = [
     base containers directory dlist exceptions filepath path temporary
     time transformers unix-compat
