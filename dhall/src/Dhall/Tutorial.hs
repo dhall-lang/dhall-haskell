584b97c6cf4abd3daffed7bad4844d88095fa72c
Jeremy Schlatter <jeremy@intelligence.org>
2020-11-20 16:59:55 -0800

    Fix more example code in tutorial
    - `Dhall.expected resultDecoder` is a `Validation Expected TypeErrors (Expr Src Void)`,
      not a `Expr Src Void`. `maximum` is a terse way of extracting the
      `Expr Src Void` or crashing in the error case. (And in this example,
      it is never the error case.)
    
    - ++ works for String, but not Data.Text. Use the Semigroup instance
      instead.

--- dhall/src/Dhall/Tutorial.hs
+++ dhall/src/Dhall/Tutorial.hs
@@ -1992,14 +1992,14 @@ import Dhall
 -- > resultDecoder = Dhall.auto
 -- >
 -- > resultType :: Expr Src Void
--- > resultType = Dhall.expected resultDecoder
+-- > resultType = maximum $ Dhall.expected resultDecoder
 -- >
 -- > resultTypeString :: String
 -- > resultTypeString = show $ pretty resultType
 --
 -- Now we just have to inject that type into the Dhall code and we are done. One common way to do that is to wrap the import of example.dhall in a let expression:
 --
--- > Dhall.input (Dhall.auto :: Dhall.Decoder Result) ("let Result = " ++ Data.Text.pack resultTypeString ++ " in ./example.dhall")
+-- > Dhall.input (Dhall.auto :: Dhall.Decoder Result) ("let Result = " <> Data.Text.pack resultTypeString <> " in ./example.dhall")
 --
 -- Now we can omit the definition of Result in our example.dhall file. While this will work perfectly Dhall provides a cleaner solution for our \"injection problem\":
 --
