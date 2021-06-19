625b861c8a0c50bbc9be525aca5b623120345c89
MarcosJLR <socram.693@gmail.com>
2021-06-19 17:07:22 -0400

    feat(dhall-to-csv): add basic main to test new dhallToCsv function

--- dhall-csv/dhall-to-csv/Main.hs
+++ dhall-csv/dhall-to-csv/Main.hs
@@ -1,6 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Data.ByteString.Lazy
+import qualified Data.Csv
+import qualified Data.HashMap.Strict
+import qualified Data.Text.IO
+import qualified Data.Vector
 import qualified Dhall.Csv
+import qualified GHC.IO.Encoding
 
 main :: IO ()
-main = putStrLn Dhall.Csv.hello
+main = do
+    GHC.IO.Encoding.setLocaleEncoding GHC.IO.Encoding.utf8
+
+    text <- Data.Text.IO.getContents
+    csv <- Dhall.Csv.codeToValue Nothing text
+
+    let header = case csv of
+            [] -> Data.Vector.empty
+            (m:_) -> Data.Vector.fromList $ Data.HashMap.Strict.keys m
+
+    Data.ByteString.Lazy.putStr $ Data.Csv.encodeByName header csv
+
+    return ()
