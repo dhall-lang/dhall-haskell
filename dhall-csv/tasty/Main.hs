4526861982ecb6f58daa908b6260b7293cdb3a75
MarcosJLR <38771386+MarcosJLR@users.noreply.github.com>
2021-06-18 20:34:08 -0400

    feat(dhall-csv): skeleton to provide golden testing for the package (#2225)

---
+++ dhall-csv/tasty/Main.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Test.Tasty           (TestTree)
+import Test.Tasty.Silver    (findByExtension)
+import System.FilePath      (takeBaseName, replaceExtension)
+
+import qualified Data.Text.IO
+import qualified GHC.IO.Encoding
+import qualified Test.Tasty
+import qualified Test.Tasty.Silver as Silver
+
+
+main :: IO ()
+main = do
+    GHC.IO.Encoding.setLocaleEncoding GHC.IO.Encoding.utf8
+
+    testTree <- goldenTests
+    Test.Tasty.defaultMain testTree
+
+goldenTests :: IO TestTree
+goldenTests = do
+    dhallToCsvTree <- dhallToCsvGolden
+    csvToDhallTree <- csvToDhallGolden
+    return $ Test.Tasty.testGroup "dhall-csv"
+        [ dhallToCsvTree
+        , csvToDhallTree
+        ]
+
+dhallToCsvGolden :: IO TestTree
+dhallToCsvGolden = do
+    dhallFiles <- findByExtension [".dhall"] "./tasty/data/dhall-to-csv"
+    return $ Test.Tasty.testGroup "dhall-to-csv"
+        [ Silver.goldenVsAction
+            (takeBaseName dhallFile)
+            csvFile
+            (Data.Text.IO.readFile dhallFile)
+            id
+        | dhallFile <- dhallFiles
+        , let csvFile = replaceExtension dhallFile ".csv"
+        ]
+
+csvToDhallGolden :: IO TestTree
+csvToDhallGolden = do
+    csvFiles <- findByExtension [".csv"] "./tasty/data/csv-to-dhall"
+    return $ Test.Tasty.testGroup "csv-to-dhall"
+        [ Silver.goldenVsAction
+            (takeBaseName csvFile)
+            dhallFile
+            (Data.Text.IO.readFile dhallFile)
+            id
+        | csvFile <- csvFiles
+        , let dhallFile = replaceExtension csvFile ".dhall"
+        ]
