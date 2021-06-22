ccfc1614578a1939d8956084e5da361e8e9accad
MarcosJLR <socram.693@gmail.com>
2021-06-22 12:36:04 -0400

    fix(dhall-csv): sort header to standardize output

--- dhall-csv/src/Dhall/Csv/Util.hs
+++ dhall-csv/src/Dhall/Csv/Util.hs
@@ -1,16 +1,17 @@
 module Dhall.Csv.Util (encodeCsvDefault) where
 
+import Data.List (sort)
 import Data.Text (Text)
 
 import qualified Data.ByteString.Lazy   as ByteString
 import qualified Data.Csv
 import qualified Data.HashMap.Strict    as HashMap
-import qualified Data.Vector            as Vector
 import qualified Data.Text.Encoding
+import qualified Data.Vector            as Vector
 
 encodeCsvDefault :: [Data.Csv.NamedRecord] -> Text
 encodeCsvDefault csv = Data.Text.Encoding.decodeUtf8 $ ByteString.toStrict $ Data.Csv.encodeByName header csv
   where
     header = case csv of
         [] -> Vector.empty
-        (m:_) -> Vector.fromList $ HashMap.keys m
+        (m:_) -> Vector.fromList $ sort $ HashMap.keys m
