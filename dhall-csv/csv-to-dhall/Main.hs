4476bded76c489272ae1dfff4bd9670178bf87c0
MarcosJLR <socram.693@gmail.com>
2021-06-27 09:38:00 -0400

    feat(csv-to-dhall): basic functions to convert CSVs to lists of records of text

--- dhall-csv/csv-to-dhall/Main.hs
+++ dhall-csv/csv-to-dhall/Main.hs
@@ -1,6 +1,73 @@
+{-#LANGUAGE OverloadedStrings#-}
+
 module Main where
 
-import qualified Dhall.CsvToDhall
+import Control.Exception    (SomeException, throwIO)
+import Dhall.CsvToDhall     (dhallFromCsv)
+import Dhall.Pretty         (CharacterSet (..))
+
+import qualified Control.Exception
+import qualified Data.Text.IO                              as Text.IO
+import qualified Data.Text.Prettyprint.Doc                 as Pretty
+import qualified Data.Text.Prettyprint.Doc.Render.Terminal as Pretty.Terminal
+import qualified Data.Text.Prettyprint.Doc.Render.Text     as Pretty.Text
+import qualified Dhall.Csv.Util
+import qualified Dhall.Pretty
+import qualified GHC.IO.Encoding
+import qualified System.Console.ANSI                       as ANSI
+import qualified System.IO as IO
+import qualified System.Exit
 
 main :: IO ()
-main = putStrLn Dhall.CsvToDhall.hello
+main = do
+    GHC.IO.Encoding.setLocaleEncoding GHC.IO.Encoding.utf8
+
+    let toCsv file = do
+            text <- case file of
+                    Nothing -> Text.IO.getContents
+                    Just path -> Text.IO.readFile path
+
+            case Dhall.Csv.Util.decodeCsvDefault text of
+                Left err -> throwIO (userError err)
+                Right csv -> pure csv
+
+    let renderExpression characterSet plain output expression = do
+            let document = Dhall.Pretty.prettyCharacterSet characterSet expression
+
+            let stream = Dhall.Pretty.layout document
+
+            case output of
+                Nothing -> do
+                    supportsANSI <- ANSI.hSupportsANSI IO.stdout
+
+                    let ansiStream =
+                            if supportsANSI && not plain
+                            then fmap Dhall.Pretty.annToAnsiStyle stream
+                            else Pretty.unAnnotateS stream
+
+                    Pretty.Terminal.renderIO IO.stdout ansiStream
+
+                    Text.IO.putStrLn ""
+
+                Just file_ ->
+                    IO.withFile file_ IO.WriteMode $ \h -> do
+                        Pretty.Text.renderIO h stream
+
+                        Text.IO.hPutStrLn h ""
+
+    handle $ do
+        csv <- toCsv Nothing
+
+        let expression = dhallFromCsv csv
+
+        renderExpression Unicode False Nothing expression
+
+
+handle :: IO a -> IO a
+handle = Control.Exception.handle handler
+  where
+    handler :: SomeException -> IO a
+    handler e = do
+        IO.hPutStrLn IO.stderr ""
+        IO.hPrint    IO.stderr e
+        System.Exit.exitFailure
--- dhall-csv/src/Dhall/CsvToDhall.hs
+++ dhall-csv/src/Dhall/CsvToDhall.hs
@@ -1,4 +1,24 @@
-module Dhall.CsvToDhall (hello) where
+module Dhall.CsvToDhall (dhallFromCsv) where
 
-hello :: String
-hello = "Hello CSV to Dhall!"
+import Data.Text.Encoding   (decodeUtf8)
+import Data.Void            (Void)
+import Dhall.Core           (Expr, RecordField)
+import Dhall.Src            (Src)
+
+import qualified Data.Csv
+import qualified Data.HashMap.Strict    as HashMap
+import qualified Data.Sequence          as Sequence
+import qualified Dhall.Core             as Core
+import qualified Dhall.Map
+
+dhallFromCsv :: [Data.Csv.NamedRecord] -> Expr Src Void
+dhallFromCsv csv = Core.ListLit Nothing $ Sequence.fromList $ map convertRecord csv
+  where
+    convertRecord :: Data.Csv.NamedRecord -> Expr Src Void
+    convertRecord recordCsv = Core.RecordLit dhallMap
+      where
+        recordTextKeys = HashMap.mapKeys decodeUtf8 recordCsv
+        recordDhallFields = HashMap.map convertField recordTextKeys
+        dhallMap = Dhall.Map.fromList $ HashMap.toList recordDhallFields
+    convertField :: Data.Csv.Field -> RecordField Src Void
+    convertField = Core.makeRecordField . Core.TextLit . (Core.Chunks []) . decodeUtf8
