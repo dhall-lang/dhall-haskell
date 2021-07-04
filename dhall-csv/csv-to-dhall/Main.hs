a9730df0ed75ae0dc67f177a9098b7ea56c3caf9
MarcosJLR <socram.693@gmail.com>
2021-07-04 09:45:27 -0400

    feat: use renderExpression from Dhall.Util

--- dhall-csv/csv-to-dhall/Main.hs
+++ dhall-csv/csv-to-dhall/Main.hs
@@ -8,13 +8,9 @@ import Dhall.Pretty         (CharacterSet (..))
 
 import qualified Control.Exception
 import qualified Data.Text.IO                              as Text.IO
-import qualified Data.Text.Prettyprint.Doc                 as Pretty
-import qualified Data.Text.Prettyprint.Doc.Render.Terminal as Pretty.Terminal
-import qualified Data.Text.Prettyprint.Doc.Render.Text     as Pretty.Text
 import qualified Dhall.Csv.Util
-import qualified Dhall.Pretty
+import qualified Dhall.Util
 import qualified GHC.IO.Encoding
-import qualified System.Console.ANSI                       as ANSI
 import qualified System.IO as IO
 import qualified System.Exit
 
@@ -31,36 +27,12 @@ main = do
                 Left err -> throwIO (userError err)
                 Right csv -> pure csv
 
-    let renderExpression characterSet plain output expression = do
-            let document = Dhall.Pretty.prettyCharacterSet characterSet expression
-
-            let stream = Dhall.Pretty.layout document
-
-            case output of
-                Nothing -> do
-                    supportsANSI <- ANSI.hSupportsANSI IO.stdout
-
-                    let ansiStream =
-                            if supportsANSI && not plain
-                            then fmap Dhall.Pretty.annToAnsiStyle stream
-                            else Pretty.unAnnotateS stream
-
-                    Pretty.Terminal.renderIO IO.stdout ansiStream
-
-                    Text.IO.putStrLn ""
-
-                Just file_ ->
-                    IO.withFile file_ IO.WriteMode $ \h -> do
-                        Pretty.Text.renderIO h stream
-
-                        Text.IO.hPutStrLn h ""
-
     handle $ do
         csv <- toCsv Nothing
 
         let expression = dhallFromCsv csv
 
-        renderExpression Unicode False Nothing expression
+        Dhall.Util.renderExpression Unicode False Nothing expression
 
 
 handle :: IO a -> IO a
