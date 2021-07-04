79e77713856aa297ba02b4a2badbb480fa206847
MarcosJLR <socram.693@gmail.com>
2021-07-04 12:45:01 -0400

    feat: add command line options for csv-to-dhall executable

--- dhall-csv/csv-to-dhall/Main.hs
+++ dhall-csv/csv-to-dhall/Main.hs
@@ -1,22 +1,129 @@
-{-#LANGUAGE OverloadedStrings#-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
+import Control.Applicative  (optional, (<|>))
 import Control.Exception    (SomeException, throwIO)
+import Data.Version         (showVersion)
 import Dhall.CsvToDhall     (dhallFromCsv)
 import Dhall.Pretty         (CharacterSet (..))
+import Data.Text            (Text)
+import Options.Applicative  (Parser, ParserInfo)
 
 import qualified Control.Exception
 import qualified Data.Text.IO                              as Text.IO
 import qualified Dhall.Csv.Util
 import qualified Dhall.Util
 import qualified GHC.IO.Encoding
+import qualified Options.Applicative                       as Options
+import qualified Paths_dhall_csv                           as Meta
 import qualified System.IO as IO
 import qualified System.Exit
+
+parserInfo :: ParserInfo Options
+parserInfo = Options.info
+          (  Options.helper <*> parseOptions)
+          (  Options.fullDesc
+          <> Options.progDesc "Convert a CSV expression to a Dhall expression, given the expected Dhall type"
+          )
+
+data Options
+    = Default
+        { schema     :: Maybe Text
+        , file       :: Maybe FilePath
+        , output     :: Maybe FilePath
+        , ascii      :: Bool
+        , plain      :: Bool
+        }
+    | Type
+        { file       :: Maybe FilePath
+        , output     :: Maybe FilePath
+        , ascii      :: Bool
+        , plain      :: Bool
+        }
+    | Version
+    deriving Show
+
+parseOptions :: Parser Options
+parseOptions =
+        typeCommand
+    <|> (   Default
+        <$> optional parseSchema
+        <*> optional parseFile
+        <*> optional parseOutput
+        <*> parseASCII
+        <*> parsePlain
+        )
+    <|> parseVersion
+  where
+    typeCommand =
+        Options.hsubparser
+            (Options.command "type" info <> Options.metavar "type")
+      where
+        info =
+            Options.info parser (Options.progDesc "Output the inferred Dhall type from a CSV value (not implemented)")
+
+        parser =
+                Type
+            <$> optional parseFile
+            <*> optional parseOutput
+            <*> parseASCII
+            <*> parsePlain
+
+    parseSchema =
+        Options.strArgument
+            (  Options.metavar "SCHEMA"
+            <> Options.help "Dhall type (schema).  You can omit the schema to let the executable infer the schema from the CSV value (not implemented)"
+            )
+
+    parseVersion =
+        Options.flag'
+            Version
+            (  Options.long "version"
+            <> Options.short 'V'
+            <> Options.help "Display version"
+            )
+
+    parseFile =
+        Options.strOption
+            (   Options.long "file"
+            <>  Options.help "Read CSV from a file instead of standard input"
+            <>  Options.metavar "FILE"
+            <>  Options.action "file"
+            )
+
+    parseOutput =
+        Options.strOption
+            (   Options.long "output"
+            <>  Options.help "Write Dhall expression to a file instead of standard output"
+            <>  Options.metavar "FILE"
+            <>  Options.action "file"
+            )
+
+    parseASCII =
+        Options.switch
+            (   Options.long "ascii"
+            <>  Options.help "Format code using only ASCII syntax"
+            )
+
+    parsePlain =
+        Options.switch
+            (   Options.long "plain"
+            <>  Options.help "Disable syntax highlighting"
+            )
+
 
 main :: IO ()
 main = do
     GHC.IO.Encoding.setLocaleEncoding GHC.IO.Encoding.utf8
+
+    options <- Options.execParser parserInfo
+
+    let toCharacterSet ascii = case ascii of
+            True  -> ASCII
+            False -> Unicode
 
     let toCsv file = do
             text <- case file of
@@ -27,12 +134,22 @@ main = do
                 Left err -> throwIO (userError err)
                 Right csv -> pure csv
 
-    handle $ do
-        csv <- toCsv Nothing
+    case options of
+        Version ->
+            putStrLn (showVersion Meta.version)
 
-        let expression = dhallFromCsv csv
+        Default{..} -> do
+            handle $ do
+                let characterSet = toCharacterSet ascii
 
-        Dhall.Util.renderExpression Unicode False Nothing expression
+                csv <- toCsv file
+
+                let expression = dhallFromCsv csv
+
+                Dhall.Util.renderExpression characterSet plain output expression
+
+        Type{} -> do
+            putStrLn "type command has not been implemented yet"
 
 
 handle :: IO a -> IO a
