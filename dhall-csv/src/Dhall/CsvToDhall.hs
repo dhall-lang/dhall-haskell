19e4f027c08b3d25817b9a9f68ebe195475e9a40
MarcosJLR <socram.693@gmail.com>
2021-08-14 19:28:49 -0400

    feat(csv-to-dhall): improved error messages

--- dhall-csv/src/Dhall/CsvToDhall.hs
+++ dhall-csv/src/Dhall/CsvToDhall.hs
@@ -109,9 +109,7 @@
     Optional values can be either missing or have the expected value.
     The missing value is represented by the empty string.
     If a field's expected value is an Optional and the field is not
-    in the CSV, then all the values will be None unless `--records-strict`
-    flag is specified, in that case it will throw an error if there
-    is any missign field.
+    in the CSV, then all the values will be None.
 
 > $ cat example.csv
 > exampleOptional
@@ -140,32 +138,38 @@ module Dhall.CsvToDhall (
     , CompileError(..)
     ) where
 
-import Control.Applicative      ((<|>))
-import Control.Exception        (Exception, throwIO)
-import Control.Monad.Catch      (MonadCatch, throwM)
-import Data.Csv                 (NamedRecord)
-import Data.Either              (lefts, rights)
-import Data.Either.Combinators  (mapRight)
-import Data.Foldable            (toList)
-import Data.List                ((\\))
-import Data.Text                (Text)
-import Data.Text.Encoding       (decodeUtf8, decodeUtf8', encodeUtf8)
-import Data.Text.Encoding.Error (UnicodeException)
-import Data.Text.Read           (decimal, double, signed)
-import Data.Void                (Void)
-import Dhall.Core               (Expr)
-import Dhall.Src                (Src)
-import Options.Applicative      (Parser)
+import Control.Applicative          ((<|>))
+import Control.Exception            (Exception, throwIO)
+import Control.Monad.Catch          (MonadCatch, throwM)
+import Data.Csv                     (NamedRecord)
+import Data.Either                  (lefts, rights)
+import Data.Either.Combinators      (mapRight)
+import Data.Foldable                (toList)
+import Data.List                    ((\\))
+import Data.Text                    (Text)
+import Data.Text.Encoding           (decodeUtf8, decodeUtf8', encodeUtf8)
+import Data.Text.Encoding.Error     (UnicodeException)
+import Data.Text.Prettyprint.Doc    (Pretty)
+import Data.Text.Read               (decimal, double, signed)
+import Data.Void                    (Void)
+import Dhall.Core                   (Expr)
+import Dhall.Src                    (Src)
+import Dhall.Util                   (_ERROR)
+import Options.Applicative          (Parser)
 
 import qualified Data.Csv
-import qualified Data.HashMap.Strict         as HashMap
-import qualified Data.Sequence               as Sequence
-import qualified Dhall.Core                  as Core
+import qualified Data.HashMap.Strict                    as HashMap
+import qualified Data.Sequence                          as Sequence
+import qualified Data.Text
+import qualified Data.Text.Prettyprint.Doc.Render.Text  as Pretty
+import qualified Dhall.Core                             as Core
 import qualified Dhall.Import
-import qualified Dhall.Map                   as Map
+import qualified Dhall.Map                              as Map
 import qualified Dhall.Parser
-import qualified Dhall.TypeCheck             as TypeCheck
-import qualified Options.Applicative         as O
+import qualified Dhall.Pretty
+import qualified Dhall.TypeCheck                        as TypeCheck
+import qualified Dhall.Util
+import qualified Options.Applicative                    as O
 
 -- ----------
 -- Conversion
@@ -292,16 +296,16 @@ dhallFromCsv Conversion{..} typeExpr = listConvert (Core.normalize typeExpr)
                             Right _field ->
                                 if _field == unionKey
                                 then Right $ Core.Field t $ Core.makeFieldSelection unionKey
-                                else Left $ Mismatch t field recordKey
+                                else Left $ Mismatch t _field recordKey
             f unionKey (Just _type) = do
                 expression <- fieldConvert recordKey _type maybeField
                 return (Core.App (Core.Field t $ Core.makeFieldSelection unionKey) expression)
 
         case (unions, rights (toList (Map.mapWithKey f tm)), maybeField) of
             (UNone  , _         , _         ) -> Left $ ContainsUnion t
-            (UStrict, xs@(_:_:_), Just field) -> Left $ UndecidableUnion t field xs
-            (UStrict, xs@(_:_:_), Nothing   ) -> Left $ UndecidableMissingUnion t xs
-            (_      , []        , Just field) -> Left $ Mismatch t field recordKey
+            (UStrict, xs@(_:_:_), Just field) -> Left $ UndecidableUnion t (decodeUtf8 field) recordKey xs
+            (UStrict, xs@(_:_:_), Nothing   ) -> Left $ UndecidableMissingUnion t recordKey xs
+            (_      , []        , Just field) -> Left $ Mismatch t (decodeUtf8 field) recordKey
             (_      , []        , Nothing   ) -> Left $ MissingKey recordKey
             (UFirst , x:_       , _         ) -> Right $ x
             (UStrict, [x]       , _         ) -> Right $ x
@@ -314,10 +318,13 @@ dhallFromCsv Conversion{..} typeExpr = listConvert (Core.normalize typeExpr)
 
     -- Bools
     fieldConvert key Core.Bool (Just field) =
-        case field of
-            "true"  -> Right (Core.BoolLit True)
-            "false" -> Right (Core.BoolLit False)
-            _ -> Left $ Mismatch Core.Bool field key
+        case decodeUtf8' field of
+            Left err -> Left $ UnicodeError err
+            Right _field ->
+                case _field of
+                    "true"  -> Right (Core.BoolLit True)
+                    "false" -> Right (Core.BoolLit False)
+                    _ -> Left $ Mismatch Core.Bool _field key
 
     -- Naturals
     fieldConvert key Core.Natural (Just field) =
@@ -325,8 +332,8 @@ dhallFromCsv Conversion{..} typeExpr = listConvert (Core.normalize typeExpr)
             Left err -> Left $ UnicodeError err
             Right _field ->
                 case decimal _field of
-                    Right (v, "") -> Right $ Core.NaturalLit v           -- What to do when there is more text left to read?
-                    _ -> Left $ Mismatch Core.Natural field key
+                    Right (v, "") -> Right $ Core.NaturalLit v
+                    _ -> Left $ Mismatch Core.Natural _field key
 
     -- Integers
     fieldConvert key Core.Integer (Just field) =
@@ -334,8 +341,8 @@ dhallFromCsv Conversion{..} typeExpr = listConvert (Core.normalize typeExpr)
             Left err -> Left $ UnicodeError err
             Right _field ->
                 case (signed decimal) _field of
-                    Right (v, "") -> Right $ Core.IntegerLit v           -- What to do when there is more text left to read?
-                    _ -> Left $ Mismatch Core.Integer field key
+                    Right (v, "") -> Right $ Core.IntegerLit v
+                    _ -> Left $ Mismatch Core.Integer _field key
 
     -- Doubles
     fieldConvert _ Core.Double (Just field) =
@@ -381,17 +388,162 @@ data CompileError
     | UnhandledKeys [Text] -- Keys in CSV but not in schema
     | Mismatch
         ExprX           -- Expected Dhall Type
-        Data.Csv.Field  -- Actual field
+        Text            -- Actual field
         Text            -- Record key
     | ContainsUnion ExprX
     | UndecidableUnion
         ExprX           -- Expected Type
-        Data.Csv.Field  -- CSV Field
+        Text            -- CSV Field
+        Text            -- Record Key
         [ExprX]         -- Multiple conversions
     | UndecidableMissingUnion
         ExprX           -- Expected Type
+        Text            -- Record Key
         [ExprX]         -- Multiple Conversions
     | UnicodeError UnicodeException
-    deriving Show
+
+instance Show CompileError where
+    show (Unsupported e) =
+        Data.Text.unpack $
+            _ERROR <> ": Invalid record field type                                           \n\
+            \                                                                                \n\
+            \Explanation: Only the following types of record fields are valid:               \n\
+            \                                                                                \n\
+            \● ❰Bool❱                                                                        \n\
+            \● ❰Natural❱                                                                     \n\
+            \● ❰Integer❱                                                                     \n\
+            \● ❰Double❱                                                                      \n\
+            \● ❰Text❱                                                                        \n\
+            \● ❰Optional❱ tp (where tp is a valid record field type)                         \n\
+            \● Unions *                                                                      \n\
+            \                                                                                \n\
+            \* Unions can have empty alternatives or alternatives with valid                 \n\
+            \  record field types                                                            \n\
+            \                                                                                \n\
+            \Expected one of the previous types but instead got:                             \n\
+            \                                                                                \n\
+            \" <> insert e
+
+    show (NotAList e) =
+        Data.Text.unpack $
+            _ERROR <> ": Top level object must be of type ❰List❱                             \n\
+            \                                                                                \n\
+            \Explanation: Translation from CSV only returns ❰List❱s of records.              \n\
+            \Other types can not be translated.                                              \n\
+            \                                                                                \n\
+            \Expected type List {...} but instead got the following type:                    \n\
+            \                                                                                \n\
+            \" <> insert e
+
+    show (NotARecord e) =
+        Data.Text.unpack $
+            _ERROR <> ": Elements of the top-level list must be records                      \n\
+            \                                                                                \n\
+            \Explanation: Translation from CSV only returns ❰List❱s of records.              \n\
+            \Other types can not be translated.                                              \n\
+            \                                                                                \n\
+            \Expected a record type but instead got the following type:                      \n\
+            \                                                                                \n\
+            \" <> insert e
+
+    show (TypeError e) = show e
+
+    show (BadDhallType t e) =
+        Data.Text.unpack $
+            _ERROR <> ": Schema expression parsed successfully but has wrong Dhall type.     \n\
+            \                                                                                \n\
+            \Expected Dhall type: Type                                                       \n\
+            \                                                                                \n\
+            \Actual Dhall type:                                                              \n\
+            \" <> insert t <>
+            "                                                                                \n\
+            \                                                                                \n\
+            \Parsed Expression:                                                              \n\
+            \" <> insert e
+
+    show (MissingKey key) =
+        Data.Text.unpack $
+            _ERROR <> ": Missing key: \'" <> key <> "\'.                                     \n\
+            \                                                                                \n\
+            \Explanation: Key present in Dhall type (and not optional) is not provided       \n\
+            \in CSV. Please make sure every field key (non-optional) in Dhall type is        \n\
+            \present in CSV header.                                                          \n\
+            \                                                                                \n\
+            \If working with headerless CSVs, fields in Dhall type should have keys          \n\
+            \_1, _2, _3, ... and so forth                                                    "
+
+    show (UnhandledKeys keys) =
+        Data.Text.unpack $
+            _ERROR <> ": Following key(s): " <> (Data.Text.intercalate ", " keys) <>        "\n\
+            \are not handled.                                                                \n\
+            \                                                                                \n\
+            \Explanation: Keys present in CSV header are not present in Dhall type.          \n\
+            \You may turn off the --strict-recs flag to ignore this error.                   "
+
+    show (Mismatch tp field key) =
+        Data.Text.unpack $
+            _ERROR <> ": Type Mismtatch with key: " <> key <>                               "\n\
+            \                                                                                \n\
+            \Explanation: Could not parse CSV field " <> field <>                           "\n\
+            \into the expected Dhall type:                                                   \n\
+            \                                                                                \n\
+            \" <> insert tp
+
+    show (ContainsUnion e) =
+        Data.Text.unpack $
+            _ERROR <> ": Dhall type contains a Union type.                                   \n\
+            \                                                                                \n\
+            \Explanation: Dhall type contains a Union type for one of the record fields.     \n\
+            \This error occurs because flag --unions-none is turned on. If it is desired to  \n\
+            \have unions in the parsed expression, disable --unions-none flag.               \n\
+            \                                                                                \n\
+            \Expected no unions but got the following type in schema:                        \n\
+            \                                                                                \n\
+            \" <> insert e
+
+    show (UndecidableUnion tp field key opts) =
+        Data.Text.unpack $
+            _ERROR <> ": A union typed field can be parsed in more than one way.             \n\
+            \                                                                                \n\
+            \Explanation: The CSV field " <> field <>                                       "\n\
+            \             with key " <> key <>                                              "\n\
+            \can be converted in more than one of the expected union type alternatives.      \n\
+            \This error occurs because flag --unions-strict is turned on. You may turn off   \n\
+            \this flag to select the first valid alternative found.                          \n\
+            \                                                                                \n\
+            \Expected union type:                                                            \n\
+            \                                                                                \n\
+            \" <> insert tp <>
+            "\nField can be parsed as the following expressions: \n"
+            <> Data.Text.intercalate
+            "\n------------------------------------------------------------------------------\n"
+            (map insert opts)
+
+    show (UndecidableMissingUnion tp key opts) =
+        Data.Text.unpack $
+            _ERROR <> ": A union typed field can be parsed in more than one way.             \n\
+            \                                                                                \n\
+            \Explanation: The record field key " <> key <>                                  "\n\
+            \             missing in CSV                                                     \n\
+            \can be converted in more than one of the expected union type alternatives.      \n\
+            \That is to say, there are more than one alternatives with Optional types.       \n\
+            \This error occurs because flag --unions-strict is turned on. You may turn off   \n\
+            \this flag to select the first valid alternative found.                          \n\
+            \                                                                                \n\
+            \Expected union type:                                                            \n\
+            \                                                                                \n\
+            \" <> insert tp <>
+            "\nMissing field can be parsed as the following expressions:                     \n\
+            \" <> Data.Text.intercalate
+            "\n------------------------------------------------------------------------------\n"
+            (map insert opts)
+
+    show (UnicodeError e) = show e
+
+    show _ = undefined
 
 instance Exception CompileError
+
+
+insert :: Pretty a => a -> Text
+insert = Pretty.renderStrict . Dhall.Pretty.layout . Dhall.Util.insert
