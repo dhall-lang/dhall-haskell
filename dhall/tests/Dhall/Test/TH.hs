e9e8b2e5e6fd6e49c3e97feceed478d252354378
Mann mit Hut <github+mmhat@with-h.at>
2021-09-10 19:02:45 +0200

    Added tests for `makeHaskellTypesWith`

--- dhall/tests/Dhall/Test/TH.hs
+++ dhall/tests/Dhall/Test/TH.hs
@@ -7,9 +7,12 @@
 
 module Dhall.Test.TH where
 
+import Control.Exception (throwIO)
+import Data.Either.Validation (Validation(..))
 import Dhall.TH   (HaskellType (..))
 import Test.Tasty (TestTree)
 
+import qualified Data.Text
 import qualified Dhall
 import qualified Dhall.TH
 import qualified Test.Tasty       as Tasty
@@ -70,3 +73,83 @@ makeHaskellTypeFromUnion = Tasty.HUnit.testCase "makeHaskellTypeFromUnion" $ do
     qux <- Dhall.input Dhall.auto "let T = ./tests/th/issue2066.dhall in T.Qux.Foo { foo = +2, bar = { baz = +3 } }"
 
     Tasty.HUnit.assertEqual "" qux (Foo MakeFoo{ foo = 2, bar = MakeBar{ baz = 3 } })
+
+Dhall.TH.makeHaskellTypesWith (Dhall.TH.defaultGenerateOptions
+    { Dhall.TH.constructorModifier = ("My" <>)
+    , Dhall.TH.fieldModifier = ("my" <>) . Data.Text.toTitle
+    })
+    [ MultipleConstructors "MyT" "./tests/th/example.dhall"
+    , MultipleConstructors "MyDepartment" "./tests/th/Department.dhall"
+    , SingleConstructor "MyEmployee" "Employee" "./tests/th/Employee.dhall"
+    ]
+
+deriving instance Eq   MyT
+deriving instance Eq   MyDepartment
+deriving instance Eq   MyEmployee
+deriving instance Show MyT
+deriving instance Show MyDepartment
+deriving instance Show MyEmployee
+
+testMakeHaskellTypesWith :: TestTree
+testMakeHaskellTypesWith = Tasty.HUnit.testCase "makeHaskellTypesWith" $ do
+    let text0 = "let T = ./tests/th/example.dhall in T.A { x = True, y = [] : List Text }"
+        ref0 = MyA{ myX = True, myY = [] }
+    myTest text0 ref0
+
+    let text1 = "let T = ./tests/th/example.dhall in T.B (None (List Natural))"
+        ref1 = MyB Nothing
+    myTest text1 ref1
+
+    let text2 = "let T = ./tests/th/example.dhall in T.C"
+        ref2 = MyC
+    myTest text2 ref2
+
+    let textDepartment = "let T = ./tests/th/Department.dhall in T.Sales"
+        refDepartment = MySales
+    myTest textDepartment refDepartment
+
+    let textEmployee = "let T = ./tests/th/Department.dhall in T.Sales"
+        refEmployee = MyEmployee{ myName = "", myDepartment = MySales }
+    myTest textEmployee refEmployee
+    where
+        myTest text ref = do
+            expr <- Dhall.inputExpr text
+            t <- case Dhall.extract Dhall.auto expr of
+                Failure e -> throwIO e
+                Success t -> return t
+
+            Tasty.HUnit.assertEqual "" t ref
+            Tasty.HUnit.assertEqual "" expr $ Dhall.embed Dhall.inject ref
+
+Dhall.TH.makeHaskellTypesWith (Dhall.TH.defaultGenerateOptions
+    { Dhall.TH.constructorModifier = ("NoFromDhall" <>)
+    , Dhall.TH.fieldModifier = ("noFromDhall" <>) . Data.Text.toTitle
+    , Dhall.TH.generateFromDhallInstance = False
+    })
+    [ MultipleConstructors "NoFromDhallT" "./tests/th/example.dhall"
+    ]
+
+instance Dhall.FromDhall NoFromDhallT
+
+Dhall.TH.makeHaskellTypesWith (Dhall.TH.defaultGenerateOptions
+    { Dhall.TH.constructorModifier = ("NoToDhall" <>)
+    , Dhall.TH.fieldModifier = ("noToDhall" <>) . Data.Text.toTitle
+    , Dhall.TH.generateToDhallInstance = False
+    })
+    [ MultipleConstructors "NoToDhallT" "./tests/th/example.dhall"
+    ]
+
+instance Dhall.ToDhall NoToDhallT
+
+Dhall.TH.makeHaskellTypesWith (Dhall.TH.defaultGenerateOptions
+    { Dhall.TH.constructorModifier = ("NoInstances" <>)
+    , Dhall.TH.fieldModifier = ("noInstances" <>) . Data.Text.toTitle
+    , Dhall.TH.generateFromDhallInstance = False
+    , Dhall.TH.generateToDhallInstance = False
+    })
+    [ MultipleConstructors "NoInstancesT" "./tests/th/example.dhall"
+    ]
+
+deriving instance Dhall.Generic NoInstancesT
+instance Dhall.FromDhall NoInstancesT
+instance Dhall.ToDhall NoInstancesT
