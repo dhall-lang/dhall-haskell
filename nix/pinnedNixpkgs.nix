8d3fe3d1389d154304b1a24b1360b94450a07d9b
Gabriel Gonzalez <Gabriel439@gmail.com>
2021-09-14 09:23:52 -0700

    Upgrade Nixpkgs

--- nix/pinnedNixpkgs.nix
+++ nix/pinnedNixpkgs.nix
@@ -1,5 +1,5 @@
 { nixpkgs = builtins.fetchTarball {
-    url    = "https://github.com/NixOS/nixpkgs/archive/0c960262d159d3a884dadc3d4e4b131557dad116.tar.gz";
-    sha256 = "0d7ms4dxbxvd6f8zrgymr6njvka54fppph1mrjjlcan7y0dhi5rb";
+    url    = "https://github.com/NixOS/nixpkgs/archive/d00b5a5fa6fe8bdf7005abb06c46ae0245aec8b5.tar.gz";
+    sha256 = "08497wbpnf3w5dalcasqzymw3fmcn8qrnbkf8rxxwwvyjdnczxdv";
   };
 }
