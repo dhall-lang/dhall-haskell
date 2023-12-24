4c0b92f8654f11bf1bd9cd3895a2a65d187d35f2
Vekhir <Vekhir@yahoo.com>
2023-12-24 07:35:58 +0100

    fix: Update CI lsp versions
    lsp 2.x needs 'row-types'

--- nix/packages/lsp-types.nix
+++ nix/packages/lsp-types.nix
@@ -1,24 +1,31 @@
 { mkDerivation, aeson, base, binary, containers, data-default
-, deepseq, Diff, dlist, exceptions, filepath, hashable, hspec
-, hspec-discover, lens, lib, mod, mtl, network-uri, QuickCheck
-, quickcheck-instances, safe, scientific, some, template-haskell
-, text, tuple, unordered-containers
+, deepseq, Diff, directory, dlist, exceptions, file-embed, filepath
+, hashable, hspec, hspec-discover, lens, lib, mod, mtl, network-uri
+, prettyprinter, QuickCheck, quickcheck-instances, regex, row-types
+, safe, some, template-haskell, text, unordered-containers
 }:
 mkDerivation {
   pname = "lsp-types";
-  version = "1.6.0.1";
-  sha256 = "47d084f3a56195706381b51ad2f5d1b22a958238f3e5519625dbc54a2503f670";
+  version = "2.0.1.0";
+  sha256 = "57406c159d14aa30b3d8e5f48ca713186b340f668c93e940e884387fe561ffe0";
+  isLibrary = true;
+  isExecutable = true;
   libraryHaskellDepends = [
     aeson base binary containers data-default deepseq Diff dlist
-    exceptions filepath hashable lens mod mtl network-uri safe
-    scientific some template-haskell text unordered-containers
+    exceptions file-embed filepath hashable lens mod mtl network-uri
+    row-types safe some template-haskell text unordered-containers
+  ];
+  executableHaskellDepends = [
+    base containers directory filepath mtl prettyprinter regex text
   ];
   testHaskellDepends = [
     aeson base filepath hspec lens network-uri QuickCheck
-    quickcheck-instances text tuple
+    quickcheck-instances row-types text
   ];
   testToolDepends = [ hspec-discover ];
+  doHaddock = false;
   homepage = "https://github.com/haskell/lsp";
   description = "Haskell library for the Microsoft Language Server Protocol, data types";
   license = lib.licenses.mit;
+  mainProgram = "generator";
 }
