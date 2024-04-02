7f8721fa7c1de6ab0784e5316a72e5860a1f94e8
kukimik <kukimik@poczta.onet.pl>
2024-04-03 00:34:37 +0200

    Render Markdown files as HTML

--- dhall-docs/src/Dhall/Docs/Markdown.hs
+++ dhall-docs/src/Dhall/Docs/Markdown.hs
@@ -2,13 +2,16 @@
 -}
 module Dhall.Docs.Markdown
     ( MarkdownParseError(..)
+    , MMark
+    , parseMarkdown
     , markdownToHtml
+    , MMark.render
     ) where
 
 import Data.Text       (Text)
 import Lucid
 import Path            (File, Path, Rel)
-import Text.MMark      (MMarkErr)
+import Text.MMark      (MMarkErr, MMark)
 import Text.Megaparsec (ParseErrorBundle (..))
 
 import qualified Path
@@ -27,6 +30,16 @@ markdownToHtml
     -> Text          -- ^ Text to parse
     -> Either MarkdownParseError (Html ())
 markdownToHtml relFile contents =
+    MMark.render <$> parseMarkdown relFile contents
+
+{-| Takes a text that could contain markdown and returns either the parsed
+    markdown or, if parsing fails, the error information.
+-}
+parseMarkdown
+    :: Path Rel File -- ^ Used by `Mmark.parse` for error messages
+    -> Text          -- ^ Text to parse
+    -> Either MarkdownParseError MMark
+parseMarkdown relFile contents =
     case MMark.parse (Path.fromRelFile relFile) contents of
         Left err -> Left MarkdownParseError { unwrap = err }
-        Right mmark -> Right $ MMark.render mmark
+        Right mmark -> Right mmark
