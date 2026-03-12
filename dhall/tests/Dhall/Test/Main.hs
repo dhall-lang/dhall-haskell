8e74fe179ff7f019d132285186efcb2a44d3c15c
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-03-12 15:23:08 +0300

    Add CSE

--- dhall/tests/Dhall/Test/Main.hs
+++ dhall/tests/Dhall/Test/Main.hs
@@ -7,6 +7,7 @@ import Test.Tasty      (TestTree)
 import qualified Dhall.Test.Dhall
 import qualified Dhall.Test.Diff
 import qualified Dhall.Test.DirectoryTree
+import qualified Dhall.Test.CSE
 import qualified Dhall.Test.Format
 import qualified Dhall.Test.Freeze
 import qualified Dhall.Test.Import
@@ -44,6 +45,8 @@ getAllTests = do
 
     lintTests <- Dhall.Test.Lint.getTests
 
+    cseTests <- Dhall.Test.CSE.getTests
+
     tagsTests <- Dhall.Test.Tags.getTests
 
     diffTests <- Dhall.Test.Diff.getTests
@@ -62,6 +65,7 @@ getAllTests = do
                 , typeinferenceTests
                 , formattingTests
                 , lintTests
+                , cseTests
                 , diffTests
                 , semanticHashTests
                 , tagsTests
